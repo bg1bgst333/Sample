2b45046ea044a03b450bf0923efc2a5422641634
B.G <bg1bgst333@gmail.com>
2014-12-12 16:41:40 +0900

    add main_function

---
+++ c/main_function/main_function/src/main_function/main_function.c
@@ -0,0 +1,14 @@
+/* ヘッダファイルのインクルード */
+#include <stdio.h> /* 標準入出力 */
+
+/* main関数の定義 */
+int main(void){ /* C言語プログラムとしてはここからがスタート. */
+
+  /* 以下の処理を実行 */
+  printf("main_function\n"); /* printfで"main_function"と出力する. */
+
+  /* プログラムの終了 */
+  return 0; /* システムに0を返す. */
+
+}
+
