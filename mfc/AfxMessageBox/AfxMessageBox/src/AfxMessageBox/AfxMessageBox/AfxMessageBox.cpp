fe2362bbf44e0d96698ae0320efd98ebb10dd91e
BG1supernode333 <bg.supernode@gmail.com>
2014-12-24 17:25:08 +0900

    add AfxMessageBox

---
+++ mfc/AfxMessageBox/AfxMessageBox/src/AfxMessageBox/AfxMessageBox/AfxMessageBox.cpp
@@ -0,0 +1,13 @@
+// ヘッダファイルのインクルード
+#include <afxwin.h>	// MFC標準
+ 
+// WinMain関数の定義
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){	// Windowsプログラムのエントリポイント
+
+	// 以下の処理を実行
+	AfxMessageBox("AfxMessageBox");	// AfxMessageBoxで"AfxMessageBox"と表示する.
+
+	// プログラムの終了
+	return 0;
+
+}
