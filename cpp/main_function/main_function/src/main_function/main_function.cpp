6d87129ac1c259dccf6af8e4199970359c34f968
bg1bgst333 <bg1bgst333@gmail.com>
2014-12-24 11:47:50 +0900

    add main_function

---
+++ cpp/main_function/main_function/src/main_function/main_function.cpp
@@ -0,0 +1,13 @@
+// ヘッダのインクルード
+#include <iostream> // C++標準入出力
+
+// C++形式でのmain関数の定義
+int main(){ // voidは省略可.
+
+  // 以下の処理を実行
+  std::cout << "main_function" << std::endl; // 出力演算子"<<"で"std::cout"に"main_function"と出力する.
+
+  // プログラムの終了
+  return 0; // システムに0を返す.
+
+}
