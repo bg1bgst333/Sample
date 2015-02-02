bd845dcc16fc0b5312b81486a08c6eb6c374d6f4
bg1bgst333 <bg1bgst333@gmail.com>
2015-02-02 11:02:19 +0900

    fixed strcmp.c's comment

--- c/strcmp/strcmp/src/strcmp/strcmp.c
+++ c/strcmp/strcmp/src/strcmp/strcmp.c
@@ -8,7 +8,7 @@ int main(void){
   /* 配列の宣言 */
   char str1[256]; /* 入力された文字列を格納するchar型配列str1. */
   char str2[] = "ABCDE"; /* 比較対象となるchar型配列str2は"ABCDE"と初期化. */
-  int compare_result; /* strcmpによる比較の結果を格納するint型変数compare_result */
+  int compare_result; /* strcmpによる比較の結果を格納するint型変数compare_result. */
 
   /* 文字列の入力 */
   printf("str1: "); /* str1の入力フォーム */
@@ -16,13 +16,13 @@ int main(void){
 
   /* 文字列の比較 */
   compare_result = strcmp(str1, str2); /* strcmpでstr1とstr2を比較. */
-  if (compare_result < 0){ /* str1とstr2で文字列長が短い方の長さをnとして, 最後のn-1番目のASCIIコードがstr1の方が小さい場合.(同じ場合は文字列長がstr1の方が短い場合.) */
+  if (compare_result < 0){ /* str1とstr2の要素である文字を先頭から比較して, 違う文字となったところで, ASCIIコードがstr1の比較対象文字の方が小さい場合.(str1, str2の文字列長の短い方の最後の文字まで同じだった場合は, str1が文字列長が短い場合.) */
   
     /* ASCII順でstr1の方が先. */
     printf("str1(\"%s\") < str2(\"%s\")\n", str1, str2); /* str1 < str2であることを出力. */
 
   }
-  else if (compare_result > 0){ /* str2の方が小さい場合.(同じ場合は文字列長がstr2の方が短い場合.) */
+  else if (compare_result > 0){ /* str2の比較対象文字の方が小さい場合.(短い方の最後まで同じ場合は, str2が短い場合.) */
 
     /* ASCII順でstr2の方が先. */
     printf("str1(\"%s\") > str2(\"%s\")\n", str1, str2); /* str1 > str2であることを出力. */
