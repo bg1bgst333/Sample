e8b4e11719bde5ac9b53a91df71ba3ebf70098b2
BG1supernode333 <bg.supernode@gmail.com>
2015-03-06 16:53:05 +0900

    add RegisterClass

---
+++ winapi/RegisterClass/RegisterClass/src/RegisterClass/RegisterClass/RegisterClass.cpp
@@ -0,0 +1,67 @@
+// ヘッダファイルのインクルード
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd){
+
+	// 変数の宣言
+	HWND hWnd;			// CreateWindowで作成したウィンドウのウィンドウハンドルを格納するHWND型変数hWnd.
+	MSG msg;			// ウィンドウメッセージ情報を格納するMSG構造体型変数msg.
+	WNDCLASS wc;		// ウィンドウクラス情報をもつWNDCLASS構造体型変数wc.
+
+	// ウィンドウクラスの設定
+	wc.lpszClassName = _T("RegisterClass");					// ウィンドウクラス名は"RegisterClass".
+	wc.style = CS_HREDRAW | CS_VREDRAW;						// スタイルはCS_HREDRAW | CS_VREDRAW.
+	wc.lpfnWndProc = DefWindowProc;							// ウィンドウプロシージャは既定の処理をするAPIのDefWindowProc.
+	wc.hInstance = hInstance;								// インスタンスハンドルは_tWinMainの引数.
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);				// アイコンはアプリケーション既定のもの.
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);				// カーソルは矢印.
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	// 背景は白ブラシ.
+	wc.lpszMenuName = NULL;									// メニューはなし.
+	wc.cbClsExtra = 0;										// 0でいい.
+	wc.cbWndExtra = 0;										// 0でいい.
+
+	// ウィンドウクラスの登録
+	if (!RegisterClass(&wc)){	// RegisterClassでウィンドウクラスを登録し, 0が返ったらエラー.
+
+		// エラー処理
+		MessageBox(NULL, _T("RegisterClass failed!"), _T("RegisterClass"), MB_OK | MB_ICONHAND);	// MessageBoxで"RegisterClass failed!"とエラーメッセージを表示.
+		return -1;	// 異常終了(1)
+
+	}
+
+	// ウィンドウの作成
+	hWnd = CreateWindow(_T("RegisterClass"), _T("RegisterClass"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);	// CreateWindowで, 上で登録した"RegisterClass"ウィンドウクラスのウィンドウを作成.
+	if (hWnd == NULL){	// ウィンドウの作成に失敗したとき.
+
+		// エラー処理
+		MessageBox(NULL, _T("CreateWindow failed!"), _T("RegisterClass"), MB_OK | MB_ICONHAND);	// MessageBoxで"CreateWindow failed!"とエラーメッセージを表示.
+		return -2;	// 異常終了(2)
+
+	}
+
+	// ウィンドウの表示
+	ShowWindow(hWnd, SW_SHOW);	// ShowWindowでSW_SHOWを指定してウィンドウの表示.
+
+	// メッセージループ
+	while (TRUE){	// 常にTRUE(真)で, 以下のbreak以外では抜けない.
+
+		// ウィンドウメッセージの取得
+		GetMessage(&msg, NULL, 0, 0);	// GetMessageでウィンドウメッセージを取得.
+		if (msg.message == WM_LBUTTONDOWN){	// もし, ウィンドウメッセージがWM_LBUTTONDOWN(マウスの左ボタンが押された)のとき.
+
+			// メッセージループの終了
+			break;	// breakでメッセージループを抜ける.
+
+		}
+
+	}
+
+	// ウィンドウの破棄
+	DestroyWindow(hWnd);	// DestroyWindowでウィンドウを破棄.
+
+	// プログラムの終了
+	return 0;
+
+}
