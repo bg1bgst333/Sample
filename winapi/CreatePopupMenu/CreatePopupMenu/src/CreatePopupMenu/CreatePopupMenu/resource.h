9f8302c98e2234eec096b72c3c38085412939b0f
BG1supernode333 <bg.supernode@gmail.com>
2015-04-29 11:40:31 +0900

    add CreatePopupMenu

---
+++ winapi/CreatePopupMenu/CreatePopupMenu/src/CreatePopupMenu/CreatePopupMenu/resource.h
@@ -0,0 +1,9 @@
+#define ID_ITEM_1_1	10101
+#define ID_ITEM_1_2	10102
+#define ID_ITEM_1_3	10103
+#define ID_ITEM_2_1	10201
+#define ID_ITEM_2_2	10202
+#define ID_ITEM_2_3	10203
+#define ID_ITEM_3_1	10301
+#define ID_ITEM_3_2	10302
+#define ID_ITEM_3_3	10303
