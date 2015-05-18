dc05ca9606274836a15a9726ed70bff610fb2b5b
BG1supernode333 <bg.supernode@gmail.com>
2015-05-18 14:16:47 +0900

    add STRINGTABLE

---
+++ winapi/STRINGTABLE/STRINGTABLE/src/STRINGTABLE/STRINGTABLE/resource.h
@@ -0,0 +1,18 @@
+#include <windows.h>
+
+#define IDR_MAINMENU	101
+#define ID_ITEM_1_1	10101
+#define ID_ITEM_1_2	10102
+#define ID_ITEM_1_3	10103
+#define ID_ITEM_2_1	10201
+#define ID_ITEM_2_2	10202
+#define ID_ITEM_2_3	10203
+#define ID_ITEM_3_1	10301
+#define ID_ITEM_3_2	10302
+#define ID_ITEM_3_3	10303
+
+#define IDD_DIALOG	201
+
+#define IDI_ICON1	301
+
+#define IDS_WINDOW_NAME	401
