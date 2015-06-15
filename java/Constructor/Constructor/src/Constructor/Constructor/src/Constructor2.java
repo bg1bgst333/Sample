bf74e2d838eda73c514ce1647ec88850cded90be
BG1supernode333 <bg.supernode@gmail.com>
2015-06-15 19:47:18 +0900

    add Constructor

---
+++ java/Constructor/Constructor/src/Constructor/Constructor/src/Constructor2.java
@@ -0,0 +1,32 @@
+// 整数値と文字列を持つクラス
+class Constructor2 {	// Constructor2クラスの定義
+
+	// フィールドの定義
+	private int i;	// int型フィールドi
+	private String str;	// Stringクラスフィールドstr
+
+	// メソッドの定義
+	// 初期化処理
+	Constructor2(){	// コンストラクタConstructor2()
+		i = 0;	// 0をiにセット.
+		str = "-";	// "-"をstrにセット.
+	}
+
+	// 初期化処理(指定の値をセット.)
+	Constructor2(int n, String s){	// コンストラクタConstructor2(int n, String s)
+		i = n;	// nをiにセット.
+		str = s;	// sをstrにセット.
+	}
+
+	// 整数値と文字列をフィールドにセット.
+	public void set(int n, String s) {	// メソッドsetの定義
+		i = n;	// nをiにセット.
+		str = s;	// sをstrにセット.
+	}
+
+	// 各フィールドを出力.
+	public void output(){	// メソッドoutputの定義
+		System.out.println("i = " + i + ", " + "str = " + str);	// iとstrを出力.
+	}
+
+}
