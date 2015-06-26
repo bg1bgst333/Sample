a4ec682a398c7b8aefea1ac9cc52418c0b7b5781
BG1supernode333 <bg.supernode@gmail.com>
2015-06-26 18:59:34 +0900

    add trycatchException

---
+++ java/trycatchException/trycatchException/src/trycatchException/trycatchException/src/trycatchException.java
@@ -0,0 +1,23 @@
+// メインクラス
+class trycatchException {	// trycatchExceptionクラスの定義
+
+	// Javaのエントリポイント
+	public static void main(String[] args) {	// mainメソッドの定義
+
+		// try-catch
+		try {	// 例外が起きそうな場所.(tryブロック)
+
+			// 0で除算してみる.
+			int i = 0 / 0;	// 0の除算になるので, 例外が発生する.
+
+		}
+		catch (Exception ex)	{	// 例外を処理する場所.(catchブロック)
+
+			// 例外処理
+			System.out.println("ex = " + ex.toString());	// toStringメソッドで例外exの内容を取得し, 出力.
+
+		}
+
+	}
+
+}
