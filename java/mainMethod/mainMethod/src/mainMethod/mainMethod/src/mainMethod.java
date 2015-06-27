f822824370b987c02a4b757b410a78454680cdd6
BG1supernode333 <bg.supernode@gmail.com>
2015-06-27 17:15:20 +0900

    delete mainMethod

--- java/mainMethod/mainMethod/src/mainMethod/mainMethod/src/mainMethod.java
+++
@@ -1,8 +0,0 @@
-// メインクラス
-class mainMethod {	// mainMethodクラスの定義
-	// Javaのエントリポイント
-	public static void main(String[] args) {	// mainメソッドの定義
-		// 以下の処理を実行.
-		System.out.println("mainMethod");	// System.out.printlnで"mainMethod"と出力.
-	}
-}
