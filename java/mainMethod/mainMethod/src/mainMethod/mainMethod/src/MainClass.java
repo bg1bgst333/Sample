8c5a4d51b47c06facadea308ecf08103e91e099d
BG1supernode333 <bg.supernode@gmail.com>
2015-06-27 17:23:19 +0900

    remake mainMethod

---
+++ java/mainMethod/mainMethod/src/mainMethod/mainMethod/src/MainClass.java
@@ -0,0 +1,8 @@
+// メインクラス
+class MainClass {	// MainClassクラスの定義
+	// Javaのエントリポイント
+	public static void main(String[] args) {	// mainメソッドの定義
+		// 以下の処理を実行.
+		System.out.println("mainMethod");	// System.out.printlnで"mainMethod"と出力.
+	}
+}
