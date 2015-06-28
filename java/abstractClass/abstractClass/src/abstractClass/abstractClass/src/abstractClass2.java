69b844dbf943b11b3f88c920aa4a412321f97545
BG1supernode333 <bg.supernode@gmail.com>
2015-06-28 10:21:26 +0900

    delete abstractClass

--- java/abstractClass/abstractClass/src/abstractClass/abstractClass/src/abstractClass2.java
+++
@@ -1,8 +0,0 @@
-// 抽象クラス
-abstract class abstractClass2 {	// 抽象クラスabstractClass2の定義
-
-	// メソッドの宣言
-	void set(int i, String str){}	// set(int i, Strimg str)の宣言.(処理を定義しない.)
-	void output(){}	// output()の宣言.(処理を定義しない.)
-
-}
