49bc47b7c04a90f4833abbfa4e8f7ffceb50f982
BG1supernode333 <bg.supernode@gmail.com>
2015-06-29 16:21:12 +0900

    MainMethod

---
+++ cs/MainMethod/MainMethod/src/MainMethod/MainMethod/MainClass.cs
@@ -0,0 +1,10 @@
+// メインクラスの定義
+class MainClass     // MainClassクラスの定義
+{
+    // メインメソッドの定義
+    static void Main()  // Mainメソッドの定義
+    {
+        // 以下の処理を実行
+        System.Console.WriteLine("MainMethod"); // System.Console.WriteLineで"MainMethod"と出力.
+    }
+}
