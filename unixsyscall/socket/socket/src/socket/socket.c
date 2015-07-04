1038b3c52e186e69dbc01b03876c8d5505179375
bg1bgst333 <bg1bgst333@gmail.com>
2015-07-04 17:03:35 +0900

    add socket

---
+++ unixsyscall/socket/socket/src/socket/socket.c
@@ -0,0 +1,32 @@
+/* ヘッダファイルのインクルード */
+#include <stdio.h> /* 標準入出力 */
+#include <stdio.h> /* UNIX標準 */
+#include <sys/types.h> /* 派生型 */
+#include <sys/socket.h> /* ソケット */
+
+/* main関数の定義 */
+int main(void){
+
+  /* 変数の宣言 */
+  int soc; /* ソケットファイルディスクリプタsoc */
+
+  /* ソケットの作成 */
+  soc = socket(AF_INET, SOCK_STREAM, 0); /* socketでソケットを作成し, ソケットファイルディスクリプタをfdに格納. */
+  if (soc == -1){ /* socが-1の時はエラー. */
+
+    /* エラー処理 */
+    printf("socket error!\n"); /* "socket error!"と出力. */
+    return -1; /* -1を返す. */
+
+  }
+
+  /* socの値を出力. */
+  printf("soc = %d\n", soc); /* printfでsocの値を出力. */
+
+  /* ソケットファイルディスクリプタを閉じる. */
+  close(soc); /* closeでsocを閉じる. */
+
+  /* プログラムの終了 */
+  return 0;
+
+}
