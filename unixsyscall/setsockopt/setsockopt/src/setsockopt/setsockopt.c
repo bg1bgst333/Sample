101ba9def898443a7630ac7db3e58fa9813dae42
bg1bgst333 <bg1bgst333@gmail.com>
2015-07-05 18:51:15 +0900

    add setsockopt

---
+++ unixsyscall/setsockopt/setsockopt/src/setsockopt/setsockopt.c
@@ -0,0 +1,66 @@
+/* ヘッダファイルのインクルード */
+#include <stdio.h> /* 標準入出力 */
+#include <stdio.h> /* UNIX標準 */
+#include <sys/types.h> /* 派生型 */
+#include <sys/socket.h> /* ソケット */
+#include <arpa/inet.h> /* IPアドレス変換 */
+
+/* main関数の定義 */
+int main(void){
+
+  /* 変数の宣言 */
+  int soc; /* ソケットファイルディスクリプタsoc */
+  struct sockaddr_in addr; /* IPアドレスやポート番号などのIPv4インターネット情報を持つsockaddr_in構造体addr */
+  int optval = 1; /* セットするオプションの値optvalを1に初期化. */
+
+  /* ソケットの作成 */
+  soc = socket(AF_INET, SOCK_STREAM, 0); /* socketでソケットを作成し, ソケットファイルディスクリプタをfdに格納. */
+  if (soc == -1){ /* socが-1の時はエラー. */
+
+    /* エラー処理 */
+    printf("socket error!\n"); /* "socket error!"と出力. */
+    return -1; /* -1を返す. */
+
+  }
+
+  /* socの値を出力. */
+  printf("soc = %d\n", soc); /* printfでsocの値を出力. */
+
+  /* バインドするアドレス情報addrの設定. */
+  addr.sin_family = AF_INET; /* IPv4インターネットのアドレス・ファミリーAF_INET */
+  addr.sin_port = htons(3000); /* ポート番号3000番をhtonsで変換してセット. */
+  addr.sin_addr.s_addr = INADDR_ANY; /* すべてのローカルインターフェイスにバインドする. */
+
+  /* オプションSO_REUSEADDRの有効化. */
+  if (setsockopt(soc, SOL_SOCKET, SO_REUSEADDR, (const char *)&optval, sizeof(optval)) == -1){ /* setsockoptでSO_REUSEADDRを有効化.(-1ならエラー.) */
+
+    /* エラー処理 */
+    printf("setsockopt(SO_REUSEADDR) error!\n"); /* "setsockopt(SO_REUSEADDR) error!"と出力. */
+    close(soc); /* closeでsocを閉じる. */
+    return -1; /* -1を返す. */
+
+  }
+
+  /* 成功したら"setsockopt(SO_REUSEADDR) success." */
+  printf("setsockopt(SO_REUSEADDR) success.\n"); /* printfで"setsockopt(SO_REUSEADDR) success."と出力. */
+   
+  /* socにaddrをバインド(紐付け)する. */
+  if (bind(soc, (struct sockaddr *)&addr, sizeof(addr)) == -1){ /* 戻り値が-1の時はエラー. */
+
+    /* エラー処理 */
+    printf("bind error!\n"); /* "bind error!"と出力. */
+    close(soc); /* closeでsocを閉じる. */
+    return -1; /* -1を返す. */
+
+  }
+
+  /* 成功したら"bind success." */
+  printf("bind success.\n"); /* printfで"bind success."と出力. */
+
+  /* ソケットファイルディスクリプタを閉じる. */
+  close(soc); /* closeでsocを閉じる. */
+
+  /* プログラムの終了 */
+  return 0;
+
+}
