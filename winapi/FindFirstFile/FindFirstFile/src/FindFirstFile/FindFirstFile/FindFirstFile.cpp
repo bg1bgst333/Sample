7c5b279bd1f516028b222cd4a7633d71a27698a8
BG1supernode333 <bg.supernode@gmail.com>
2015-08-23 19:33:55 +0900

    fixed FindFirstFile

--- winapi/FindFirstFile/FindFirstFile/src/FindFirstFile/FindFirstFile/FindFirstFile.cpp
+++ winapi/FindFirstFile/FindFirstFile/src/FindFirstFile/FindFirstFile/FindFirstFile.cpp
@@ -7,12 +7,12 @@
 int _tmain(int argc, TCHAR *argv[]){	// main関数のTCHAR版.
 
 	// 変数の宣言
-	HANDLE hFile;			// ファイルハンドルhFile.
+	HANDLE hFind;			// 検索ハンドルhFind.
 	WIN32_FIND_DATA wfd;	// ファイル検索情報wfd.
 
 	// "test～"で始まるファイルの中から最初に見つかったファイルの情報を取得.
-	hFile = FindFirstFile(_T("test*"), &wfd);	// FindFirstFileで"test*"に当てはまるファイルを探し, 最初に見つかったファイル情報をwfdに格納.
-	if (hFile == INVALID_HANDLE_VALUE){	// INVALID_HANDLE_VALUEなら失敗.
+	hFind = FindFirstFile(_T("test*"), &wfd);	// FindFirstFileで"test*"に当てはまるファイルを探し, 最初に見つかったファイル情報をwfdに格納.
+	if (hFind == INVALID_HANDLE_VALUE){	// INVALID_HANDLE_VALUEなら失敗.
 
 		// エラー処理.
 		_tprintf(_T("FindFirstFile Error!\n"));	// _tprintfで"FindFirstFile Error!"と出力.
@@ -24,7 +24,7 @@ int _tmain(int argc, TCHAR *argv[]){	// main関数のTCHAR版.
 	_tprintf(_T("wfd.cFileName = %s\n"), wfd.cFileName);	// 見つかったファイルのファイル名wfd.cFileNameを出力.
 
 	// ハンドルを閉じる.
-	FindClose(hFile);	// FindCloseでhFileを閉じる.
+	FindClose(hFind);	// FindCloseでhFindを閉じる.
 
 	// プログラムの終了.
 	return 0;
