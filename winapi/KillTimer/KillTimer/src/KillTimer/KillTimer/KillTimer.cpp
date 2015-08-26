be045a14aeab5a216e94400b678e63c5cdfd8217
BG1supernode333 <bg.supernode@gmail.com>
2015-08-27 01:57:43 +0900

    add KillTimer

---
+++ winapi/KillTimer/KillTimer/src/KillTimer/KillTimer/KillTimer.cpp
@@ -0,0 +1,330 @@
+// ヘッダファイルのインクルード
+// 既定のヘッダファイル
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+#include <stdio.h>		// C標準入出力
+#include <string.h>		// C文字列処理
+
+// 独自のヘッダファイル
+#include "resource.h"	// リソースID
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ウィンドウメッセージに対して独自の処理をするように定義したコールバック関数WindowProc.
+VOID CALLBACK TimerProc(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime);		// WM_TIMERが発生したときに行う処理を定義したコールバック関数TimerProc.
+
+// スタティック変数の宣言.
+static int count;	// タイマーで数えるカウントcount.
+static int count2;	// 10秒ごとに数えるカウントcount2.
+
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd){
+
+	// 変数の宣言・初期化
+	HWND hWnd;					// CreateWindowで作成したウィンドウのウィンドウハンドルを格納するHWND型変数hWnd.
+	MSG msg;					// ウィンドウメッセージ情報を格納するMSG構造体型変数msg.
+	WNDCLASS wc;				// ウィンドウクラス情報をもつWNDCLASS構造体型変数wc.
+	HDC hDC = NULL;				// このウィンドウのデバイスコンテキストハンドルhDCをNULLに初期化.
+
+	// ウィンドウクラスの設定
+	wc.lpszClassName = _T("KillTimer");						// ウィンドウクラス名は"KillTimer".
+	wc.style = CS_HREDRAW | CS_VREDRAW;						// スタイルはCS_HREDRAW | CS_VREDRAW.
+	wc.lpfnWndProc = WindowProc;							// ウィンドウプロシージャは独自の処理を定義したWindowProc.
+	wc.hInstance = hInstance;								// インスタンスハンドルは_tWinMainの引数.
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);				// アイコンはアプリケーション既定のもの.
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);				// カーソルは矢印.
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	// 背景は白ブラシ.
+	wc.lpszMenuName = MAKEINTRESOURCE(IDR_MAINMENU);		// メニューにはIDR_MAINMENUをMAKEINTRESOURCEマクロで指定.
+	wc.cbClsExtra = 0;										// 0でいい.
+	wc.cbWndExtra = 0;										// 0でいい.
+
+	// ウィンドウクラスの登録
+	if (!RegisterClass(&wc)){	// RegisterClassでウィンドウクラスを登録し, 0が返ったらエラー.
+
+		// エラー処理
+		MessageBox(NULL, _T("RegisterClass failed!"), _T("KillTimer"), MB_OK | MB_ICONHAND);	// MessageBoxで"RegisterClass failed!"とエラーメッセージを表示.
+		return -1;	// 異常終了(1)
+
+	}
+
+	// ウィンドウの作成
+	hWnd = CreateWindow(_T("KillTimer"), _T("KillTimer"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);	// CreateWindowで, 上で登録した"KillTimer"ウィンドウクラスのウィンドウを作成.
+	if (hWnd == NULL){	// ウィンドウの作成に失敗したとき.
+
+		// エラー処理
+		MessageBox(NULL, _T("CreateWindow failed!"), _T("KillTimer"), MB_OK | MB_ICONHAND);	// MessageBoxで"CreateWindow failed!"とエラーメッセージを表示.
+		return -2;	// 異常終了(2)
+
+	}
+
+	// ウィンドウの表示
+	ShowWindow(hWnd, SW_SHOW);	// ShowWindowでSW_SHOWを指定してウィンドウの表示.
+
+	// デバイスコンテキストの取得.
+	hDC = GetDC(hWnd);	// GetDCでデバイスコンテキストハンドルhDCを取得.
+
+	// PeekMessageによるメインループ.
+	while (TRUE){	// 常に真(TRUE)なので無限ループ.
+
+		// ウィンドウメッセージが来ているかを確認する.
+		if (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE)){	// PeekMessageでウィンドウメッセージが来ているかを確認し, 真なら来ている.(PM_NOREMOVEなのでメッセージキューからこのメッセージを削除しない.次のGetMessageがそのメッセージを処理する.)
+
+			// 来ていたらそのメッセージを取得.
+			if (GetMessage(&msg, NULL, 0, 0) > 0){	// GetMessageでPeekMessageで確認したメッセージを取得.
+
+				// ウィンドウメッセージの送出
+				TranslateMessage(&msg);	// TranslateMessageで仮想キーメッセージを文字メッセージへ変換.
+				DispatchMessage(&msg);	// DispatchMessageで受け取ったメッセージをウィンドウプロシージャ(この場合は独自に定義したWindowProc)に送出.
+
+			}
+			else{	// 正常終了(0), またはエラーによる異常終了(-1).
+
+				// メインループを抜ける.
+				break;	// breakでメインループを抜ける.
+
+			}
+
+		}
+		else{	// 偽ならウィンドウメッセージが来ていないとき.
+
+			// 何もしない.
+
+		}
+
+	}
+
+	// デバイスコンテキストの解放.
+	if (hDC != NULL){	// hDCが解放されていなければ.
+
+		// デバイスコンテキストを解放する.
+		ReleaseDC(hWnd, hDC);	// ReleaseDCでhDCを解放.
+		hDC = NULL;	// NULLをセット.
+
+	}
+
+	// プログラムの終了
+	return (int)msg.wParam;	// 終了コード(msg.wParam)を戻り値として返す.
+
+}
+
+// WindowProc関数の定義
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){	// ウィンドウメッセージに対して独自の処理をするように定義したウィンドウプロシージャ.
+
+	// ウィンドウメッセージに対する処理.
+	switch (uMsg){	// switch-casa文でuMsgの値ごとに処理を振り分ける.
+
+		// ウィンドウの作成が開始された時.
+		case WM_CREATE:		// ウィンドウの作成が開始された時.(uMsgがWM_CREATEの時.)
+
+			// WM_CREATEブロック
+			{
+
+				// ウィンドウ作成成功
+				return 0;	// return文で0を返して, ウィンドウ作成成功とする.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// ウィンドウが破棄された時.
+		case WM_DESTROY:	// ウィンドウが破棄された時.(uMsgがWM_DESTROYの時.)
+
+			// WM_DESTROYブロック
+			{
+
+				// 終了メッセージの送信.
+				PostQuitMessage(0);	// PostQuitMessageで終了コードを0としてWM_QUITメッセージを送信.(するとメッセージループのGetMessageの戻り値が0になるので, メッセージループから抜ける.)
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 画面の描画を要求された時.
+		case WM_PAINT:		// 画面の描画を要求された時.(uMsgがWM_PAINTの時.)
+ 
+			// WM_PAINTブロック
+			{
+
+				// このブロックのローカル変数・配列の宣言
+				HDC hDC;				// デバイスコンテキストハンドルを格納するHDC型変数hDC.
+				PAINTSTRUCT ps;			// ペイント情報を管理するPAINTSTRUCT構造体型の変数ps.
+				TCHAR tszCount[64];		// countを文字列に変換したものを格納するバッファtszCount.(長さ64)
+				TCHAR tszCount2[64];	// count2を文字列に変換したものを格納するバッファtszCount2.(長さ64)
+				int len;				// tszCountの長さlen.
+				int len2;				// tszCount2の長さlen2.
+
+				// ウィンドウの描画開始
+				hDC = BeginPaint(hwnd, &ps);	// BeginPaintでこのウィンドウの描画の準備をする. 戻り値にはデバイスコンテキストハンドルが返るので, hDCに格納.
+
+				// カウントを文字列に変換.
+				_stprintf(tszCount, _T("%d"), count);	// _stprintfでcountを文字列に変換して, tszCountに格納.
+				_stprintf(tszCount2, _T("%d"), count2);	// _stprintfでcount2を文字列に変換して, tszCount2に格納.
+				len = _tcslen(tszCount);	// _tcslenでtszCountの長さを取得し, lenに格納.
+				len2 = _tcslen(tszCount2);	// _tcslenでtszCount2の長さを取得し, len2に格納.
+
+				// TextOutでtszCount, tszCount2を描画.
+				TextOut(hDC, 25, 25, tszCount, len);	// TextOutでtszCountを描画.
+				TextOut(hDC, 25, 75, tszCount2, len2);	// TextOutでtszCount2を描画.
+
+				// ウィンドウの描画終了
+				EndPaint(hwnd, &ps);	// EndPaintでこのウィンドウの描画処理を終了する.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりして, コマンドが発生した時.
+		case WM_COMMAND:	// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりして, コマンドが発生した時.(uMsgがWM_COMMANDの時.)
+
+			// WM_COMMANDブロック
+			{
+
+				// どのメニュー項目が選択されたかを判定する.
+				switch (LOWORD(wParam)){	// LOWORD(wParam)で選択されたメニュー項目のIDが取得できるので, その値で判定する.
+
+					// 取得したIDごとに処理を分岐.
+					// Timer1のStartが選択された時.
+					case ID_ITEM_TIMER1_START:
+
+						// ID_ITEM_TIMER1_STARTブロック
+						{
+
+							// タイマーの開始.
+							SetTimer(hwnd, ID_TIMER1, 1000, TimerProc);	// SetTimerで1秒(1000ミリ秒)ごとにWM_TIMERを発行するタイマーをセット.
+
+							// 0を返す.
+							return 0;	// 処理したので戻り値として0を返す.
+
+						}
+
+						// 既定の処理へ向かう.
+						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+					// Timer1のStopが選択された時.
+					case ID_ITEM_TIMER1_STOP:
+
+						// ID_ITEM_TIMER1_STOPブロック
+						{
+
+							// タイマーの破棄
+							KillTimer(hwnd, ID_TIMER1);	// KillTimerでタイマーを破棄.
+
+							// 0を返す.
+							return 0;	// 処理したので戻り値として0を返す.
+
+						}
+
+						// 既定の処理へ向かう.
+						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+					// Timer2のStartが選択された時.
+					case ID_ITEM_TIMER2_START:
+
+						// ID_ITEM_TIMER2_STARTブロック
+						{
+
+							// 10秒ごとのタイマーの開始.
+							SetTimer(hwnd, ID_TIMER2, 10000, TimerProc);	// SetTimerで10秒(10000ミリ秒)ごとにWM_TIMERを発行するタイマーをセット.
+
+							// 0を返す.
+							return 0;	// 処理したので戻り値として0を返す.
+
+						}
+
+						// 既定の処理へ向かう.
+						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+					// Timer2のStopが選択された時.
+					case ID_ITEM_TIMER2_STOP:
+
+						// ID_ITEM_TIMER2_STOPブロック
+						{
+
+							// 10秒ごとのタイマーの破棄
+							KillTimer(hwnd, ID_TIMER2);	// KillTimerで10秒ごとのタイマーを破棄.
+
+							// 0を返す.
+							return 0;	// 処理したので戻り値として0を返す.
+
+						}
+
+						// 既定の処理へ向かう.
+						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+					// 上記以外の時.
+					default:
+
+						// 既定の処理へ向かう.
+						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+						
+				}
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// タイマーイベントが発生した時.
+		case WM_TIMER:	// タイマーイベントが発生した時.(uMsgがWM_TIMERの時.)
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 上記以外の時.
+		default:	// 上記以外の値の時の既定処理.
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+	}
+
+	// あとは既定の処理に任せる.
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);	// 戻り値も含めDefWindowProcに既定の処理を任せる.
+
+}
+
+// TimerProc関数の定義
+VOID CALLBACK TimerProc(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime){	// WM_TIMERが発生したときに行う処理をするコールバック関数.
+
+	// タイマーIDごとに処理を振り分ける.
+	switch (idEvent){	// idEventで振り分ける.
+
+		// ID_TIMER1の場合.
+		case ID_TIMER1:
+
+			// ID_TIMER1ブロック.
+			{
+
+				// カウントを1増やす.
+				count++;
+
+				// 画面を更新する.
+				InvalidateRect(hwnd, NULL, TRUE);	// InvalidateRectでクライアント領域全体を更新する.
+
+			}
+
+			// 抜ける.
+			break;	// breakで抜ける.
+
+		// ID_TIMER2の場合.
+		case ID_TIMER2:
+
+			// ID_TIMER2ブロック.
+			{
+
+				// 10秒ごとのカウントを1増やす.
+				count2++;
+
+				// 画面を更新する.
+				InvalidateRect(hwnd, NULL, TRUE);	// InvalidateRectでクライアント領域全体を更新する.
+
+			}
+
+			// 抜ける.
+			break;	// breakで抜ける.
+
+	}
+
+}
---
+++ winapi/KillTimer/KillTimer/src/KillTimer/KillTimer/resource.h
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+#define IDR_MAINMENU		101
+#define ID_ITEM_TIMER1_START	10101
+#define ID_ITEM_TIMER1_STOP	10102
+#define ID_ITEM_TIMER2_START	10201
+#define ID_ITEM_TIMER2_STOP	10202
+
+#define ID_TIMER1		201
+#define ID_TIMER2		202
