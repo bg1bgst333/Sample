3c6e646ef29d5e3d77a8d2e47ddd86534b606aca
BG1supernode333 <bg.supernode@gmail.com>
2015-08-29 10:26:44 +0900

    add HeapAlloc

---
+++ winapi/HeapAlloc/HeapAlloc/src/HeapAlloc/HeapAlloc/HeapAlloc.cpp
@@ -0,0 +1,31 @@
+// ヘッダファイルのインクルード
+#include <windows.h>	// 標準WindowsAPI
+#include <stdio.h>		// C標準入出力
+#include <string.h>		// C文字列処理
+
+// main関数の定義
+int main(int argc, TCHAR *argv[]){	// main関数
+
+	// 変数の宣言.
+	HANDLE hHeap;	// ヒープハンドルhHeap
+	LPSTR lpszText;	// 文字列ポインタlpszText
+
+	// このプロセスのヒープを取得.
+	hHeap = GetProcessHeap();	// GetProcessHeapでこのプロセスのヒープハンドルを取得.
+
+	// メモリ確保.
+	lpszText = (LPSTR)HeapAlloc(hHeap, HEAP_ZERO_MEMORY, 16);	// HeapAllocでサイズ16のメモリを確保し, そのメモリのポインタを返す.(LPVOIDなのでLPSTRにキャストしてlpszTextに格納.)
+
+	// 文字列を確保したメモリにコピー.
+	strcpy(lpszText, "ABCDE");	// strcpyでlpszTextに"ABCDE"をコピー.
+
+	// 文字列の出力.
+	printf("lpszText(hHeap) = %s\n", lpszText);	// printfでlpszTextを出力.
+
+	// メモリ解放.
+	HeapFree(hHeap, NULL, lpszText);	// HeapFreeでlpszTextのメモリを解放.
+
+	// プログラムの終了.
+	return 0;
+
+}
