16b30dbbf1484c6db2b2a3f4e0f908fd34149e59
BG1supernode333 <bg.supernode@gmail.com>
2015-09-05 19:13:01 +0900

    add WM_MOUSEMOVE

---
+++ winapi/WM_MOUSEMOVE/WM_MOUSEMOVE/src/WM_MOUSEMOVE/WM_MOUSEMOVE/WM_MOUSEMOVE.cpp
@@ -0,0 +1,159 @@
+// ヘッダファイルのインクルード
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+#include <stdio.h>		// 標準入出力
+#include <string.h>		// 文字列処理
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ウィンドウメッセージに対して独自の処理をするように定義したコールバック関数WindowProc.
+
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd){
+
+	// 変数の宣言
+	HWND hWnd;			// CreateWindowで作成したウィンドウのウィンドウハンドルを格納するHWND型変数hWnd.
+	MSG msg;			// ウィンドウメッセージ情報を格納するMSG構造体型変数msg.
+	WNDCLASS wc;		// ウィンドウクラス情報をもつWNDCLASS構造体型変数wc.
+
+	// ウィンドウクラスの設定
+	wc.lpszClassName = _T("WM_MOUSEMOVE");					// ウィンドウクラス名は"WM_MOUSEMOVE".
+	wc.style = CS_HREDRAW | CS_VREDRAW;						// スタイルはCS_HREDRAW | CS_VREDRAW.
+	wc.lpfnWndProc = WindowProc;							// ウィンドウプロシージャは独自の処理を定義したWindowProc.
+	wc.hInstance = hInstance;								// インスタンスハンドルは_tWinMainの引数.
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);				// アイコンはアプリケーション既定のもの.
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);				// カーソルは矢印.
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	// 背景は白ブラシ.
+	wc.lpszMenuName = NULL;									// メニューはなし.
+	wc.cbClsExtra = 0;										// 0でいい.
+	wc.cbWndExtra = 0;										// 0でいい.
+
+	// ウィンドウクラスの登録
+	if (!RegisterClass(&wc)){	// RegisterClassでウィンドウクラスを登録し, 0が返ったらエラー.
+ 
+		// エラー処理
+		MessageBox(NULL, _T("RegisterClass failed!"), _T("WM_MOUSEMOVE"), MB_OK | MB_ICONHAND);	// MessageBoxで"RegisterClass failed!"とエラーメッセージを表示.
+		return -1;	// 異常終了(1)
+ 
+	}
+
+	// ウィンドウの作成
+	hWnd = CreateWindow(_T("WM_MOUSEMOVE"), _T("WM_MOUSEMOVE"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);	// CreateWindowで, 上で登録した"WM_MOUSEMOVE"ウィンドウクラスのウィンドウを作成.
+	if (hWnd == NULL){	// ウィンドウの作成に失敗したとき.
+ 
+		// エラー処理
+		MessageBox(NULL, _T("CreateWindow failed!"), _T("WM_MOUSEMOVE"), MB_OK | MB_ICONHAND);	// MessageBoxで"CreateWindow failed!"とエラーメッセージを表示.
+		return -2;	// 異常終了(2)
+ 
+	}
+
+	// ウィンドウの表示
+	ShowWindow(hWnd, SW_SHOW);	// ShowWindowでSW_SHOWを指定してウィンドウの表示.
+ 
+	// メッセージループ
+	while (GetMessage(&msg, NULL, 0, 0) > 0){	// GetMessageでメッセージを取得, 戻り値が0より大きい間はループし続ける.
+ 
+		// ウィンドウメッセージの送出
+		TranslateMessage(&msg);	// TranslateMessageで仮想キーメッセージを文字メッセージへ変換.
+		DispatchMessage(&msg);	// DispatchMessageで受け取ったメッセージをウィンドウプロシージャ(この場合は独自に定義したWindowProc)に送出.
+
+	}
+ 
+	// プログラムの終了
+	return (int)msg.wParam;	// 終了コード(msg.wParam)を戻り値として返す.
+ 
+}
+
+// WindowProc関数の定義
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){	// ウィンドウメッセージに対して独自の処理をするように定義したウィンドウプロシージャ.
+
+	// スタティック変数の宣言
+	static int x;	// マウス位置座標x
+	static int y;	// マウス位置座標y
+
+	// ウィンドウメッセージに対する処理.
+	switch (uMsg){	// switch-casa文でuMsgの値ごとに処理を振り分ける.
+ 
+		// ウィンドウの作成が開始された時.
+		case WM_CREATE:		// ウィンドウの作成が開始された時.(uMsgがWM_CREATEの時.)
+ 
+			// WM_CREATEブロック
+			{
+
+				// ウィンドウ作成成功
+				return 0;	// return文で0を返して, ウィンドウ作成成功とする.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// ウィンドウが破棄された時.
+		case WM_DESTROY:	// ウィンドウが破棄された時.(uMsgがWM_DESTROYの時.)
+ 
+			// WM_DESTROYブロック
+			{
+
+				// 終了メッセージの送信.
+				PostQuitMessage(0);	// PostQuitMessageで終了コードを0としてWM_QUITメッセージを送信.(するとメッセージループのGetMessageの戻り値が0になるので, メッセージループから抜ける.)
+ 
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+		
+		// 画面の描画を要求された時.
+		case WM_PAINT:		// 画面の描画を要求された時.(uMsgがWM_PAINTの時.)
+
+			// WM_PAINTブロック
+			{
+
+				// 変数の宣言
+				HDC hDC;				// デバイスコンテキストハンドルhDC
+				PAINTSTRUCT ps;			// PAINTSTRUCT構造体変数ps
+				TCHAR tszPosition[32];	// マウスの位置座標文字列tszPosition(長さ32)
+
+				// ウィンドウの描画開始
+				hDC = BeginPaint(hwnd, &ps);	// BeginPaintでこのウィンドウの描画の準備をする. 戻り値にはデバイスコンテキストハンドルが返るので, hDCに格納.
+
+				// マウスの位置座標を描画.
+				_stprintf(tszPosition, _T("(%d, %d)"), x, y);	// _stprintfでマウス位置座標(x, y)を文字列に変換してtszPositionに格納.
+				TextOut(hDC, 50, 50, tszPosition, (int)_tcslen(tszPosition));	// TextOutでtszPositionを描画.
+
+				// ウィンドウの描画終了
+				EndPaint(hwnd, &ps);	// EndPaintでこのウィンドウの描画処理を終了する.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// マウスが移動した時.
+		case WM_MOUSEMOVE:	// マウスが移動した時.(uMsgがWM_MOUSEMOVEの時.)
+
+			// WM_MOUSEMOVEブロック
+			{
+
+				// マウスカーソルの位置座標を取得.
+				x = LOWORD(lParam);	// LOWORD(lParam)でマウスカーソルの位置座標xを取得.
+				y = HIWORD(lParam);	// HIWORD(lParam)でマウスカーソルの位置座標yを取得.
+
+				// 画面の更新
+				InvalidateRect(hwnd, NULL, TRUE);	// InvalidateRectでクライアント領域全体を更新.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 上記以外の時.
+		default:	// 上記以外の値の時の既定処理.
+ 
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+ 
+	}
+
+	// あとは既定の処理に任せる.
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);	// 戻り値も含めDefWindowProcに既定の処理を任せる.
+
+}
