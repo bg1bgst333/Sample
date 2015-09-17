6127c66c5d31751bbb6dd83c08734ff5c4d65c78
BG1supernode333 <bg.supernode@gmail.com>
2015-09-17 16:18:02 +0900

    add PBM_SETSTEP

---
+++ winapi/PBM_SETSTEP/PBM_SETSTEP/src/PBM_SETSTEP/PBM_SETSTEP/resource.h
@@ -0,0 +1,9 @@
+#include <windows.h>
+#include <commctrl.h>
+
+#define IDR_MAINMENU	101
+#define ID_DIALOG		10101
+#define IDD_DIALOG1		201
+#define IDC_PROGRESS1	301
+#define ID_BUTTON1		311
+#define ID_TIMER1		401
