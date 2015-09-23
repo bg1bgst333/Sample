7d78de26554ce94b7a7effefa7171d6e1734279e
BG1supernode333 <bg.supernode@gmail.com>
2015-09-23 12:48:37 +0900

    add InternetConnect

---
+++ winapi/InternetConnect/InternetConnect/src/InternetConnect/InternetConnect/InternetConnect.cpp
@@ -0,0 +1,35 @@
+// ヘッダファイルのインクルード
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+#include <stdio.h>		// 標準入出力
+#include <wininet.h>	// Win32インターネット拡張機能
+
+// _tmain関数の定義
+int _tmain(int argc, TCHAR *argv[]){	// main関数のTCHAR版.
+
+	// 変数の宣言
+	HINTERNET hInternet;	// InternetHandleで返されたインターネットハンドルを格納するHINTERNET型変数hInternet.
+	HINTERNET hSession;		// InternetConnectで返されたセッションハンドルを格納するHINTERNET型変数hSession.
+
+	// WinInetの初期化.
+	hInternet = InternetOpen(_T("B.G-STATION Sample InternetOpen"), INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);	// InternetOpenでWinInetを初期化して, 戻り値のインターネットハンドルをhInternetに格納する.
+
+	// インターネットハンドルの出力.
+	_tprintf(_T("hInternet = %08x\n"), hInternet);	// _tprintfでhInternetを文字列に変換して出力.
+
+	// 指定のサーバに接続.
+	hSession = InternetConnect(hInternet, _T("bgstation0.com"), INTERNET_DEFAULT_HTTP_PORT, NULL, NULL, INTERNET_SERVICE_HTTP, 0, 0);	// InternetConnectで"bgstation0.com"にHTTPで接続.
+
+	// セッションハンドルの出力.
+	_tprintf(_T("hSession = %08x\n"), hSession);	// _tprintfでhSessionを文字列に変換して出力.
+
+	// セッションハンドルを閉じる.
+	InternetCloseHandle(hSession);	// InternetCloseHandleでhSessionを閉じる.
+
+	// インターネットハンドルを閉じる.
+	InternetCloseHandle(hInternet);	// InternetCloseHandleでhInternetを閉じる.
+
+	// プログラムの終了.
+	return 0;
+
+}
