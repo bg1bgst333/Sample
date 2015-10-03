79f1aa31a825b57d36952c1ec50bee7a33fa4661
BG1supernode333 <bg.supernode@gmail.com>
2015-10-03 15:17:39 +0900

    add WSAHtons

---
+++ winapi/WSAHtons/WSAHtons/src/WSAHtons/WSAHtons/WSAHtons.cpp
@@ -0,0 +1,59 @@
+// ヘッダファイルのインクルード
+#include <tchar.h>		// TCHAR型
+#include <stdio.h>		// 標準入出力
+#include <winsock2.h>	// Windowsソケット
+#include <windows.h>	// 標準WindowsAPI
+
+// _tmain関数の定義
+int _tmain(int argc, TCHAR *argv[]){	// main関数のTCHAR版.
+
+	// 変数の宣言
+	WSADATA wsaData;	// WinSockの初期化に必要なWSADATA構造体変数wsaData.
+	int iRet;			// 初期化の結果iRet.
+	SOCKET soc;			// SOCKET型のソケットファイルディスクリプタsoc.
+	u_short ns_port;	// ポート番号のネットワークバイトオ―ダ値.
+
+	// WinSockの初期化
+	iRet = WSAStartup(MAKEWORD(2, 2), &wsaData);	// WSAStartupでWinSockの初期化.
+	if (iRet){	// 0でない場合.
+
+		// エラー処理.
+		_tprintf(_T("Error!(iRet = %d.)\n"), iRet);	// 戻り値を出力.
+		WSACleanup();	// WSACleanupで終了処理.
+		return -1;	// -1を返して異常終了.
+
+	}
+
+	// 初期化成功メッセージ.
+	_tprintf(_T("WSAStartup success!\n"));	// "WSAStartup success!"と出力.
+
+	// ソケットの作成
+	soc = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, 0);	// WSASocketでソケット作成.
+	if (soc == INVALID_SOCKET){	// socがINVALID_SOCKET( == -1)なら.
+
+		// エラー処理
+		_tprintf(_T("WSASocket Error!\n"));	// "WSASocket Error!"と出力.
+		WSACleanup();	// WSACleanupで終了処理.
+		return -1;	// -1を返して異常終了.
+
+	}
+
+	// socの値を出力.
+	_tprintf(_T("soc = %lu\n"), soc);	// socの値を出力.
+
+	// ポート番号の変換.
+	WSAHtons(soc, 4000, &ns_port);	// WSAHtonsでポート番号をネットワークバイトオ―ダに変換.
+
+	// ポート番号の出力.
+	_tprintf(_T("port = %04x, ns_port = %04x\n"), 4000, ns_port);	// ポート番号とネットワークバイトオ―ダなポート番号を出力.
+
+	// ソケットファイルディスクリプタを閉じる.
+	closesocket(soc);	// closesocketでsocを閉じる.
+
+	// WinSockの終了処理.
+	WSACleanup();	// WSACleanupで終了処理.
+
+	// プログラムの終了.
+	return 0;
+
+}
