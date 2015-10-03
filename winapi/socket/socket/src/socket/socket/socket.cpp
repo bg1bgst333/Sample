38e93bdfc51242a90785bff45444a00ea5262bf4
BG1supernode333 <bg.supernode@gmail.com>
2015-10-03 15:08:17 +0900

    add socket

---
+++ winapi/socket/socket/src/socket/socket/socket.cpp
@@ -0,0 +1,52 @@
+// ヘッダファイルのインクルード
+#include <tchar.h>		// TCHAR型
+#include <stdio.h>		// 標準入出力
+#include <winsock2.h>	// Windowsソケット
+#include <windows.h>	// 標準WindowsAPI
+
+// _tmain関数の定義
+int _tmain(int argc, TCHAR *argv[]){	// main関数のTCHAR版.
+
+	// 変数の宣言
+	WSADATA wsaData;	// WinSockの初期化に必要なWSADATA構造体変数wsaData.
+	int iRet;			// 初期化の結果iRet.
+	int soc;			// ソケットファイルディスクリプタsoc.
+
+	// WinSockの初期化
+	iRet = WSAStartup(MAKEWORD(2, 2), &wsaData);	// WSAStartupでWinSockの初期化.
+	if (iRet){	// 0でない場合.
+
+		// エラー処理.
+		_tprintf(_T("Error!(iRet = %d.)\n"), iRet);	// 戻り値を出力.
+		WSACleanup();	// WSACleanupで終了処理.
+		return -1;	// -1を返して異常終了.
+
+	}
+
+	// 初期化成功メッセージ.
+	_tprintf(_T("WSAStartup success!\n"));	// "WSAStartup success!"と出力.
+
+	// ソケットの作成
+	soc = socket(AF_INET, SOCK_STREAM, 0);	// socketでソケット作成.
+	if (soc == -1){	// socが-1なら.
+
+		// エラー処理
+		_tprintf(_T("socket Error!\n"));	// "socket Error!"と出力.
+		WSACleanup();	// WSACleanupで終了処理.
+		return -1;	// -1を返して異常終了.
+
+	}
+
+	// socの値を出力.
+	_tprintf(_T("soc = %d\n"), soc);	// socの値を出力.
+
+	// ソケットファイルディスクリプタを閉じる.
+	closesocket(soc);	// closesocketでsocを閉じる.
+
+	// WinSockの終了処理.
+	WSACleanup();	// WSACleanupで終了処理.
+
+	// プログラムの終了.
+	return 0;
+
+}
