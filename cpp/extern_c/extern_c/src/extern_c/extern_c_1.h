e128bf9722d676d7ca69f7b47c8f47d8fc24d2dd
bg1bgst333 <bg1bgst333@gmail.com>
2015-10-27 10:27:10 +0900

    fixed extern_c

--- cpp/extern_c/extern_c/src/extern_c/extern_c_1.h
+++ cpp/extern_c/extern_c/src/extern_c/extern_c_1.h
@@ -1,2 +1,2 @@
-/* $B4X?t$N%W%m%H%?%$%W@k8@(B */
-void extern_c_1_func(void); /* extern_c_1.c$B$N4X?t(Bextern_c_1_func$B$N@k8@(B. */
+/* 関数のプロトタイプ宣言 */
+void extern_c_1_func(void); /* extern_c_1.cの関数extern_c_1_funcの宣言. */
