c8beb0f0721dd624ed1e09479bc94646f526a409
BG1supernode333 <bg.supernode@gmail.com>
2016-02-02 12:26:32 +0900

    add CButton::GetCheck

---
+++ mfc/CButton/GetCheck/src/CButton/CButton/CButtonDlg.h
@@ -0,0 +1,32 @@
+// CButtonDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CButtonDlg ダイアログ
+class CButtonDlg : public CDialog
+{
+// コンストラクション
+public:
+	CButtonDlg(CWnd* pParent = NULL);	// 標準コンストラクタ
+
+// ダイアログ データ
+	enum { IDD = IDD_CBUTTON_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+};
