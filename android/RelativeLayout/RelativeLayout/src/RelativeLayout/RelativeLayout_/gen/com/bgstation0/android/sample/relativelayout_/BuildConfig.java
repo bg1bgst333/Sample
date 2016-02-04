cac194632e8342b3d2abfc9f713dbcd7ddb6918c
BG1supernode333 <bg.supernode@gmail.com>
2016-02-04 15:46:35 +0900

    add RelativeLayout

---
+++ android/RelativeLayout/RelativeLayout/src/RelativeLayout/RelativeLayout_/gen/com/bgstation0/android/sample/relativelayout_/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.bgstation0.android.sample.relativelayout_;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
