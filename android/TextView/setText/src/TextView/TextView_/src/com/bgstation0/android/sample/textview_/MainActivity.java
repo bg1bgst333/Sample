cebbb3d3dbab8a8bc95157bb1f83c3478da5e8c4
bg1bgst333 <bg1bgst333@gmail.com>
2016-02-20 21:12:16 +0900

    add TextView.setText

--- android/TextView/setText/src/TextView/TextView_/src/com/bgstation0/android/sample/textview_/MainActivity.java
+++ android/TextView/setText/src/TextView/TextView_/src/com/bgstation0/android/sample/textview_/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         // TextViewオブジェクトのインスタンスを作成し, "TextView.setText"とテキストをセットし, それをビューにセット.
         //setContentView(R.layout.activity_main);	// この部分はコメントアウト.
         TextView tv = new TextView(this);	// TextViewオブジェクトのインスタンスをtvに格納.
