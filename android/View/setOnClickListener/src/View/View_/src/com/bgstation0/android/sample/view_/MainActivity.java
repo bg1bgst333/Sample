376aafe77306e79677030d51141399b46b7cfa50
bg1bgst333 <bg1bgst333@gmail.com>
2016-03-01 00:04:19 +0900

    add View.setOnClickListener

---
+++ android/View/setOnClickListener/src/View/View_/src/com/bgstation0/android/sample/view_/MainActivity.java
@@ -0,0 +1,29 @@
+package com.bgstation0.android.sample.view_;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener{	// View.OnClickListenerを実装.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // button1を取得し, OnClickListenerとして自身をセット.
+        Button button1 = (Button)findViewById(R.id.button1);	// findViewByIdでR.id.button1を取得.
+        button1.setOnClickListener(this);	// button1.setOnClickListenerでthis(自身)をセット.
+    }
+    
+    // View.OnClickListenerインタフェースのオーバーライドメソッドを実装.
+    public void onClick(View v){	// onClickをオーバーライド.
+    	// イベント発生元のボタンを取得し, 表示名を"Clicked!"に変更.
+    	Button b1 = (Button)v;	// イベント発生元オブジェクトvがViewで返されるのでButtonにキャストしてb1に格納.
+		b1.setText("Clicked!");	// b1.setTextで表示名を"Clicked!"に変更.
+    }
+}
