6a4cb1c26e3b46075a7fcc88af9828aa321792ad
bg1bgst333 <bg1bgst333@gmail.com>
2016-03-31 11:14:07 +0900

    add CListBox::GetTextLen

---
+++ mfc/CListBox/GetTextLen/src/CListBox/CListBox/CListBoxDlg.h
@@ -0,0 +1,33 @@
+// CListBoxDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CListBoxDlg ダイアログ
+class CListBoxDlg : public CDialog
+{
+// コンストラクション
+public:
+	CListBoxDlg(CWnd* pParent = NULL);	// 標準コンストラクタ
+
+// ダイアログ データ
+	enum { IDD = IDD_CLISTBOX_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+};
