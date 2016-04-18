79ce35e0a383c4a28536024db4527c1d2a6d73ef
bg1bgst333 <bg1bgst333@gmail.com>
2016-04-18 17:18:04 +0900

    add Label

---
+++ dotnet/Label/Label/src/Label_/Label_/Form1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Label_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ラベルのテキストを変更する.
+            label1.Text = "label1 Clicked!";    // label1.Textのテキストを"label1 Clicked!"に変更.
+        }
+    }
+}
