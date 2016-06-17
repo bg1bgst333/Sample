2fc7151929415796acbc1b31822c14541ef9fca2
bg1bgst333 <bg1bgst333@gmail.com>
2016-06-17 13:23:39 +0900

    fixed public.cpp

--- cpp/public/public/src/public/public.cpp
+++ cpp/public/public/src/public/public.cpp
@@ -1,22 +1,18 @@
-// $B%X%C%@%U%!%$%k$N%$%s%/%k!<%I(B
-// $B4{Dj$N%X%C%@(B
-#include <iostream> // C++$BI8=`F~=PNO(B
-// $BFH<+$N%X%C%@(B
-#include "public.h" // $B%/%i%9(Bclass_public
+// 独自のヘッダ
+#include "public.h" // クラスclass_public
 
-// class_public$B$N%a%s%P$NDj5A(B.
-// $B%a%s%P4X?t(Bprint_base()
-void class_public::print_base(){ // class_base$B$N%a%s%P$r=PNO(B.
+// class_publicのメンバの定義.
+// メンバ関数print_base()
+void class_public::print_base(){ // class_baseのメンバを出力.
 
-  // class_base$B$N%a%s%P$r=PNO(B.
-  //private_variable_ = 1; // private$B$J$N$G%"%/%;%9IT2D(B.
-  //private_function(); // private$B$J$N$G%"%/%;%9IT2D(B.
-  protected_variable_ = 1; // protected$B$J$N$G%"%/%;%92D(B.
-  std::cout << "protected_variable_ = " << protected_variable_ << std::endl; // protected$B$J$N$G%"%/%;%92D(B.
-  protected_function(); // protected$B$J$N$G%"%/%;%92D(B.
-  public_variable_ = 1; // public$B$J$N$G%"%/%;%92D(B.
-  std::cout << "public_variable_ = " << public_variable_ << std::endl; // public$B$J$N$G%"%/%;%92D(B.
-  public_function(); // public$B$J$N$G%"%/%;%92D(B.
+  // class_baseのメンバを出力.
+  //private_variable_ = 1; // privateなのでアクセス不可.
+  //private_function(); // privateなのでアクセス不可.
+  protected_variable_ = 1; // protectedなのでアクセス可.
+  std::cout << "protected_variable_ = " << protected_variable_ << std::endl; // protectedなのでアクセス可.
+  protected_function(); // protectedなのでアクセス可.
+  public_variable_ = 1; // publicなのでアクセス可.
+  std::cout << "public_variable_ = " << public_variable_ << std::endl; // publicなのでアクセス可.
+  public_function(); // publicなのでアクセス可.
 
 }
-
