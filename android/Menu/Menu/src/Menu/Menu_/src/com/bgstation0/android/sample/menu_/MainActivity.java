2ce94943572c8ce1fa401a61a001f7bd7b56a758
bg1bgst333 <bg1bgst333@gmail.com>
2016-06-19 10:40:02 +0900

    add Menu

---
+++ android/Menu/Menu/src/Menu/Menu_/src/com/bgstation0/android/sample/menu_/MainActivity.java
@@ -0,0 +1,25 @@
+package com.bgstation0.android.sample.menu_;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // オプションメニューが作成される時.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+    	// メニューの作成
+    	getMenuInflater().inflate(R.menu.main, menu);	// getMenuInflaterでMenuItemを取得し, そのままinflateで指定されたR.menu.mainを元にmenuを作成.
+    	return true;	// trueを返す.
+    	
+    }
+}
