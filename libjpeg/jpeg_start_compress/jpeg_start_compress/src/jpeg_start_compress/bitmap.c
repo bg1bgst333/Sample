2ceb27c22e892680da40575dc99594c3b8583283
Hiroki Miura <hmiura@h.miura-VirtualBox>
2016-08-06 18:21:11 +0900

    add jpeg_start_compress

---
+++ libjpeg/jpeg_start_compress/jpeg_start_compress/src/jpeg_start_compress/bitmap.c
@@ -0,0 +1,49 @@
+/* ヘッダファイルのインクルード */
+/* 独自のヘッダファイル */
+#include "bitmap.h" /* ビットマップ */
+/* 既定のヘッダファイル */
+#include <stdio.h> /* 標準入出力 */
+#include <unistd.h> /* UNIX標準 */
+#include <sys/stat.h> /* ファイル状態 */
+#include <stdlib.h> /* 標準ユーティリティ */
+#include <string.h> /* C文字列処理 */
+
+/* load_bitmapの定義 */
+int load_bitmap(const char *filename, bitmap_t *bitmap_ptr){
+
+  /* 変数の宣言 */
+  FILE *fp = NULL; /* FILE型ポインタfpをNULLに初期化. */
+  bitmap_t bitmap = {0}; /* bitmap_t型bitmapを0で初期化. */
+  struct stat s = {0}; /* stat構造体sを0で初期化. */
+  int buf_size = 0; /* int型buf_sizeを0に初期化. */
+
+  /* ファイルを開く */
+  fp = fopen(filename, "rb"); /* fopenでfilenameをバイナリ読み込みで開く. */
+  if (fp != NULL){ /* NULLでなければ. */
+    /* ビットマップの読み込み. */
+    fread(&bitmap_ptr->file_header, sizeof(bitmap_file_header_t), 1, fp); /* freadでfile_headerを読み込み. */
+    fread(&bitmap_ptr->info_header, sizeof(bitmap_info_header_t), 1, fp); /* freadでinfo_headerを読み込み. */
+    stat(filename, &s); /* statでfilenameのファイル情報をsに格納. */
+    buf_size = s.st_size - sizeof(bitmap_file_header_t) - sizeof(bitmap_info_header_t); /* buf_sizeを求める. */
+    bitmap_ptr->buf = (unsigned char *)malloc(buf_size * sizeof(unsigned char)); /* mallocでバッファを確保. */
+    fread(bitmap_ptr->buf, sizeof(unsigned char), buf_size, fp); /* freadでピクセル列の読み込み. */
+    /* ファイルを閉じる. */
+    fclose(fp); /* fcloseでfpを閉じる. */
+    /* 0を返す. */
+    return 0;
+  }
+
+  /* -1を返す. */
+  return -1;
+
+}
+
+/* destroy_bitmapの定義 */
+void destroy_bitmap(bitmap_t *bitmap_ptr){
+
+  /* バッファメモリを解放 */
+  free(bitmap_ptr->buf); /* freeでbitmap_ptr->bufを解放. */
+  bitmap_ptr->buf = NULL; /* NULLをセット. */
+  memset(bitmap_ptr, 0, sizeof(bitmap_t)); /* memsetで0で埋める. */
+
+}
---
+++ libjpeg/jpeg_start_compress/jpeg_start_compress/src/jpeg_start_compress/bitmap.h
@@ -0,0 +1,54 @@
+/* 二重インクルードの防止 */
+#ifndef BITMAP_H
+#define BITMAP_H
+
+/* ワード境界を一時的に2にする. */
+#pragma pack(2)
+
+/* ビットマップファイルヘッダ構造体 */
+typedef struct tag_bitmap_file_header{
+  unsigned short type; /* タイプ */
+  unsigned int size; /* ファイルサイズ */
+  unsigned short reserved1; /* 予約済み */
+  unsigned short reserved2; /* 予約済み */
+  unsigned int off_bits; /* ビット配列までのオフセット */ 
+} bitmap_file_header_t;
+
+/* ワード境界を元に戻す. */
+#pragma pack()
+
+/* ビットマップ情報ヘッダ構造体 */
+typedef struct tag_bitmap_info_header{
+  unsigned int size; /* 構造体サイズ */ 
+  int width; /* 幅 */
+  int height; /* 高さ */
+  unsigned short planes; /* プレーンの数 */
+  unsigned short bit_count; /* ピクセルあたりの色数 */
+  unsigned int compression; /* 圧縮方式 */
+  unsigned int size_image; /* ビットマップのサイズ */
+  int x_pels_per_meter; /* 水平解像度 */
+  int y_pels_per_meter; /* 垂直解像度 */
+  unsigned int clr_used; /* イメージで使われている色数 */
+  unsigned int clr_important; /* 重要な色の数 */
+} bitmap_info_header_t;
+
+/* カラーテーブル構造体(今回は24bitビットマップのみ扱うので使わない.) */
+typedef struct tag_rgb_quad{
+  unsigned char blue; /* 青 */
+  unsigned char green; /* 緑 */
+  unsigned char red; /* 赤 */
+  unsigned char reserved; /* 予約済み */
+} rgb_quad_t;
+
+/* ビットマップ(今回はカラーテーブルを含まない.) */
+typedef struct tag_bitmap{
+  bitmap_file_header_t file_header; /* ビットマップファイルヘッダ */
+  bitmap_info_header_t info_header; /* ピットマップ情報ヘッダ */
+  unsigned char *buf; /* ピクセルバッファへのポインタ */
+} bitmap_t;
+
+/* 関数のプロトタイプ宣言 */
+int load_bitmap(const char *filename, bitmap_t *bitmap_ptr); /* ビットマップのロード */
+void destroy_bitmap(bitmap_t *bitmap_ptr); /* ビットマップの破棄 */
+
+#endif
