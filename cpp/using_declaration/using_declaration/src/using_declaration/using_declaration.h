365321577cd4773cd7e011fcf635861b52992d87
bg1bgst333 <bg1bgst333@gmail.com>
2016-08-26 15:47:18 +0900

    add using_declaration

---
+++ cpp/using_declaration/using_declaration/src/using_declaration/using_declaration.h
@@ -0,0 +1,15 @@
+// –¼‘O‹óŠÔtest1
+namespace test1{
+  int x; // intŒ^x.
+  int y; // intŒ^y.
+}
+// –¼‘O‹óŠÔtest2
+namespace test2{
+  int y; // intŒ^y.
+  int z; // intŒ^z.
+}
+// –¼‘O‹óŠÔtest3
+namespace test3{
+  int x; // intŒ^x.
+  int z; // intŒ^z.
+}
