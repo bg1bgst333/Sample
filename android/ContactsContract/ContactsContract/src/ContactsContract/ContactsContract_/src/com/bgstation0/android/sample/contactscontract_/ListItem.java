3966480bdeda1b1cfa5bb634cf5675bda1ae9f28
bg1bgst333 <bg1bgst333@gmail.com>
2016-12-11 12:59:41 +0900

    add ContactsContract

---
+++ android/ContactsContract/ContactsContract/src/ContactsContract/ContactsContract_/src/com/bgstation0/android/sample/contactscontract_/ListItem.java
@@ -0,0 +1,8 @@
+package com.bgstation0.android.sample.contactscontract_;
+
+//リストアイテムクラスListItem
+public class ListItem {	// ListItemの定義
+	public String name;	// 名前name.
+	public String phoneNumber;	// 電話番号phoneNumber.
+	public String emailAddress;	// メールアドレスemailAddress.
+}
