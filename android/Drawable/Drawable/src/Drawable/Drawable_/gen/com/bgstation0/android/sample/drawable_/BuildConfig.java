75be7f2d3e0fd11c361e9c4955f57786d2c55b12
bg1bgst333 <bg1bgst333@gmail.com>
2017-01-01 13:32:52 +0900

    add Drawable

---
+++ android/Drawable/Drawable/src/Drawable/Drawable_/gen/com/bgstation0/android/sample/drawable_/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.bgstation0.android.sample.drawable_;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
