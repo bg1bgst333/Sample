537ea12a87ca881c1444897ed3a6bf7ef6633b74
bg1bgst333 <bg1bgst333@gmail.com>
2017-03-09 12:31:42 +0900

    add ifDirective

---
+++ cs/ifDirective/ifDirective/src/ifDirective/ifDirective/MainClass.cs
@@ -0,0 +1,25 @@
+// 名前空間の登録
+using System;       // 共通データ型と基本クラス(System名前空間)
+
+// メインクラス
+class MainClass // MainClassクラスの定義
+{
+
+    // メインメソッド
+    static void Main()  // Mainメソッドの定義
+    {
+
+// TESTが定義済みならコンパイル対象.
+#if TEST
+
+        // "Test"と出力.
+        Console.WriteLine("Test");  // Console.WriteLineで"Test"と出力.
+
+#endif
+
+        // "ABCDE"と出力.
+        Console.WriteLine("ABCDE"); // Console.WriteLineで"ABCDE"と出力.
+
+    }
+
+}
