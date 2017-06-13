eeb0eded578892f279fc521bb83c3ab0c82c28bb
BG1 <bg1bgst333@gmail.com>
2017-06-13 15:50:30 +0900

    fixed oauth2/token

--- twitter/oauth2/token/src/oauth2/oauth2/main.cpp
+++ twitter/oauth2/token/src/oauth2/oauth2/main.cpp
@@ -1,8 +1,9 @@
 // ヘッダのインクルード
 // 既定のヘッダ
-#include <tchar.h>	// TCHAR型
-#include <iostream>	// C++標準入出力
-#include <string>	// std::string
+#include <tchar.h>			// TCHAR型
+#include <iostream>			// C++標準入出力
+#include <string>			// std::string
+#include <openssl/bio.h>	// BIO 
 
 // マクロの定義
 // UNICODE切り替え
@@ -15,8 +16,17 @@
 // _tmain関数の定義
 int _tmain() {
 
-	// "oauth/token"と出力.
-	std::wcout << L"oauth/token" << std::endl;	// 出力演算子で"oauth/token"と出力.
+	// 変数の宣言と初期化.
+	BIO *buffer = NULL;	// バッファ用フィルタBIOへのポインタbufferをNULLに初期化.
+
+	// bufferの作成.
+	buffer = BIO_new(BIO_f_buffer());	// BIO_newにBIO_f_bufferの戻り値を渡すことでbufferを作成.
+
+	// bufferを出力.
+	_tprintf(_T("buffer = %08x\n"), buffer);	// _tprintfでbufferを出力.
+
+	// bufferの解放.
+	BIO_free(buffer);	// BIO_freeでbufferを解放.
 
 	// プログラムの終了
 	return 0;	// 0を返す.
