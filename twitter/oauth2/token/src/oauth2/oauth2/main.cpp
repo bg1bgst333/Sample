9bc4d24f71a012ec59c4588fbc7d1fe0330d4c44
BG1 <bg1bgst333@gmail.com>
2017-06-13 15:25:26 +0900

    add oauth2/token

---
+++ twitter/oauth2/token/src/oauth2/oauth2/main.cpp
@@ -0,0 +1,24 @@
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <tchar.h>	// TCHAR型
+#include <iostream>	// C++標準入出力
+#include <string>	// std::string
+
+// マクロの定義
+// UNICODE切り替え
+#ifdef UNICODE
+#define tstring std::wstring
+#else
+#define tstring std::string
+#endif
+
+// _tmain関数の定義
+int _tmain() {
+
+	// "oauth/token"と出力.
+	std::wcout << L"oauth/token" << std::endl;	// 出力演算子で"oauth/token"と出力.
+
+	// プログラムの終了
+	return 0;	// 0を返す.
+
+}
