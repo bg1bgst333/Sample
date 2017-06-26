54e45149a8f665146dbf0aa094aec95fd5b4a36a
BG1 <bg1bgst333@gmail.com>
2017-06-26 16:49:13 +0900

    add statuses/update

---
+++ twitter/statuses/update/src/statuses/statuses/credentials.h
@@ -0,0 +1,4 @@
+#define API_KEY _T("xxxxxxxxxxxxxxxxxxxxxxxxx")
+#define API_SECRET _T("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+#define ACCESS_TOKEN _T("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+#define ACCESS_TOEKN_SECRET _T("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
