872d1582690d7aab5dbab759ee70d5a57c368f2c
bg1bgst333 <bg1bgst333@gmail.com>
2017-07-14 11:45:22 +0900

    add WebView.HitTestResult.getType

---
+++ android/WebView.HitTestResult/getType/src/WebView.HitTestResult/WebView.HitTestResult_/src/com/bgstation0/android/sample/webview/hittestresult_/CustomWebChromeClient.java
@@ -0,0 +1,34 @@
+package com.bgstation0.android.sample.webview.hittestresult_;
+
+import android.app.Activity;
+import android.content.Context;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+public class CustomWebChromeClient extends WebChromeClient {
+
+	// メンバフィールドの定義
+	private Context context;	// Context型context
+	private TextView progressTextView;	// TextView型progressTextView
+	
+	// コンストラクタ
+	public CustomWebChromeClient(Context context){
+		
+		// メンバの初期化
+		this.context = context;	// 引数のcontextをメンバのcontextにセット.
+		Activity activity = (Activity)context;	// contextをActivity型のactivityにキャスト.
+		progressTextView = (TextView)activity.findViewById(R.id.progresstextview);	// progressTextViewを取得.
+		
+	}
+	
+	// ロードの進捗が変化した時.
+	@Override
+	public void onProgressChanged(WebView view, int newProgress){
+		
+		// 進捗の更新.
+		progressTextView.setText(newProgress + "%");	// progressTextView.setTextでnewProgressを"%"を付けて表示.
+		
+	}
+	
+}
