a14e89319173ffd56b22d28a79ff383c5cece5bd
bg1bgst333 <bg1bgst333@gmail.com>
2017-09-03 11:43:32 +0900

    add CWnd::UpdateData

---
+++ mfc/CWnd/UpdateData/src/CWnd/CWnd/CWndDlg.cpp
@@ -0,0 +1,100 @@
+// CWndDlg.cpp : 実装ファイル
+//
+
+#include "stdafx.h"
+#include "CWnd.h"
+#include "CWndDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CWndDlg ダイアログ
+
+
+
+
+CWndDlg::CWndDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CWndDlg::IDD, pParent)
+	, m_xvEdit1(_T(""))
+	, m_xvEdit2(_T(""))
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CWndDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, m_xvEdit1);
+	DDX_Text(pDX, IDC_EDIT2, m_xvEdit2);
+}
+
+BEGIN_MESSAGE_MAP(CWndDlg, CDialog)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	//}}AFX_MSG_MAP
+	ON_BN_CLICKED(IDC_BUTTON1, &CWndDlg::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// CWndDlg メッセージ ハンドラ
+
+BOOL CWndDlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// このダイアログのアイコンを設定します。アプリケーションのメイン ウィンドウがダイアログでない場合、
+	//  Framework は、この設定を自動的に行います。
+	SetIcon(m_hIcon, TRUE);			// 大きいアイコンの設定
+	SetIcon(m_hIcon, FALSE);		// 小さいアイコンの設定
+
+	// TODO: 初期化をここに追加します。
+
+	return TRUE;  // フォーカスをコントロールに設定した場合を除き、TRUE を返します。
+}
+
+// ダイアログに最小化ボタンを追加する場合、アイコンを描画するための
+//  下のコードが必要です。ドキュメント/ビュー モデルを使う MFC アプリケーションの場合、
+//  これは、Framework によって自動的に設定されます。
+
+void CWndDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 描画のデバイス コンテキスト
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// クライアントの四角形領域内の中央
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// アイコンの描画
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialog::OnPaint();
+	}
+}
+
+// ユーザーが最小化したウィンドウをドラッグしているときに表示するカーソルを取得するために、
+//  システムがこの関数を呼び出します。
+HCURSOR CWndDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+void CWndDlg::OnBnClickedButton1()
+{
+	// TODO: ここにコントロール通知ハンドラ コードを追加します。
+	UpdateData();	// UpdateData(デフォルト引数は1(TRUE))でDDX変数を更新.
+	m_xvEdit2 = m_xvEdit1;	// m_xvEdit1の内容をm_xvEdit2に代入.
+	UpdateData(FALSE);	// UpdateData(FALSE)でDDX変数の内容をコントロールに反映.
+}
---
+++ mfc/CWnd/UpdateData/src/CWnd/CWnd/CWndDlg.h
@@ -0,0 +1,34 @@
+// CWndDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CWndDlg ダイアログ
+class CWndDlg : public CDialog
+{
+// コンストラクション
+public:
+	CWndDlg(CWnd* pParent = NULL);	// 標準コンストラクタ
+
+// ダイアログ データ
+	enum { IDD = IDD_CWND_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_xvEdit1;
+	CString m_xvEdit2;
+	afx_msg void OnBnClickedButton1();
+};
