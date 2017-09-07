00b95c7315f386af544e595341cebd4ef824e714
BG1 <bg1bgst333@gmail.com>
2017-09-07 15:01:30 +0900

    add oauth2/token

---
+++ google/oauth2/token/src/oauth2/oauth2/credentials.h
@@ -0,0 +1,3 @@
+#define CLIENT_ID _T("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+#define REDIRECT_URI _T("xxxxxxxxxxxxxxxxxxxxxxxxx")
+#define SCOPE _T("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
