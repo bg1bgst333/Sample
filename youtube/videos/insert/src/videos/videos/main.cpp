1453e33d3d02916b5042def1ba377821ae8ddf9f
BG1 <bg1bgst333@gmail.com>
2017-09-08 11:47:56 +0900

    add videos/insert

---
+++ youtube/videos/insert/src/videos/videos/main.cpp
@@ -0,0 +1,26 @@
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <tchar.h>			// TCHAR型
+#include <iostream>			// C++標準入出力
+#include <winsock2.h>		// Windowsソケット
+#include <ws2tcpip.h>		// WinSock2 TCP/IP
+#include <openssl/bio.h>	// BIO 
+#include <openssl/ssl.h>	// SSL
+#include <openssl/err.h>	// エラー
+// 独自のヘッダ
+#include "credentials.h"	// 認証情報
+
+// マクロの定義
+// UNICODE切り替え
+#ifdef UNICODE
+#define tstring std::wstring
+#else
+#define tstring std::string
+#endif
+
+// _tmain関数の定義
+int _tmain() {
+
+	return 0;
+
+}
