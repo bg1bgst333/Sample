ae88e4755286ae7cc5cd762510fbcf23ea1a9573
BG1 <bg1bgst333@gmail.com>
2017-09-26 06:50:16 +0900

    add IObserver_T

---
+++ rx/IObserver_T/IObserver_T/src/IObserver_T/IObserver_T/CustomObservable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IObserver_T
+{
+    class CustomObservable : IObservable<string>
+    {
+        // オブザーバーリスト
+        private List<IObserver<string>> observers = new List<IObserver<string>>();  // IObserver<string>のList observersを生成.
+
+        public IDisposable Subscribe(IObserver<string> observer)
+        {
+            // オブザーバーの追加.
+            observers.Add(observer);    // observers.Addでobserverを追加.
+            return null;    // ひとまずnullを返しておく.
+            //throw new NotImplementedException();
+        }
+
+        // 独自の実行メソッドExecute
+        public void Execute(string value)
+        {
+            // オブザーバーのOnNextを実行.
+            foreach (var o in observers)    // observers全てに対して.
+            {
+                o.OnNext(value);    // o.OnNext(value)を実行.
+            }
+        }
+    }
+}
---
+++ rx/IObserver_T/IObserver_T/src/IObserver_T/IObserver_T/CustomObserver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IObserver_T
+{
+    class CustomObserver : IObserver<string>
+    {
+        public void OnCompleted()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnNext(string value)
+        {
+            // valueの表示.
+            MessageBox.Show("OnNext: " + value);    // MessageBox.Showでvalueを表示.
+            //throw new NotImplementedException();
+        }
+    }
+}
