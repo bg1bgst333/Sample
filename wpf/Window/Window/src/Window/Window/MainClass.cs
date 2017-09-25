c6b474d166327d942b7f373e3bc5d1a0a9ffe5eb
bg1bgst333 <bg1bgst333@gmail.com>
2017-09-25 13:40:04 +0900

    add Window

---
+++ wpf/Window/Window/src/Window/Window/MainClass.cs
@@ -0,0 +1,18 @@
+﻿// 名前空間の登録
+using System;   // 共通データ型と基本クラス(System名前空間)
+using System.Windows;   // WPFアプリケーション(System.Windows名前空間)
+
+// MainClassの定義
+class MainClass
+{
+    // Mainメソッドの定義.
+    [STAThread] // シングルスレッドアパートメント
+    static void Main()
+    {
+        // ウィンドウの作成とアプリケーションの実行.
+        Application application = new Application();    // Applicationオブジェクトapplicationの作成.
+        Window window = new Window();   // Windowオブジェクトwindowの作成.
+        window.Title = "Window";    // window.Titleでタイトルを"Window"にセット.
+        application.Run(window); // windowをapplication.Runに渡して実行.
+    }
+}
