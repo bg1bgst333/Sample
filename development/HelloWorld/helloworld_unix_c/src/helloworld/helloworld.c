5bd90f00059282b81ec3214f72c6fab198f8b71b
B.G <bg1bgst333@gmail.com>
2017-09-28 16:13:47 +0900

    add HelloWorld/helloworld_unix_c

---
+++ development/HelloWorld/helloworld_unix_c/src/helloworld/helloworld.c
@@ -0,0 +1,13 @@
+/* ヘッダファイルのインクルード */
+#include <stdio.h> /* C標準入出力 */
+
+/* main関数の定義 */
+int main(void){
+
+  /* "Hello, world!"の出力 */
+  printf("Hello, world!\n"); /* printfで"Hello, world!"を出力. */
+
+  /* プログラムの終了 */
+  return 0; /* 0を返して正常終了. */
+
+}
