654f25344ba59da498f8c0d54eafdfc7c0f0d336
B.G <bg1bgst333@gmail.com>
2017-09-30 14:53:15 +0900

    add HelloWorld/helloworld_unix_cpp

---
+++ development/HelloWorld/helloworld_unix_cpp/src/helloworld/helloworld.cpp
@@ -0,0 +1,13 @@
+// ヘッダのインクルード
+#include <iostream> // C++標準入出力
+
+// main関数の定義
+int main(){
+
+  // "Hello, world!"の出力
+  std::cout << "Hello, world!" << std::endl; // 出力演算子"<<"で"std::cout"に"Hello, world!"を出力.
+
+  // プログラムの終了
+  return 0; // 0を返して正常終了.
+
+}
