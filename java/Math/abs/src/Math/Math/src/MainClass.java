778cc574de7adf037c4920e3effbd2a6374e1c96
bg1bgst333 <bg1bgst333@gmail.com>
2018-05-30 16:24:30 +0900

    add Math.abs

---
+++ java/Math/abs/src/Math/Math/src/MainClass.java
@@ -0,0 +1,13 @@
+// メインクラス
+public class MainClass{	// MainClassの定義
+
+	// Javaのエントリポイント
+	public static void main(String[] args){	// mainメソッドの定義
+
+		// Math.absで絶対値を求める.
+		System.out.println(Math.abs(10));	// Math.abs(10)の結果を出力.
+		System.out.println(Math.abs(-10));	// Math.abs(-10)の結果を出力.
+
+	}
+
+}
