40414bc4751f6185056611abe54704dfb1b8b70e
bg1bgst333 <bg1bgst333@gmail.com>
2018-06-05 08:35:44 +0900

    add Spinner.setDropDownViewResource

---
+++ android/Spinner/setDropDownViewResource/src/Spinner/Spinner_/gen/com/bgstation0/android/sample/spinner_/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.bgstation0.android.sample.spinner_;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
