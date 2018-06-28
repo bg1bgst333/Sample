53709d0caacf7c61049b8fd3fbaaa98e706a19ae
bg1bgst333 <bg1bgst333@gmail.com>
2018-06-28 12:52:19 +0900

    add WebSettings.setSupportMultipleWindows

---
+++ android/WebSettings/setSupportMultipleWindows/src/WebSettings/WebSettings_/src/com/bgstation0/android/sample/websettings_/CustomWebChromeClient.java
@@ -0,0 +1,53 @@
+package com.bgstation0.android.sample.websettings_;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Message;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CustomWebChromeClient extends WebChromeClient {
+
+	// メンバフィールドの定義
+	private Context context;	// Context型context
+	private TextView progressTextView;	// TextView型progressTextView
+	private ProgressBar progressBar;	// ProgressBar型progressBar.
+	
+	// コンストラクタ
+	public CustomWebChromeClient(Context context){
+		
+		// メンバの初期化
+		this.context = context;	// 引数のcontextをメンバのcontextにセット.
+		Activity activity = (Activity)context;	// contextをActivity型のactivityにキャスト.
+		progressTextView = (TextView)activity.findViewById(R.id.progresstextview);	// progressTextViewを取得.
+		progressBar = (ProgressBar)activity.findViewById(R.id.progressbar);	// findViewByIdでR.id.progressbarを取得し, progressBarに格納.
+		
+	}
+	
+	// ロードの進捗が変化した時.
+	@Override
+	public void onProgressChanged(WebView view, int newProgress){
+		
+		// 進捗の更新.
+		progressTextView.setText(newProgress + "%");	// progressTextView.setTextでnewProgressを"%"を付けて表示.
+		progressBar.setProgress(newProgress);	// progressBar.setProgressで進捗にnewProgressをセット.
+		
+	}
+	
+	// ウィンドウ作成時.
+	@Override
+	public boolean onCreateWindow(WebView view, boolean dialog, boolean userGesture, Message resultMsg){
+		
+		// "onCreateWindow"を表示.
+		Activity activity = (Activity)context;	// contextをActivity型のactivityにキャスト.
+		Toast.makeText(activity, "onCreateWindow", Toast.LENGTH_LONG).show();	// "onCreateWindow"
+		
+		// 開いた先に制御が行っても, こちらも反応するようにfalseにする.
+		return false;	// falseを返す.
+		
+	}
+	
+}
