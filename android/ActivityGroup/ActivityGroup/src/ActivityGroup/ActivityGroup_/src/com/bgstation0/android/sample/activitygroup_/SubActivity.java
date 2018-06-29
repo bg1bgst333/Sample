826700d080b628f45181ca1a9db64da4ecb6da8a
bg1bgst333 <bg1bgst333@gmail.com>
2018-06-29 18:36:31 +0900

    add ActivityGroup

---
+++ android/ActivityGroup/ActivityGroup/src/ActivityGroup/ActivityGroup_/src/com/bgstation0/android/sample/activitygroup_/SubActivity.java
@@ -0,0 +1,15 @@
+package com.bgstation0.android.sample.activitygroup_;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubActivity extends Activity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+        
+	}
+	
+}
