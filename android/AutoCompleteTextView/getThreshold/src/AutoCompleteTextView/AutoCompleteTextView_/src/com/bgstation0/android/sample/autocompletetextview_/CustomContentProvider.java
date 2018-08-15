35fae2f38c5db1a4d3a30817c55a6f89e30e7c18
bg1bgst333 <bg1bgst333@gmail.com>
2018-08-15 15:53:46 +0900

    add AutoCompleteTextView.getThreshold

---
+++ android/AutoCompleteTextView/getThreshold/src/AutoCompleteTextView/AutoCompleteTextView_/src/com/bgstation0/android/sample/autocompletetextview_/CustomContentProvider.java
@@ -0,0 +1,20 @@
+package com.bgstation0.android.sample.autocompletetextview_;
+
+import android.content.SearchRecentSuggestionsProvider;
+
+public class CustomContentProvider extends SearchRecentSuggestionsProvider {
+
+	// メンバフィールドの定義.
+	public final static String AUTHORITY = "com.bgstation0.android.sample.autocompletetextview_.CustomContentProvider";
+	public final static int MODE =  DATABASE_MODE_QUERIES | DATABASE_MODE_2LINES;	// DATABASE_MODE_2LINESを追加.
+	
+	// コンストラクタ
+	public CustomContentProvider(){
+		
+		// 初期化処理.
+		super();	// 親クラスのコンストラクタを呼ぶ.
+		setupSuggestions(AUTHORITY, MODE);	// setupSuggestionsでAUTHORITYとMODEをセットして, セットアップ.
+		
+	}
+	
+}
