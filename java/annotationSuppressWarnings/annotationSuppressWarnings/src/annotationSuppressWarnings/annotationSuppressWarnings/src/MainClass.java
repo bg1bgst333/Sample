b384c819a04ee1cffe8a41dca3387d1e355890f3
bg1bgst333 <bg1bgst333@gmail.com>
2018-10-15 16:40:08 +0900

    add annotationSuppressWarnings

---
+++ java/annotationSuppressWarnings/annotationSuppressWarnings/src/annotationSuppressWarnings/annotationSuppressWarnings/src/MainClass.java
@@ -0,0 +1,14 @@
+// メインクラス
+@SuppressWarnings("deprecation")
+public class MainClass{	// MainClassの定義
+
+	// Javaのエントリポイント
+	public static void main(String[] args){	// mainメソッドの定義
+
+		// TestClassオブジェクトを作成し, TestClassオブジェクトに格納.
+		TestClass t = new TestClass();	// TestClassオブジェクトをTestClassのtに格納.
+		t.print();	// t.printで表示.
+
+	}
+
+}
---
+++ java/annotationSuppressWarnings/annotationSuppressWarnings/src/annotationSuppressWarnings/annotationSuppressWarnings/src/TestClass.java
@@ -0,0 +1,13 @@
+// テストクラス
+public class TestClass {
+
+	// メンバprintの定義.
+	@Deprecated
+	public void print(){
+
+		// "TestClass print!"と表示.
+		System.out.println("TestClass print!");	// printlnで"TestClass print!"の出力.
+
+	}
+
+}
