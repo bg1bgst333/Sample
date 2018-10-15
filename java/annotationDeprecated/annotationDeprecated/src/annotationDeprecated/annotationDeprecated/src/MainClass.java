ac669f2438c2b6c31f991efee235555b5dc8b1c1
bg1bgst333 <bg1bgst333@gmail.com>
2018-10-15 15:56:51 +0900

    add annotationDeprecated

---
+++ java/annotationDeprecated/annotationDeprecated/src/annotationDeprecated/annotationDeprecated/src/MainClass.java
@@ -0,0 +1,13 @@
+// メインクラス
+public class MainClass{	// MainClassの定義
+
+	// Javaのエントリポイント
+	public static void main(String[] args){	// mainメソッドの定義
+
+		// TestClassオブジェクトを作成し, TestClassオブジェクトに格納.
+		TestClass t = new TestClass();	// TestClassオブジェクトをTestClassのtに格納.
+		t.print();	// t.printで表示.
+
+	}
+
+}
