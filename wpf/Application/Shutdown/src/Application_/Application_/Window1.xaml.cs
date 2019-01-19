77f738639490b84d9d860690940b79f87630ec8d
bg1bgst333 <bg1bgst333@gmail.com>
2019-01-19 13:14:07 +0900

    add Application.Shutdown

---
+++ wpf/Application/Shutdown/src/Application_/Application_/Window1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Application_
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            // アプリケーションの終了
+            App.Current.Shutdown(); // App.Currentに現在のApplicationオブジェクトがあるので, それのShutdownを呼ぶことでApplicationを終了させる.
+
+        }
+    }
+}
