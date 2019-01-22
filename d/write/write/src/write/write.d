35cbb9bb26d9f01050754e0fc58ceaa4a77623da
Hiroki Miura <h.miura@localhost.localdomain>
2019-01-22 10:23:47 +0900

    add write

---
+++ d/write/write/src/write/write.d
@@ -0,0 +1,11 @@
+// モジュールのインポート
+import std.stdio; // 標準入出力
+
+// main関数の定義
+void main() // 戻り値はvoidでもいい.
+{
+
+  // "ABCDE"を出力.
+  write("ABCDE\n"); // writeで"ABCDE"を出力.(writeは改行しないので"\n"を付加.)
+
+}
