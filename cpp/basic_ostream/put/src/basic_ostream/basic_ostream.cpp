b820406377f3f2265eafa4c0edbdfbf2b3bdf4ab
B.G <bg1bgst333@gmail.com>
2019-02-19 23:03:44 +0900

    add basic_ostream::put

---
+++ cpp/basic_ostream/put/src/basic_ostream/basic_ostream.cpp
@@ -0,0 +1,13 @@
+// ヘッダのインクルード
+#include <iostream> // 標準入出力
+
+// main関数
+int main(){
+
+  // 文字の出力
+  std::cout.put('A'); // グローバルなbasic_ostream<char>オブジェクトであるstd::coutのputメソッドで'A'を出力.
+
+  // プログラムの終了.
+  return 0; // 0を返して正常終了.
+
+}
