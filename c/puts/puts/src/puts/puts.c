e4a9d3ddda556155afb724b6ba72e7da2c599d8e
B.G <bg1bgst333@gmail.com>
2019-02-23 12:01:12 +0900

    add puts

---
+++ c/puts/puts/src/puts/puts.c
@@ -0,0 +1,19 @@
+/* ヘッダファイルのインクルード */
+#include <stdio.h> /* 標準入出力 */
+
+/* main関数の定義 */
+int main(void){
+
+  /* 配列の初期化 */
+  char text[6] = "FGHIJ"; /* textを"FGHIJ"で初期化. */
+
+  /* 文字列の出力 */
+  puts("ABCDE"); /* putsで"ABCDE"を出力.(改行が付加される.)
+
+  /* 文字配列の出力 */
+  puts(text); /* putsでtextを出力.(改行が付加される.)
+
+  /* プログラムの終了 */
+  return 0; /* 0を返して正常終了. */
+
+}
