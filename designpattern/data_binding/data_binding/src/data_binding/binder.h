7254b1471a5d7d7781f448b5373f0ca87a7f0c01
B.G <bg1bgst333@gmail.com>
2019-04-02 10:01:28 +0900

    fixed data_binding

---
+++ designpattern/data_binding/data_binding/src/data_binding/binder.h
@@ -0,0 +1,20 @@
+// 二重インクルード防止
+#ifndef __BINDER_H_
+#define __BINDER_H_
+
+// テンプレートインターフェースinterface_binder<K>
+template <typename K> class interface_binder{
+
+  // 公開メンバ
+  public:
+
+    // 公開メンバ関数
+    // コンストラクタとデストラクタ
+    interface_binder(){}; // コンストラクタinterface_binder
+    virtual ~interface_binder(){}; // デストラクタ~interface_binder
+    // メンバ関数
+    virtual bool bind(interface_binder<K> *b, K *key) = 0; // 純粋仮想関数bind
+
+};
+
+#endif
