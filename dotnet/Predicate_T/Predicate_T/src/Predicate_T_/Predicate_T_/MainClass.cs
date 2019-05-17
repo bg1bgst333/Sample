2db0e541506aa206788614b32f2756e7a9b34968
bg1bgst333 <bg1bgst333@gmail.com>
2019-05-17 09:12:53 +0900

    add Predicate_T

---
+++ dotnet/Predicate_T/Predicate_T/src/Predicate_T_/Predicate_T_/MainClass.cs
@@ -0,0 +1,55 @@
+﻿// 名前空間の登録
+using System;       // 共通データ型と基本クラス(System名前空間)
+using System.IO;    // ファイル入出力とデータストリーム(System.IO名前空間)
+
+// メインクラス
+class MainClass // MainClassの定義
+{
+
+    // メインメソッド
+    static void Main()  // Mainメソッドの定義
+    {
+
+        // ファイルチェックデリゲートを定義済みデリゲートPredicate<T>で生成.(処理内容をラムダ式で定義.)
+        Predicate<string> fcd = (filename) =>
+        {
+
+            // ファイルが存在するかをチェック.
+            if (File.Exists(filename))  // File.Existsでfilenameなるファイルが存在するかチェック.
+            {
+
+                // 存在する場合.
+                Console.WriteLine(filename + " is Exists."); // "Exists"と出力.
+                return true;    // trueを返す.
+
+            }
+            else
+            {
+
+                // 存在しない場合.
+                Console.WriteLine(filename + " is not Exists."); // "not Exists"と出力.
+                return false;    // falseを返す.
+
+            }
+
+        };
+
+        // "test.txt"があるかどうかチェック.
+        if (fcd("test.txt"))    // fcdに"test.txt"を渡す.
+        {
+
+            // OK
+            Console.WriteLine("Check OK."); // "Check OK."と出力.
+
+        }
+        else
+        {
+
+            // NG
+            Console.WriteLine("Check NG."); // "Check NG."と出力.
+
+        }
+
+    }
+
+}
