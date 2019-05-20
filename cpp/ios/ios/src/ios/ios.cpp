870867331be53c99615fd12a3c3adf5bb35c6010
B.G <bg1bgst333@gmail.com>
2019-05-21 00:24:08 +0900

    add ios

---
+++ cpp/ios/ios/src/ios/ios.cpp
@@ -0,0 +1,30 @@
+// ヘッダのインクルード
+#include <iostream> // 標準入出力
+
+// main関数
+int main(){
+
+  // 入力フォーム
+  std::cout << "s: "; // 出力演算子でstd::coutにsの入力フォームを出力.
+
+  // 文字配列の初期化
+  char s[] = "ABCDEFGHIJ"; // sを"ABCDEFGHIJ"という10文字の文字列で初期化.
+
+  // 指定された文字数ちょうどの読み込み.
+  std::cin.read(s, 5); // std::cin.readで5文字読み込んでsに格納.
+
+  // sをそのまま出力.
+  std::cout << "s = " << s << std::endl; // sを出力.
+
+  // std::cinはstd::iosの派生オブジェクト.
+  std::ios& acin = std::cin; // std::iosの参照acinにcinを参照渡しできる.
+
+  // acinが異常状態かどうかをチェック.
+  if (!acin){ // !acinが真.
+    std::cout << "!" << std::endl; // "!"を出力.
+  }
+
+  // プログラムの終了
+  return 0; // 0を返して正常終了.
+
+}
