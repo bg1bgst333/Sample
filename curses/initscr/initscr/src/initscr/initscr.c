91945a4743442b6c304adc6b77a364e9e2c309e8
B.G <bg1bgst333@gmail.com>
2019-05-28 07:19:41 +0900

    add initscr

---
+++ curses/initscr/initscr/src/initscr/initscr.c
@@ -0,0 +1,13 @@
+/* ヘッダファイルのインクルード */
+#include <curses.h> /* curses */
+
+/* main関数 */
+int main(void){
+
+  /* 画面の初期化 */
+  initscr(); /* initscrで画面の初期化. */
+
+  /* プログラムの終了 */
+  return 0; /* 0を返して正常終了. */
+
+}
