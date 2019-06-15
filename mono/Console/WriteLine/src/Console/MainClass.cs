7ffab99923f7ed195ac0fea55e98d767170d539c
B.G <bg1bgst333@gmail.com>
2019-06-15 18:15:05 +0900

    add Console.WriteLine

---
+++ mono/Console/WriteLine/src/Console/MainClass.cs
@@ -0,0 +1,17 @@
+// 名前空間の登録
+using System; // 共通データ型と基本クラス(System名前空間)
+
+// MainClassの定義
+class MainClass
+{
+
+    // Mainメソッドの定義
+    public static void Main(string[] args)
+    {
+
+        // "ABCDE"の出力.
+        Console.WriteLine("ABCDE"); // Console.WriteLineで"ABCDE"を出力.
+
+    }
+
+}
