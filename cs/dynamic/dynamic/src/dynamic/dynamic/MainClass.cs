99d02e6a0fcf481e2b56766df40e8485c9bc0bed
bg1bgst333 <bg1bgst333@gmail.com>
2019-06-16 11:53:00 +0900

    add dynamic

---
+++ cs/dynamic/dynamic/src/dynamic/dynamic/MainClass.cs
@@ -0,0 +1,30 @@
+﻿// 名前空間の登録
+using System;       // 共通データ型と基本クラス(System名前空間)
+
+// メインクラス
+class MainClass // MainClassクラスの定義
+{
+    // メインメソッド
+    static void Main()  // Mainメソッドの定義
+    {
+
+        // varによる型推論.
+        var x = 10; // xはint(System.Int32)型.
+        Console.WriteLine("x.GetType() = " + x.GetType());  // x.GetTypeで型を取得して出力.
+        //x = "ABC";  // 型が違うのでビルドエラー.
+
+        // dynamicによる動的型付け.
+        dynamic y = 100;    // dynamicなyで100で初期化.
+        Console.WriteLine("y.GetType() = " + y.GetType());  // y.GetTypeで型を取得して出力.
+        Console.WriteLine("y = " + y);  // yの出力.
+        y = "ABC";  // yに"ABC"を格納.
+        Console.WriteLine("y.GetType() = " + y.GetType());  // y.GetTypeで型を取得して出力.
+        Console.WriteLine("y = " + y);  // yの出力.
+        y = 1.23;   // yに1.23を格納.
+        Console.WriteLine("y.GetType() = " + y.GetType());  // y.GetTypeで型を取得して出力.
+        Console.WriteLine("y = " + y);  // yの出力.
+        return; // 終了.
+
+    }
+
+}
