92d93b7b1b4b435b34629f98e4443ae61839709f
bg1bgst333 <bg1bgst333@gmail.com>
2019-06-16 20:25:20 +0900

    add Application.Startup

---
+++ wpf/Application/Startup/src/Application_/Application_/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Application_
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // アプリ起動時にメッセージボックスを表示.
+            MessageBox.Show("Application_Startup"); // MessageBox.Showで"Application_Startup"を表示.
+
+            // シャットダウン
+            Shutdown(); // Shutdownでアプリを終了.
+        }
+    }
+}
