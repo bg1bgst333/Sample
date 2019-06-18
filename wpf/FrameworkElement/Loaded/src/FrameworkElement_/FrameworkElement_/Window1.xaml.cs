38a24d70e8be122514b26a04399d705e28d3ceb9
bg1bgst333 <bg1bgst333@gmail.com>
2019-06-18 09:51:02 +0900

    add FrameworkElement.Loaded

---
+++ wpf/FrameworkElement/Loaded/src/FrameworkElement_/FrameworkElement_/Window1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FrameworkElement_
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // ウィンドウロード時にメッセージボックスを表示.
+            MessageBox.Show("Window_Loaded");   // "Window_Loaded"を表示.
+        }
+    }
+}
