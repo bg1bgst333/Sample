adbbf8d244c78c8400a05a3b583165d961eaffcc
bg1bgst333 <bg1bgst333@gmail.com>
2019-06-18 09:07:46 +0900

    add Application.Exit

---
+++ wpf/Application/Exit/src/Application_/Application_/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;    // ファイル入出力とデータストリーム(System.IO名前空間)
+using System.Linq;
+using System.Windows;
+
+namespace Application_
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            // アプリ終了時にメッセージボックスを表示.
+            MessageBox.Show("Application_Exit");    // "Application_Exit"を表示.
+
+            // ファイルを生成.
+            StreamWriter sw = new StreamWriter("test.txt"); // "test.txt"を開く.
+            sw.Write("ABC");    // "ABC"を書き込む.
+            sw.Close(); // 閉じる.
+        }
+    }
+}
