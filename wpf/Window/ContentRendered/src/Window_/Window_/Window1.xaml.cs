bb6e159fd3a9bb403c3783d7e6133f21eb2bdff5
bg1bgst333 <bg1bgst333@gmail.com>
2019-06-19 09:36:27 +0900

    add Window.ContentRendered

---
+++ wpf/Window/ContentRendered/src/Window_/Window_/Window1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Window_
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            // コンテンツ描画後にメッセージボックスを表示.
+            MessageBox.Show("Window_ContentRendered");   // "Window_ContentRendered"を表示.
+        }
+    }
+}
