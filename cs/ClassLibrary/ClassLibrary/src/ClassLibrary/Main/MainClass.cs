455776634cf8664d4b709821e5ce98339f645e23
bg1bgst333 <bg1bgst333@gmail.com>
2019-06-21 16:37:52 +0900

    add ClassLibrary

---
+++ cs/ClassLibrary/ClassLibrary/src/ClassLibrary/Main/MainClass.cs
@@ -0,0 +1,24 @@
+// 名前空間の登録
+using System;   // 共通データ型と基本クラス(System名前空間)
+using BGST; // 独自ライブラリClassLibraryの名前空間BGST
+
+// メインクラス
+class MainClass // MainClassクラスの定義
+{
+    // メインメソッド
+    static void Main()  // Mainメソッドの定義
+    {
+
+        // ClassLibraryClassオブジェクトの作成.
+        ClassLibraryClass clc = new ClassLibraryClass();    // ClassLibraryClassオブジェクトclcの作成.
+        
+        // ライブラリへのアクセス.
+        int i = clc.number; // iにclc.numberを代入.
+        string str = clc.GetString();   // strにclc.GetStringを代入.
+
+        // 出力.
+        Console.WriteLine("i = {0}, str = {1}", i, str);    // iとstrを出力.
+
+    }
+
+}
