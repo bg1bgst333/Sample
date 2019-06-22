c05f322fcd93809dfd52ee0adecb13be07a43460
bg1bgst333 <bg1bgst333@gmail.com>
2019-06-22 23:31:17 +0900

    add FileStaticLink

---
+++ cs/FileStaticLink/FileStaticLink/src/FileStaticLink/Lib/ClassLibrary.cs
@@ -0,0 +1,32 @@
+// 名前空間BGST
+namespace BGST
+{
+
+    // ライブラリクラス
+    public class ClassLibraryClass // ClassLibraryClassの定義
+    {
+
+        // フィールド
+        public int number;  // int型のフィールドnumber.
+
+        // コンストラクタ
+        public ClassLibraryClass()
+        {
+
+            // メンバの初期化.
+            number = 10;    // numberを10で初期化.
+
+        }
+
+        // メソッド
+        public string GetString()   // string型の文字列を返すメソッドGetString.
+        {
+
+            // "ABC"を返す.
+            return "ABC";   // "ABC"を返す.
+
+        }
+
+    }
+
+}
