f8a94210ddeffbb563fb24066fcada61fc7cfb50
bg1bgst333 <bg1bgst333@gmail.com>
2019-06-25 01:55:29 +0900

    add Assembly.LoadFrom

---
+++ dotnet/Assembly/LoadFrom/src/Assembly_/Assembly_/MainClass.cs
@@ -0,0 +1,32 @@
+// 名前空間の登録
+using System;   // 共通データ型と基本クラス(System名前空間)
+using System.Reflection;    // リフレクション(System.Reflection名前空間)
+
+// メインクラス
+class MainClass // MainClassクラスの定義
+{
+
+    // メインメソッド
+    static void Main()  // Mainメソッドの定義
+    {
+
+        // ClassLibraryClass.dllのロード.
+        try
+        {
+
+            // "ClassLibrary.dll"のロード.
+            Assembly asm = Assembly.LoadFrom("ClassLibrary.dll");   // Assembly.LoadFromで"ClassLibrary.dll"をロード.
+            Console.WriteLine("asm = " + asm);  // asmを出力.
+
+        }
+        catch (Exception ex)    // 例外発生
+        {
+
+            // 例外出力
+            Console.WriteLine(ex.ToString());   // ex.ToStringを出力.
+        
+        }       
+
+    }
+
+}
