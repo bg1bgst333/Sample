4dc9730c822f80633a0737ed6edd277f10f1fa62
bg1bgst333 <bg1bgst333@gmail.com>
2019-07-07 08:34:54 +0900

    add ContentPropertyAttribute

---
+++ wpf/ContentPropertyAttribute/ContentPropertyAttribute/src/ContentPropertyAttribute_/ContentPropertyAttribute_/TestClass.cs
@@ -0,0 +1,46 @@
+﻿// 名前空間の登録
+using System.Collections.ObjectModel;   // コレクションオブジェクトモデルクラス(System.Collections.ObjectModel名前空間)
+using System.Windows.Markup;    // マークアップ(System.Windows.Markup名前空間)
+
+// 名前空間
+namespace BGST
+{
+
+    // テストクラス
+    [ContentProperty("TestCollection")]
+    public class TestClass  // TestClassの定義
+    {
+
+        // コンストラクタ
+        public TestClass()
+        {
+            // コレクションの初期化.
+            TestCollection = new TestClassCollection(); // TestClassCollectionを生成.
+        }
+
+        // 普通の文字列プロパティ.
+        private string testProperty;    // string型のフィールドtestProperty.
+        public string TestProperty
+        {
+            // 取得
+            get
+            {
+                return testProperty;    // testPropertyを返す.
+            }
+            // 設定
+            set
+            {
+                testProperty = value;   // testPropertyに代入.
+            }
+        }
+
+        // 自身のコレクションのプロパティ.
+        public TestClassCollection TestCollection
+        {
+            get;
+            set;
+        }
+
+    }
+
+}
---
+++ wpf/ContentPropertyAttribute/ContentPropertyAttribute/src/ContentPropertyAttribute_/ContentPropertyAttribute_/TestClassCollection.cs
@@ -0,0 +1,14 @@
+﻿// 名前空間の登録
+using System.Collections.ObjectModel;   // コレクションオブジェクトモデルクラス(System.Collections.ObjectModel名前空間)
+
+// 名前空間
+namespace BGST
+{
+
+    // テストクラスコレクションクラス
+    public class TestClassCollection : Collection<TestClass>  // TestClassCollectionの定義
+    {
+
+    }
+
+}
