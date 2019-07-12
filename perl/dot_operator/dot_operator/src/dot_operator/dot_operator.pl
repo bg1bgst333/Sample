b3ec7ad1516b819ca3f14cb03d269bd4731a43b9
B.G <bg1bgst333@gmail.com>
2019-07-12 09:26:28 +0900

    add dot_operator

---
+++ perl/dot_operator/dot_operator/src/dot_operator/dot_operator.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+# 文字列連結
+$str = "ABC" . "DEF";
+print "$str\n";
+
+# 文字列変数同士の連結
+$s1 = "AAA";
+$s2 = "BBB";
+print $s1.$s2."\n";
+
+# 数値は文字列に変換される.
+$a = 123;
+$s = "CCC";
+print $a.$s."\n";
+
+# 数値同士の連結
+$x = 12;
+$y = 34;
+print $x.$y."\n";
