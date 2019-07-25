036eec40a45b0915988dc6611695a66d9e9722be
B.G <bg1bgst333@gmail.com>
2019-07-26 08:59:00 +0900

    add defined

---
+++ perl/defined/defined/src/defined/defined.pl
@@ -0,0 +1,17 @@
+# 値が未定義の変数は偽(false)となる.
+if (defined $var1){
+  print "true\n";
+}
+else{
+  print "false\n";
+}
+
+# 値が定義済みの変数は真(true)となる.
+$var2 = 10; # $var2は10とする.
+if (defined $var2){
+  print "true\n";
+  print '$var2 = '.$var2."\n";
+}
+else{
+  print "false\n";
+}
