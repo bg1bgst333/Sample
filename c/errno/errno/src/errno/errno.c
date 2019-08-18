07c6fc69bff4e40739723a007fb719f19ab2fb45
B.G <bg1bgst333@gmail.com>
2019-08-18 19:40:08 +0900

    add errno

---
+++ c/errno/errno/src/errno/errno.c
@@ -0,0 +1,32 @@
+/* ヘッダファイルのインクルード */
+#include <stdio.h> /* 標準入出力 */
+#include <errno.h> /* errno */
+
+/* main関数 */
+int main(void){
+
+  /* 変数の宣言 */
+  FILE *fp; /* ファイルポインタfp */
+
+  /* errnoの出力 */
+  printf("errno = %d\n", errno); /* errnoの出力 */
+
+  /* 存在しないファイルを開く.(わざと失敗する.) */
+  fp = fopen("test.txt", "r"); /* fopenで"test.txt"を開く. */
+  if (fp == NULL){ /* NULLなら. */
+
+    /* errnoの出力 */
+    printf("errno = %d\n", errno); /* errnoの出力 */
+
+    /* 標準エラー出力にエラーメッセージを出力. */
+    perror("fopen error!"); /* perrorで"fopen error!"と出力.(その後, 詳細なエラーメッセージも付加される.) */
+
+    /* 異常終了 */
+    return -1; /* -1を返して異常終了. */
+
+  }
+
+  /* プログラムの終了 */
+  return 0; /* 0を返して正常終了. */
+
+}
