e54eda134fbbdfce7e1d6f5aa173423ac0166b1f
B.G <bg1bgst333@gmail.com>
2019-08-18 10:36:13 +0900

    add redirect#out

---
+++ c/redirect/out/src/redirect/src.c
@@ -0,0 +1,13 @@
+/* ヘッダファイルのインクルード */
+#include <stdio.h> /* 標準入出力 */
+
+/* main関数 */
+int main(void){
+
+  /* "ABCDE"の出力 */
+  printf("ABCDE\n"); /* printfで"ABCDE"の出力. */
+
+  /* プログラムの終了 */
+  return 0; /* 正常終了 */
+
+}
