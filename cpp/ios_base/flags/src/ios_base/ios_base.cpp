56d3751939e13881f184546786ea1fa65a5d9fdb
B.G <bg1bgst333@gmail.com>
2019-09-11 07:13:50 +0900

    add ios_base::flags

---
+++ cpp/ios_base/flags/src/ios_base/ios_base.cpp
@@ -0,0 +1,13 @@
+// ヘッダのインクルード
+#include <iostream> // 標準入出力
+
+// main関数
+int main(){
+
+  // 現在の書式フラグを取得する.
+  std::cout << "std::cout.flags() = " << std::cout.flags() << std::endl; // std::cout.flagsでstd::coutの現在の書式フラグを取得.
+
+  // プログラムの終了
+  return 0; // 0を返して正常終了.
+
+}
