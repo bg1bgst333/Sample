fdc267ce6b98ce9aa708b7fbe61ec1f04e2adf74
bg1bgst333 <bg1bgst333@gmail.com>
2019-10-01 08:53:16 +0900

    add FrameRgn

---
+++ winapi/FrameRgn/FrameRgn/src/FrameRgn/FrameRgn/FrameRgn.cpp
@@ -0,0 +1,163 @@
+// ヘッダファイルのインクルード
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ウィンドウメッセージに対して独自の処理をするように定義したコールバック関数WindowProc.
+ 
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd){
+ 
+	// 変数の宣言
+	HWND hWnd;			// CreateWindowで作成したウィンドウのウィンドウハンドルを格納するHWND型変数hWnd.
+	MSG msg;			// ウィンドウメッセージ情報を格納するMSG構造体型変数msg.
+	WNDCLASS wc;		// ウィンドウクラス情報をもつWNDCLASS構造体型変数wc.
+ 
+	// ウィンドウクラスの設定
+	wc.lpszClassName = _T("FrameRgn");						// ウィンドウクラス名は"FrameRgn".
+	wc.style = CS_HREDRAW | CS_VREDRAW;						// スタイルはCS_HREDRAW | CS_VREDRAW.
+	wc.lpfnWndProc = WindowProc;							// ウィンドウプロシージャは独自の処理を定義したWindowProc.
+	wc.hInstance = hInstance;								// インスタンスハンドルは_tWinMainの引数.
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);				// アイコンはアプリケーション既定のもの.
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);				// カーソルは矢印.
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	// 背景は白ブラシ.
+	wc.lpszMenuName = NULL;									// メニューはなし.
+	wc.cbClsExtra = 0;										// 0でいい.
+	wc.cbWndExtra = 0;										// 0でいい.
+ 
+	// ウィンドウクラスの登録
+	if (!RegisterClass(&wc)){	// RegisterClassでウィンドウクラスを登録し, 0が返ったらエラー.
+ 
+		// エラー処理
+		MessageBox(NULL, _T("RegisterClass failed!"), _T("FrameRgn"), MB_OK | MB_ICONHAND);	// MessageBoxで"RegisterClass failed!"とエラーメッセージを表示.
+		return -1;	// 異常終了(1)
+ 
+	}
+ 
+	// ウィンドウの作成
+	hWnd = CreateWindow(_T("FrameRgn"), _T("FrameRgn"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);	// CreateWindowで, 上で登録した"FrameRgn"ウィンドウクラスのウィンドウを作成.
+	if (hWnd == NULL){	// ウィンドウの作成に失敗したとき.
+ 
+		// エラー処理
+		MessageBox(NULL, _T("CreateWindow failed!"), _T("FrameRgn"), MB_OK | MB_ICONHAND);	// MessageBoxで"CreateWindow failed!"とエラーメッセージを表示.
+		return -2;	// 異常終了(2)
+ 
+	}
+ 
+	// ウィンドウの表示
+	ShowWindow(hWnd, SW_SHOW);	// ShowWindowでSW_SHOWを指定してウィンドウの表示.
+ 
+	// メッセージループ
+	while (GetMessage(&msg, NULL, 0, 0) > 0){	// GetMessageでメッセージを取得, 戻り値が0より大きい間はループし続ける.
+ 
+		// ウィンドウメッセージの送出
+		DispatchMessage(&msg);	// DispatchMessageで受け取ったメッセージをウィンドウプロシージャ(この場合は独自に定義したWindowProc)に送出.
+ 
+	}
+ 
+	// プログラムの終了
+	return (int)msg.wParam;	// 終了コード(msg.wParam)を戻り値として返す.
+ 
+}
+ 
+// WindowProc関数の定義
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){	// ウィンドウメッセージに対して独自の処理をするように定義したウィンドウプロシージャ.
+
+	// ウィンドウプロシージャ全体で使われるスタティック変数の宣言.
+	static HRGN hRgn;		// スタティックなリージョンのハンドルhRgn.
+	static HBRUSH hBrush;	// HBRUSH型スタティック変数hBrush.
+	static HBRUSH hFrameBrush;	// HBRUSH型スタティック変数hFrameBrush.
+
+	// ウィンドウメッセージに対する処理.
+	switch (uMsg){	// switch-casa文でuMsgの値ごとに処理を振り分ける.
+ 
+		// ウィンドウの作成が開始された時.
+		case WM_CREATE:		// ウィンドウの作成が開始された時.(uMsgがWM_CREATEの時.)
+ 
+			// WM_CREATEブロック
+			{
+
+				// リージョンの生成.
+				hRgn = CreateRectRgn(50, 50, 300, 200);	// CreateRectRgnで位置とサイズが(50, 50, 300, 200)の矩形リージョンを作成.
+
+				// 赤いブラシの作成.
+				hBrush = CreateSolidBrush(RGB(0xFF, 0x00, 0x00));	// 赤のブラシhBrush.
+
+				// 青いフレーム用ブラシの作成.
+				hFrameBrush = CreateSolidBrush(RGB(0x00, 0x00, 0xFF));	// 青のフレーム用ブラシhFrameBrush.
+
+				// ウィンドウ作成成功
+				return 0;	// return文で0を返して, ウィンドウ作成成功とする.
+ 
+			}
+ 
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+ 
+		// ウィンドウが破棄された時.
+		case WM_DESTROY:	// ウィンドウが破棄された時.(uMsgがWM_DESTROYの時.)
+ 
+			// WM_DESTROYブロック
+			{
+
+				// 青いフレーム用ブラシの破棄.
+				DeleteObject(hFrameBrush);	// 青いフレーム用ブラシhFrameBrushを削除.
+
+				// 赤いブラシの破棄.
+				DeleteObject(hBrush);	// 赤いブラシhBrushを削除.
+
+				// リージョンの削除
+				DeleteObject(hRgn);	// DeleteObjectでhRgnを削除.
+
+				// 終了メッセージの送信.
+				PostQuitMessage(0);	// PostQuitMessageで終了コードを0としてWM_QUITメッセージを送信.(するとメッセージループのGetMessageの戻り値が0になるので, メッセージループから抜ける.)
+ 
+			}
+ 
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 画面の描画を要求された時.
+		case WM_PAINT:		// 画面の描画を要求された時.(uMsgがWM_PAINTの時.)
+ 
+			// WM_PAINTブロック
+			{
+
+				// このブロックのローカル変数の宣言
+				HDC hDC;			// デバイスコンテキストハンドルを格納するHDC型変数hDC.
+				PAINTSTRUCT ps;		// ペイント情報を管理するPAINTSTRUCT構造体型の変数ps.
+				HBRUSH hOldBrush;	// SelectObjectされるまで使っていた古いブラシのハンドルhOldBrush.
+
+				// ウィンドウの描画開始
+				hDC = BeginPaint(hwnd, &ps);	// BeginPaintでこのウィンドウの描画の準備をする. 戻り値にはデバイスコンテキストハンドルが返るので, hDCに格納.
+				
+				// 赤いブラシを選択.
+				hOldBrush = (HBRUSH)SelectObject(hDC, hBrush);	// hBrushを選択.
+
+				// フレーム付きリージョンの描画.
+				//PaintRgn(hDC, hRgn);	// 赤いリージョンの描画.
+				FrameRgn(hDC, hRgn, hFrameBrush, 1, 3);	// 横幅1で縦幅3のフレーム付きリージョンの描画.
+
+				// 元のブラシに戻す.
+				SelectObject(hDC, hOldBrush);	// SelectObjectで元のブラシに戻す.
+
+				// ウィンドウの描画終了
+				EndPaint(hwnd, &ps);	// EndPaintでこのウィンドウの描画処理を終了する.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 上記以外の時.
+		default:	// 上記以外の値の時の既定処理.
+ 
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+ 
+	}
+ 
+	// あとは既定の処理に任せる.
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);	// 戻り値も含めDefWindowProcに既定の処理を任せる.
+
+}
