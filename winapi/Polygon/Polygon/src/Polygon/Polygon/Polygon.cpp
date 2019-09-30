cbc548027453dd30e20361f1e208840f84b5b30b
bg1bgst333 <bg1bgst333@gmail.com>
2019-10-01 00:59:55 +0900

    add Polygon

---
+++ winapi/Polygon/Polygon/src/Polygon/Polygon/Polygon.cpp
@@ -0,0 +1,174 @@
+// ヘッダファイルのインクルード
+// 既定のヘッダファイル
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ウィンドウメッセージに対して独自の処理をするように定義したコールバック関数WindowProc.
+
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd) {
+
+	// 変数の宣言
+	HWND hWnd;			// CreateWindowで作成したウィンドウのウィンドウハンドルを格納するHWND型変数hWnd.
+	MSG msg;			// ウィンドウメッセージ情報を格納するMSG構造体型変数msg.
+	WNDCLASS wc;		// ウィンドウクラス情報をもつWNDCLASS構造体型変数wc.
+
+	// ウィンドウクラスの設定
+	wc.lpszClassName = _T("Polygon");						// ウィンドウクラス名は"Polygon".
+	wc.style = CS_HREDRAW | CS_VREDRAW;						// スタイルはCS_HREDRAW | CS_VREDRAW.
+	wc.lpfnWndProc = WindowProc;							// ウィンドウプロシージャは独自の処理を定義したWindowProc.
+	wc.hInstance = hInstance;								// インスタンスハンドルは_tWinMainの引数.
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);				// アイコンはアプリケーション既定のもの.
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);				// カーソルは矢印.
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	// 背景は白ブラシ.
+	wc.lpszMenuName = NULL;									// メニューはなし.
+	wc.cbClsExtra = 0;										// 0でいい.
+	wc.cbWndExtra = 0;										// 0でいい.
+
+	// ウィンドウクラスの登録
+	if (!RegisterClass(&wc)) {	// RegisterClassでウィンドウクラスを登録し, 0が返ったらエラー.
+
+		// エラー処理
+		MessageBox(NULL, _T("RegisterClass failed!"), _T("Polygon"), MB_OK | MB_ICONHAND);	// MessageBoxで"RegisterClass failed!"とエラーメッセージを表示.
+		return -1;	// 異常終了(1)
+
+	}
+
+	// ウィンドウの作成
+	hWnd = CreateWindow(_T("Polygon"), _T("Polygon"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);	// CreateWindowで, 上で登録した"Polygon"ウィンドウクラスのウィンドウを作成.
+	if (hWnd == NULL) {	// ウィンドウの作成に失敗したとき.
+
+		// エラー処理
+		MessageBox(NULL, _T("CreateWindow failed!"), _T("Polygon"), MB_OK | MB_ICONHAND);	// MessageBoxで"CreateWindow failed!"とエラーメッセージを表示.
+		return -2;	// 異常終了(2)
+
+	}
+
+	// ウィンドウの表示
+	ShowWindow(hWnd, SW_SHOW);	// ShowWindowでSW_SHOWを指定してウィンドウの表示.
+
+	// メッセージループ
+	while (GetMessage(&msg, NULL, 0, 0) > 0) {	// GetMessageでメッセージを取得, 戻り値が0より大きい間はループし続ける.
+
+		// ウィンドウメッセージの送出
+		DispatchMessage(&msg);	// DispatchMessageで受け取ったメッセージをウィンドウプロシージャ(この場合は独自に定義したWindowProc)に送出.
+
+	}
+
+	// プログラムの終了
+	return (int)msg.wParam;	// 終了コード(msg.wParam)を戻り値として返す.
+
+}
+
+// WindowProc関数の定義
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {	// ウィンドウメッセージに対して独自の処理をするように定義したウィンドウプロシージャ.
+
+	// スタティック変数の宣言
+	static POINT pt[5];	// 多角形の描画に必要な次の頂点pt.(要素数5.)
+	static HPEN hPen;	// HPEN型スタティック変数hPen.
+	static HBRUSH hBrush;	// HBRUSH型スタティック変数hBrush.
+
+	// ウィンドウメッセージに対する処理.
+	switch (uMsg) {	// switch-casa文でuMsgの値ごとに処理を振り分ける.
+
+		// ウィンドウの作成が開始された時.
+		case WM_CREATE:		// ウィンドウの作成が開始された時.(uMsgがWM_CREATEの時.)
+
+			// WM_CREATEブロック
+			{
+
+				// 多角形の描画に必要な次の頂点の初期値をセット.
+				pt[0].x = 50;	// pt[0]のX座標は50.
+				pt[0].y = 50;	// pt[0]のY座標は50.
+				pt[1].x = 250;	// pt[1]のX座標は250.
+				pt[1].y = 20;	// pt[1]のY座標は20.
+				pt[2].x = 450;	// pt[2]のX座標は450.
+				pt[2].y = 40;	// pt[2]のY座標は40.
+				pt[3].x = 450;	// pt[3]のX座標は450.
+				pt[3].y = 300;	// pt[3]のY座標は300.
+				pt[4].x = 50;	// pt[4]のX座標は50.
+				pt[4].y = 300;	// pt[4]のY座標は300.
+
+				// 黒いペンと赤いブラシの作成.
+				hPen = CreatePen(PS_SOLID, 1, RGB(0x00, 0x00, 0x00));	// 黒いペンhPen.
+				hBrush = CreateSolidBrush(RGB(0xFF, 0x00, 0x00));	// 赤のブラシhBrush.
+
+				// ウィンドウ作成成功
+				return 0;	// return文で0を返して, ウィンドウ作成成功とする.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// ウィンドウが破棄された時.
+		case WM_DESTROY:	// ウィンドウが破棄された時.(uMsgがWM_DESTROYの時.)
+
+			// WM_DESTROYブロック
+			{
+
+				// 黒いペンと赤いブラシの破棄.
+				DeleteObject(hPen);	// 黒いペンhPenを削除.
+				DeleteObject(hBrush);	// 赤いブラシhBrushを削除.
+
+				// 終了メッセージの送信.
+				PostQuitMessage(0);	// PostQuitMessageで終了コードを0としてWM_QUITメッセージを送信.(するとメッセージループのGetMessageの戻り値が0になるので, メッセージループから抜ける.)
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 画面の描画を要求された時.
+		case WM_PAINT:		// 画面の描画を要求された時.(uMsgがWM_PAINTの時.)
+
+			// WM_PAINTブロック
+			{
+
+				// このブロックのローカル変数・配列の宣言と初期化.
+				HDC hDC;		// デバイスコンテキストハンドルを格納するHDC型変数hDC.
+				PAINTSTRUCT ps;	// ペイント情報を管理するPAINTSTRUCT構造体型の変数ps.
+				HPEN hOldPen;		// SelectObjectされるまで使っていた古いペンのハンドルhOldPen.
+				HBRUSH hOldBrush;	// SelectObjectされるまで使っていた古いブラシのハンドルhOldBrush.
+
+				// ウィンドウの描画開始
+				hDC = BeginPaint(hwnd, &ps);	// BeginPaintでこのウィンドウの描画の準備をする. 戻り値にはデバイスコンテキストハンドルが返るので, hDCに格納.
+
+				// 新規に作成した黒いペンと赤いブラシを選択.
+				hOldPen = (HPEN)SelectObject(hDC, hPen);	// hPenを選択.
+				hOldBrush = (HBRUSH)SelectObject(hDC, hBrush);	// hBrushを選択.
+
+				// 多角形の描画
+				Polygon(hDC, pt, 5);	// Polygonで閉じた多角形を描画.
+
+				// 元に戻す.
+				(HPEN)SelectObject(hDC, hOldPen);	// hOldPenを選択.
+				(HBRUSH)SelectObject(hDC, hOldBrush);	// hOldBrushを選択.
+
+				// ウィンドウの描画終了
+				EndPaint(hwnd, &ps);	// EndPaintでこのウィンドウの描画処理を終了する.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりした時.
+		case WM_COMMAND:	// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりした時.(uMsgがWM_COMMANDの時.)
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 上記以外の時.
+		default:	// 上記以外の値の時の既定処理.
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+	}
+
+	// あとは既定の処理に任せる.
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);	// 戻り値も含めDefWindowProcに既定の処理を任せる.
+
+}
