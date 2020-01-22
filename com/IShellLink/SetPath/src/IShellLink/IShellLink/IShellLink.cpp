f77618b82957d149bb6934cee0fd43ddd73a42f2
bg1bgst333 <bg1bgst333@gmail.com>
2020-01-22 13:03:58 +0900

    add IShellLink::SetPath

---
+++ com/IShellLink/SetPath/src/IShellLink/IShellLink/IShellLink.cpp
@@ -0,0 +1,60 @@
+// ヘッダファイルのインクルード
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR対応
+#include <stdio.h>		// C標準入出力
+#include <string.h>		// C文字列処理
+#include <shlwapi.h>	// シェルAPI
+#include <shlguid.h>	// シェルGUID
+#include <shobjidl.h>	// シェルオブジェクトIDL
+
+// _tmain関数の定義
+int _tmain(int argc, TCHAR *argv[]){	// main関数のTCHAR版.
+
+	// COMの初期化.
+	CoInitialize(NULL);	// CoInitializeでCOMを初期化.
+
+	// ポインタの初期化.
+	IShellLink *pShellLink = NULL;	// IShellLinkインターフェースポインタpShellLinkをNULLで初期化.
+	TCHAR tszSrcPath[MAX_PATH] = {0};	// tszSrcPath(長さMAX_PATH)を{0}で初期化.
+
+	// 実行ファイルがあるフォルダのパスを取得.
+	GetModuleFileName(NULL, tszSrcPath, MAX_PATH);	// GetModuleFileNameでこの実行ファイルのフルパスを取得.
+	PathRemoveFileSpec(tszSrcPath);	// PathRemoveFileSpecでファイル名の部分を取り除く.
+
+	// ファイルの実体は上記パスの下の"test.txt".
+	_tcscat(tszSrcPath, _T("\\test.txt"));	// 上記パスに"\test.txt"を連結.
+
+	// 実体パスを出力.
+	_tprintf(_T("tszSrcPath = %s\n"), tszSrcPath);	// tszSrcPathを出力.
+
+	// インスタンス作成.
+	HRESULT hr = CoCreateInstance(CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, IID_IShellLink, (void **)&pShellLink);	// CoCreateInstanceでIShellLinkのインスタンスポインタをpShellLinkに格納.
+	if (FAILED(hr)){	// 失敗.
+		_tprintf(_T("CoCreateInstance failed!\n"));	// "CoCreateInstance failed!"と出力.
+		CoUninitialize();	// CoUninitializeで終了処理.
+		return 0;	// 0を返して終了.
+	}
+
+	// 成功.
+	_tprintf(_T("pShellLink success!, pShellLink = 0x%08x\n"), (unsigned int)pShellLink);	// "pShellLink success!"とともにpShellLinkの値を出力.
+
+	// ファイル実体のパスをセット.
+	pShellLink->SetPath(tszSrcPath);	// pShellLink->SetPathでtszSrcPathをセット.
+
+	// 確認のためパスを取得.
+	TCHAR tszPath[MAX_PATH] = {0};	// tszPath(長さMAX_PATH)を{0}で初期化.
+	pShellLink->GetPath(tszPath, MAX_PATH, NULL, SLGP_UNCPRIORITY);	// pShellLink->GetPathでパスをtszPathに格納.
+
+	// パスを出力.
+	_tprintf(_T("tszPath = %s\n"), tszPath);	// tszPathを出力.
+
+	// 解放.
+	pShellLink->Release();	// pShellLink->Releaseで解放.
+
+	// COMの終了処理.
+	CoUninitialize();	// CoUninitializeで終了処理.
+
+	// プログラムの終了.
+	return 0;	// 0を返して終了.
+
+}
