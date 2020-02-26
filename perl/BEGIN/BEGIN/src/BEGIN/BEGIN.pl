b8948925f0a6c970bdaa265eb48e3566306494ee
B.G <bg1bgst333@gmail.com>
2020-02-26 22:00:36 +0900

    add BEGIN

---
+++ perl/BEGIN/BEGIN/src/BEGIN/BEGIN.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+# CustomModuleをuseで読み込む.
+#use CustomModule; # useでもBEGINは実行される.
+require CustomModule; # requireでもBEGINは実行される.
---
+++ perl/BEGIN/BEGIN/src/BEGIN/CustomModule.pm
@@ -0,0 +1,9 @@
+# CustomModule
+package CustomModule;
+
+# BEGIN
+BEGIN{
+  print "This is BEGIN.\n"
+}
+
+1; # これがないとエラーメッセージ.
