1965769025d1a1dd229dafc2fd944e560f6ff6c7
B.G <bg1bgst333@gmail.com>
2020-03-27 12:39:29 +0900

    add IObservable_T

---
+++ rx/IObservable_T/IObservable_T/src/IObservable_T/IObservable_T/CustomObservable.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IObservable_T
+{
+    class CustomObservable : IObservable<string>
+    {
+        // オブザーバーリスト
+        private List<IObserver<string>> observers = new List<IObserver<string>>();  // IObserver<string>のList observersを生成.
+
+        public IDisposable Subscribe(IObserver<string> observer)
+        {
+            // オブザーバーの追加.
+            observers.Add(observer);    // observers.Addでobserverを追加.
+            return new UnobserverDisposable(observers, observer);   // UnobserverDisposableを返す.
+        }
+
+        // 持っているobserverを監視対象から外すUnobserverDisposable.
+        private class UnobserverDisposable : IDisposable
+        {
+            private List<IObserver<string>> observers = new List<IObserver<string>>();
+            private IObserver<string> observer;
+            public UnobserverDisposable(List<IObserver<string>> observers, IObserver<string> observer)
+            {
+                this.observers = observers;
+                this.observer = observer;
+            }
+            public void Dispose()   // 監視対象から外す.
+            {
+                if (observer != null)
+                {
+                    if (observers.IndexOf(observer) != -1)  // observerが含まれている場合.
+                    {
+                        observers.Remove(observer); // observers.Removeでobserverを削除.
+                    }
+                    observers = null;
+                    observer = null;
+                }
+                //throw new NotImplementedException();
+            }
+        }
+
+        // 独自の実行メソッドExecute
+        public void Execute(string value)
+        {
+            // valueが空ならエラー.
+            if (value == "")    // valueが空.
+            {
+                // すべてのオブザーバーでOnErrorを実行.
+                foreach (var o in observers)    // observers全てに対して.
+                {
+                    Exception e = new Exception("value is empty!"); // "value is empty!"というメッセージの例外を生成.
+                    o.OnError(e);    // o.OnError(e)を実行.
+                }
+                // オブザーバーをクリア.
+                observers.Clear();  // observers.Clearでクリア.
+            }
+            else    // valueに中身がある.
+            {
+                // オブザーバーのOnNextを実行.
+                foreach (var o in observers)    // observers全てに対して.
+                {
+                    o.OnNext(value);    // o.OnNext(value)を実行.
+                }
+            }
+        }
+
+        // 完了メソッドComplete
+        public void Complete()
+        {
+            // オブザーバーのOnCompletedを実行.
+            foreach (var o in observers)    // observers全てに対して.
+            {
+                o.OnCompleted();    // o.OnCompleted()を実行.
+            }
+            // オブザーバーをクリア.
+            observers.Clear();  // observers.Clearでクリア.
+        }
+    }
+}
---
+++ rx/IObservable_T/IObservable_T/src/IObservable_T/IObservable_T/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IObservable_T
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // オブザーバブルとオブザーバーの作成.
+        CustomObservable customObservable = new CustomObservable();
+        CustomObserver customObserver1 = new CustomObserver(1); // コンストラクタ引数に1を渡す.
+        CustomObserver customObserver2 = new CustomObserver(2); // コンストラクタ引数に2を渡す.
+        IDisposable id1;    // IDisposableのid1.
+        IDisposable id2;    // IDisposableのid2.
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // オブザーバー登録.
+            id1 = customObservable.Subscribe(customObserver1);    // customObservable.SubscribeでcustomObserver1を登録.
+            id2 = customObservable.Subscribe(customObserver2);    // customObservable.SubscribeでcustomObserver2を登録.
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // オブザーバブルの実行.
+            customObservable.Execute("ABCDE");  // customObservable.Executeで"ABCDE"を渡して実行.
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // エラーを起こす.
+            customObservable.Execute("");   // customObservable.Executeで""を渡してエラーを起こす.
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            // 完了.
+            customObservable.Complete();    // customObservable.Completeで完了を通知.
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            // id1(customObserver1)の監視は止める.
+            id1.Dispose();  // id1.Disposeで監視を止める.
+        }
+    }
+}
