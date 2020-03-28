acca0cdb29bb57b55b36d0e85439396d06d6016d
B.G <bg1bgst333@gmail.com>
2020-03-28 14:54:44 +0900

    add ObservableExtensions.Subscribe_source_onNext_onCompleted

---
+++ rx/ObservableExtensions/Subscribe_source_onNext_onCompleted/src/ObservableExtensions_/ObservableExtensions_/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableExtensions_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // observableの作成.
+            var observable = new CustomObservable();    // CustomObservableオブジェクトobservableの作成.
+
+            // オブザーバー登録.
+            var subscriber1 = observable.Subscribe(str => Console.WriteLine("OnNext[1]:" + str), () => Console.WriteLine("OnCompleted[1]"));  // strを出力するアクションを登録.
+            var subscriber2 = observable.Subscribe(str => Console.WriteLine("OnNext[2]:" + str), () => Console.WriteLine("OnCompleted[2]"));  // strを出力するアクションを登録.
+
+            // 実行.
+            observable.Execute("ABC");  // "ABC"を渡して実行.
+
+            // 完了.
+            observable.Complete();  // 完了を通知.
+
+        }
+    }
+}
