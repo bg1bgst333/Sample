afa3d5184693eecb9eebc1c3edb60e767fd39c91
B.G <bg1bgst333@gmail.com>
2020-03-28 15:12:25 +0900

    add ObservableExtensions.Subscribe_source_onNext_onError_onCompleted

---
+++ rx/ObservableExtensions/Subscribe_source_onNext_onError_onCompleted/src/ObservableExtensions_/ObservableExtensions_/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableExtensions_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // observableの作成.
+            var observable = new CustomObservable();    // CustomObservableオブジェクトobservableの作成.
+
+            // オブザーバー登録.
+            var subscriber1 = observable.Subscribe(str => Console.WriteLine("OnNext[1]:" + str), ex => Console.WriteLine("OnError[1]:" + ex.Message), () => Console.WriteLine("OnCompleted[1]"));  // strを出力するアクションを登録.
+
+            // エラーを起こす.
+            observable.Execute("");  // ""を渡してエラーを起こす.
+
+        }
+    }
+}
