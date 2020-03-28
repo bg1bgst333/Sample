7e75675db706b01aacd4bad584bd67422538a999
B.G <bg1bgst333@gmail.com>
2020-03-28 17:01:32 +0900

    add Subject_T

---
+++ rx/Subject_T/Subject_T/src/Subject_T/Subject_T/CustomObservable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subject_T
+{
+    // カスタムオブザーバブル
+    class CustomObservable : IObservable<string>
+    {
+        // サブジェクト
+        private Subject<string> subject = new Subject<string>();    // サブジェクトsubjectの初期化.
+
+        // 登録.
+        public IDisposable Subscribe(IObserver<string> observer)
+        {
+            return subject.Subscribe(observer); // subject.Subscribeの戻り値を返す.
+            //throw new NotImplementedException();
+        }
+
+        // 実行.
+        public void Execute(string value)
+        {
+            // valueが空ならエラー.
+            if (value == "")    // valueが空.
+            {
+                subject.OnError(new Exception("value is empty!"));  // "value is empty!"というメッセージの例外を生成.
+                subject = new Subject<string>();    // subjetのインスタンスを作り直し.
+            }
+            else    // そうでなければ実行.
+            {
+                subject.OnNext(value);  // subject.OnNext(value)で実行.
+            }
+        }
+
+        // 完了.
+        public void Complete()
+        {
+            subject.OnCompleted();  // subject.OnCompleted()で完了通知.
+        }
+    }
+}
---
+++ rx/Subject_T/Subject_T/src/Subject_T/Subject_T/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subject_T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // observableの作成.
+            var observable = new CustomObservable();    // CustomObservableオブジェクトobservableの作成.
+
+            // オブザーバー登録.
+            var subscriber1 = observable.Subscribe(str => Console.WriteLine("OnNext[1]:" + str), ex => Console.WriteLine("OnError[1]:" + ex.Message), () => Console.WriteLine("OnCompleted[1]"));  // strを出力するアクションを登録.
+            var subscriber2 = observable.Subscribe(str => Console.WriteLine("OnNext[2]:" + str), ex => Console.WriteLine("OnError[2]:" + ex.Message), () => Console.WriteLine("OnCompleted[2]"));  // strを出力するアクションを登録.
+
+            // 実行.
+            observable.Execute("ABC");  // "ABC"を渡して実行.
+        }
+    }
+}
