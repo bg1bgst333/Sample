7aabf3383b8386602ed77500ad3b4914028c97a4
B.G <bg1bgst333@gmail.com>
2020-04-20 09:25:09 +0900

    add tuple

---
+++ python/tuple/tuple/src/tuple/tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+lst = [10, 20, 30]
+
+print "lst = %s" % lst
+
+lst[1] = 15
+
+print "lst = %s" % lst
+
+tpl = (100, 200, 300)
+
+print tpl[0]
+print tpl[1]
+print tpl[2]
+
+#tpl[1] = 150
