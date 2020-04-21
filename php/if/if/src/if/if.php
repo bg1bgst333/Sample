c7e96fe7f7a7dbd7ca69999c6c959b8a392242fa
B.G <bg1bgst333@gmail.com>
2020-04-21 16:07:01 +0900

    add if

---
+++ php/if/if/src/if/if.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>echo</title>
+  </head>
+  <body>
+    <?php
+      $var = 1;
+    ?>
+    <?php
+      if ($var == 1){
+    ?>
+    One!
+    <?php
+      }
+    ?>
+  </body>
+</html>
