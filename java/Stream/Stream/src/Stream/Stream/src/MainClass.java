2c7a39186fbd5262cc9f74bbf9957a358b8fd382
bg1bgst333 <bg1bgst333@gmail.com>
2020-04-30 18:32:15 +0900

    add Stream

---
+++ java/Stream/Stream/src/Stream/Stream/src/MainClass.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Stream;
+
+// メインクラス
+public class MainClass{	// MainClassの定義
+
+	// Javaのエントリポイント
+	public static void main(String[] args){	// mainメソッドの定義
+
+		// ボディリストを生成し, Collectionオブジェクトに格納.
+		Collection<Body> bodies = new ArrayList<Body>();	// bodiesを生成.
+
+		// ボディの追加.
+		bodies.add(new Body("Taro", 30, 170, 60));		// Taroを追加.
+		bodies.add(new Body("Jiro", 28, 160, 68));		// Jiroを追加.
+		bodies.add(new Body("Saburo", 25, 180, 70));	// Saburoを追加.
+		bodies.add(new Body("Shiro", 19, 156, 50));		// Shiroを追加.
+		bodies.add(new Body("Goro", 17, 175, 65));		// Goroを追加.
+
+		// Streamを取得.
+		Stream<Body> sbody = bodies.stream();	// Stream<Body>インスタンスsbodyを取得.
+		Object[] list = sbody.filter(x -> x.getHeight() > 173).map(x -> x.getName()).toArray();	// x.getHeightで173を超える要素から, x.getNameで名前を取得し, 配列に変換.
+		for (Object s : list){	// listの分繰り返す.
+			System.out.println(s);	// sを出力.
+		}
+
+	}
+
+}
