90f4197387a5deac871d80e5078db08fc575052c
B.G <bg1bgst333@gmail.com>
2020-05-20 21:40:48 +0900

    add string

---
+++ php/string/string/src/string/string.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>string</title>
+  </head>
+  <body>
+    <?php
+    $str1 = 'aaa';
+    echo $str1;
+    echo '\n<br/>';
+    $str2 = "bbb";
+    echo "$str2\n<br/>";
+    $str3 = "a'b'c";
+    echo "$str3\n<br/>";
+    $str4 = 'A"B"C';
+    echo "$str4\n<br/>";
+    ?>
+  </body>
+</html>
