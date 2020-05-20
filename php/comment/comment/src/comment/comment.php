99c8c435c6bffac5a63d1a4c916229f83b25a442
B.G <bg1bgst333@gmail.com>
2020-05-20 20:53:18 +0900

    add comment

---
+++ php/comment/comment/src/comment/comment.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>comment</title>
+  </head>
+  <body>
+    <?php
+      # comment 1.
+      echo "AAA<br/>\n    "; # output "AAA".
+      // comment 2.
+      echo "BBB<br/>\n    "; // output "BBB".
+      /* comment 3.
+      multiple lines.
+      */
+      echo "CCC<br/>\n"; /* output
+                            "CCC". */
+    ?>
+  </body>
+</html>
