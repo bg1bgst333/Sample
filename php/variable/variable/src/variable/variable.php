b0e6b9246ccf40b1b27ddc483451a8e0e1cae5db
B.G <bg1bgst333@gmail.com>
2020-05-21 08:40:58 +0900

    add variable

---
+++ php/variable/variable/src/variable/variable.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>variable</title>
+  </head>
+  <body>
+    <?php
+      $ivar = 123;
+      echo "$ivar\n";
+      $fvar = 0.12;
+      echo "$fvar\n";
+      $str = "ABC";
+      echo "$str\n";
+      $文字列 = "あいうえお";
+      echo "$文字列\n";
+    ?>
+  </body>
+</html>
