ef9e7f6b678ec98b6d0f76fead93d48f9c264444
B.G <bg1bgst333@gmail.com>
2020-05-20 21:23:04 +0900

    add boolean

---
+++ php/boolean/boolean/src/boolean/boolean.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>boolean</title>
+  </head>
+  <body>
+    <?php
+      $blvalue1 = True;
+      if ($blvalue1){
+        echo "T1";
+      }
+      $blvalue2 = true;
+      if ($blvalue2){
+        echo "T2";
+      }
+      $blvalue3 = TRUE;
+      if ($blvalue3){
+        echo "T3";
+      }
+      $blvalue4 = falSE; 
+      if (!$blvalue4){
+        echo "F4";
+      }
+    ?>
+  </body>
+</html>
