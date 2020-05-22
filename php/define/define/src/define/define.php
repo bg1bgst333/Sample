a9a51a30e83cd9d8f7fbd80cd7ddaa3a1e7bfaaa
B.G <bg1bgst333@gmail.com>
2020-05-22 18:20:38 +0900

    add define

---
+++ php/define/define/src/define/define.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>define</title>
+  </head>
+  <body>
+    <?php
+      define('HOME_PATH', '/home/bg1');
+      echo HOME_PATH;
+      echo "\n";
+    ?>
+  </body>
+</html>
