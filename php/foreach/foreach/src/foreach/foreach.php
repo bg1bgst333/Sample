710b2065585edd49eae35519251273428958ef1d
B.G <bg1bgst333@gmail.com>
2020-05-22 19:05:38 +0900

    add foreach

---
+++ php/foreach/foreach/src/foreach/foreach.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>for</title>
+  </head>
+  <body>
+    <?php
+      $ary = array('aaa', 'bbb', 'ccc');
+      foreach ($ary as $str){
+        echo "$str<br />\n";
+      }
+    ?>
+  </body>
+</html>
