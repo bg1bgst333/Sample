31d5236da2511fdc63027771d20cf4386b3fc517
B.G <bg1bgst333@gmail.com>
2020-05-22 19:37:04 +0900

    add function#return

---
+++ php/function/return/src/function/return.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>function#return</title>
+  </head>
+  <body>
+    <?php
+      function func($a, $b){
+        return $a + $b;
+      }
+      $var = func(1, 2);
+      echo "$var\n";
+    ?>
+  </body>
+</html>
