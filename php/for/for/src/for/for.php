cc93b3a4d419fdda901c934838b379e48f3b7a91
B.G <bg1bgst333@gmail.com>
2020-05-22 18:58:07 +0900

    add for

---
+++ php/for/for/src/for/for.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>for</title>
+  </head>
+  <body>
+    <?php
+      $ary = array('aaa', 'bbb', 'ccc');
+      for ($i = 0; $i < 3; $i++){
+        echo "$ary[$i]<br />\n";
+      }
+    ?>
+  </body>
+</html>
