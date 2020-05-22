76dd29f069fd4adc275b3ae6337dbb7d4ec0e1a5
B.G <bg1bgst333@gmail.com>
2020-05-22 18:46:48 +0900

    add else

---
+++ php/else/else/src/else/else.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <title>else</title>
+  </head>
+  <body>
+    <?php
+      $var = 0;
+    ?>
+    <?php
+      if ($var == 1){
+    ?>
+    One!
+    <?php
+      }
+      else{
+    ?>
+    Not One!
+    <?php
+      }
+    ?>
+  </body>
+</html>
