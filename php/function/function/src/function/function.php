0a455504a435cb4be7615bf89a2af4353bf3a7c6
B.G <bg1bgst333@gmail.com>
2020-05-22 19:28:18 +0900

    add function

---
+++ php/function/function/src/function/function.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>function</title>
+  </head>
+  <body>
+    <?php
+      function func($a, $b){
+        $y = $a + $b;
+        echo "$y\n";
+      }
+      func(1, 2);
+    ?>
+  </body>
+</html>
