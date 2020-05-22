ba481f35881721bef62a0a1ecbd4ecd8c8e7d71e
B.G <bg1bgst333@gmail.com>
2020-05-22 18:53:30 +0900

    add elseif

---
+++ php/elseif/elseif/src/elseif/elseif.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>elseif</title>
+  </head>
+  <body>
+    <?php
+      $var = 3;
+    ?>
+    <?php
+      if ($var == 1){
+    ?>
+    One!
+    <?php
+      }
+      elseif($var == 2){
+    ?>
+    Two!
+    <?php
+      }
+      else{
+    ?>
+    Other!
+    <?php
+      }
+    ?>
+  </body>
+</html>
