14e2fb93fb2cc544accf4f73bb837df3c33c3bea
B.G <bg1bgst333@gmail.com>
2020-05-23 23:06:44 +0900

    add function#variable_functions

---
+++ php/function/variable_functions/src/function/variable_functions.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>variable_functions</title>
+  </head>
+  <body>
+    <?php
+      $var = "func";
+      $var();
+      function func(){
+        echo "ABC\n";
+      }
+    ?>
+  </body>
+</html>
