9c24ae137b86d8836e39d9ea4af48ac7800b1ac1
bg1bgst333 <bg1bgst333@gmail.com>
2020-07-29 14:34:34 +0900

    fixed SOCKADDR_IRDA

--- winapi/SOCKADDR_IRDA/SOCKADDR_IRDA/src/SOCKADDR_IRDA/SOCKADDR_IRDA/SOCKADDR_IRDA.cpp
+++ winapi/SOCKADDR_IRDA/SOCKADDR_IRDA/src/SOCKADDR_IRDA/SOCKADDR_IRDA/SOCKADDR_IRDA.cpp
@@ -1,5 +1,7 @@
 // ヘッダファイルのインクルード
 #include <tchar.h>		// TCHAR型
+#include <stdio.h>		// 標準入出力
+#include <string.h>		// 文字列処理
 #include <winsock2.h>	// Windowsソケット
 #include <windows.h>	// 標準WindowsAPI
 #include <af_irda.h>	// IrDA
@@ -19,6 +21,14 @@ int _tmain(int argc, TCHAR *argv[]){	// main関数のTCHAR版.
 	int acc_addr_len = sizeof(SOCKADDR_IRDA);	// 相手のアドレス情報の長さacc_addr_lenをSOCKADDR_IRDAのサイズで初期化.
 	TCHAR tszServerName[256] = {0};	// サーバー名tszServerNameを{0}で初期化.
 	int iServerNameLen = 0;	// サーバー名iServerNameLenを0で初期化.
+	BYTE btBytes[4096] = {0};	// BYTE配列btBytes(サイズ4096)を{0}で初期化.
+	int iRecvLen = 0;	// 受信したデータの長さiRecvLenを0で初期化.
+	int i = 0;	// ループ用変数iを0で初期化.
+	BYTE btResponseBytes[4096] = {0};	// レスポンスBYTE配列btResponseBytes(サイズ4096)を{0}で初期化.
+	WORD wPacketSize = 0;	// パケットサイズwPacketSizeを0で初期化.
+	TCHAR tszName[256] = {0};	// tszNameを{0}で初期化.
+	WORD wNameSize = 0;	// NameのサイズwNameSizeを0で初期化.
+	char szName[256] = {0};	// char型配列szName(長さ256)を{0}で初期化.
 
 	// WinSockの初期化.
 	iRet = WSAStartup(MAKEWORD(2,2), &wsaData);	// WSAStartupでWinSockの初期化.
@@ -59,6 +69,90 @@ int _tmain(int argc, TCHAR *argv[]){	// main関数のTCHAR版.
 						iServerNameLen = MultiByteToWideChar(CP_ACP, 0, acc_addr.irdaServiceName, -1, NULL, 0);	// MultiByteToWideCharで長さを取得.
 						MultiByteToWideChar(CP_ACP, 0, acc_addr.irdaServiceName, -1, tszServerName, iServerNameLen);	// MultiByteToWideCharで変換.
 						_tprintf(_T("acc_addr.irdaServiceName = %s\n"), tszServerName);	// tszServerNameを出力.
+
+						// データの受信
+						while (TRUE){
+							ZeroMemory(btBytes,  sizeof(btBytes));	// メモリクリア.
+							iRecvLen = recv(acc, (char *)btBytes, sizeof(btBytes), 0);	// recvでデータを受信し, btBytesに格納.
+							_tprintf(_T("btBytes = "));	// "btBytes = "と出力.
+							if (iRecvLen == 0){	// 受信出来なかった.
+								// 終了処理.
+								break;	// 抜ける.
+							}
+							else{	// 受信出来た.
+								for (i = 0; i < iRecvLen; i++){	// iRecvLen分繰り返す.
+									_tprintf(_T("%02X "), btBytes[i]);	// btBytesのi番目を出力.
+								}
+								_tprintf(_T("\n"));	// 改行を出力.
+								ZeroMemory(btResponseBytes,  sizeof(btResponseBytes));	// メモリクリア.
+								if (btBytes[0] == 0x80){	// 最初の接続リクエストが来たとき.
+									btResponseBytes[0] = 0xA0;	// 成功.
+									btResponseBytes[1] = 0x00;
+									btResponseBytes[2] = 0x07;	// サイズは7バイト.
+									btResponseBytes[3] = 0x10;	// OBEXバージョン1.0.
+									btResponseBytes[4] = 0x00;	// 接続フラグなし.
+									btResponseBytes[5] = 0x10;
+									btResponseBytes[6] = 0x00;	// 最大パケットサイズ4K
+									send(acc, (const char *)btResponseBytes, btResponseBytes[2], 0);	// sendでbtResponseBytesを送る.
+								}
+								else if (btBytes[0] == 0x82){	// 最後かつデータ書き込みされている.
+									wPacketSize = MAKEWORD(btBytes[2], btBytes[1]);	// wPacketSizeの取得.
+									_tprintf(_T("wPacketSize = %hu\n"), wPacketSize);	// wPacketSizeの出力.
+									int iIndex = 3;
+									// HIの取り出しループ.
+									while (TRUE){
+										// HIごとに分ける.
+										if (btBytes[iIndex] == 0x01){	// Name
+											int iTemp = iIndex;
+											wNameSize = MAKEWORD(btBytes[iIndex + 2], btBytes[iIndex + 1]);	// wNameSizeの取得.
+											int iTemp2 = wNameSize;
+											wNameSize = wNameSize - 3;	// HI for NameとName Lengthの3バイトを引かないといけない.
+											_tprintf(_T("wNameSize = %hu\n"), wNameSize);	// wNameSizeの出力.
+											memcpy(szName, (const char *)&btBytes[iIndex + 3], wNameSize);	// szNameにNameをコピー.
+											for (i = 0; i < (wNameSize - 1) / 2; i++){	// (wNameSize - 1) / 2の分繰り返す.
+												TCHAR t[2] = {0};	// tを{0}で初期化.
+												t[0] = MAKEWORD(szName[2 * i + 1], szName[2 * i]);	// ワイド文字tを生成.
+												t[1] = 0;	// t[1]は0.
+												_tcscat(tszName, t);	// tszNameにtを連結.
+											}
+											tszName[(wNameSize - 1) / 2] = 0;	// 最後に0をセット.
+											_tprintf(_T("tszName = %s\n"), tszName);
+											iTemp = iTemp + iTemp2;
+											iIndex = iTemp;
+										}
+										else if (btBytes[iIndex] == 0xC3){	// Length
+											int iTemp = iIndex;
+											WORD w1 = MAKEWORD(btBytes[iIndex + 2], btBytes[iIndex + 1]);
+											WORD w2 = MAKEWORD(btBytes[iIndex + 4], btBytes[iIndex + 3]);
+											DWORD dwLength = MAKELONG(w2, w1);
+											_tprintf(_T("dwLength = %08x\n"), dwLength);
+											iIndex = iTemp + 5;
+										}
+										else if (btBytes[iIndex] == 0x49){	// End-of-Body
+											WORD wSize = MAKEWORD(btBytes[iIndex + 2], btBytes[iIndex + 1]);
+											WORD wBodySize = wSize - 3;
+											_tprintf(_T("wBodySize = %08x\n"), wBodySize);
+											btResponseBytes[0] = 0xA0;	// 成功.
+											btResponseBytes[1] = 0x00;
+											btResponseBytes[2] = 0x03;	// サイズは3バイト.
+											send(acc, (const char *)btResponseBytes, 3, 0);	// 成功完了を送信.
+											FILE *fp = _tfopen(tszName, _T("wb"));	// tszNameで開く.
+											if (fp != NULL){	// fpがNULLでない.
+												int len = fwrite(&btBytes[iIndex + 3], sizeof(BYTE), wBodySize, fp);	// 書き込む.
+												fclose(fp);	// 閉じる.
+											}
+											break;
+										}
+									}
+								}
+								else{
+									break;
+								}
+							}
+						}
+
+						// 改行.
+						_tprintf(_T("\n"));	// 改行を出力.
 
 						// accを閉じる.
 						closesocket(acc);	// closesocketでaccを閉じる.
