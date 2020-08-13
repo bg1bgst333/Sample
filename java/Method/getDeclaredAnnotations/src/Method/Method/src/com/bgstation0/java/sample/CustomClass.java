9f82dc765319938c42484659b56e41b69cd8048e
bg1bgst333 <bg1bgst333@gmail.com>
2020-08-13 11:47:34 +0900

    add Method.getDeclaredAnnotations

---
+++ java/Method/getDeclaredAnnotations/src/Method/Method/src/com/bgstation0/java/sample/CustomClass.java
@@ -0,0 +1,15 @@
+package com.bgstation0.java.sample;
+
+// カスタムクラス
+public class CustomClass {
+
+	// フィールドの定義
+	public int val1;	// publicなint型フィールドval1.
+
+	// メソッドの定義
+	@Deprecated	// 非推奨にしておく.
+	public void method1(){	// publicで引数も戻り値もないメソッドmethod1.
+
+	}
+
+}
