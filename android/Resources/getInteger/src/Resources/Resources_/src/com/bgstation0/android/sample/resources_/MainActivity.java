f0bed2c376ed446578765a36e70baaea054e4bfa
bg1bgst333 <bg1bgst333@gmail.com>
2020-09-07 13:15:40 +0900

    add Resources.getInteger

---
+++ android/Resources/getInteger/src/Resources/Resources_/src/com/bgstation0/android/sample/resources_/MainActivity.java
@@ -0,0 +1,31 @@
+package com.bgstation0.android.sample.resources_;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+// メインアクティビティクラスMainActivityの定義.
+public class MainActivity extends Activity {
+
+	// メンバフィールドの定義
+	static final String TAG = "MainActivity";	// TAGを"MainActivity"で初期化.
+	
+	// アクティビティ生成時
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	
+    	// 既定の処理
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // リソースから整数値の取得.
+        Resources res = getResources();	// resを取得.
+        int ten = res.getInteger(R.integer.ten);	// R.integer.tenからtenを取得.
+        Log.d(TAG, "ten = " + ten);	// tenを出力.
+        
+    }
+    
+}
