efcd0cf990d383228f1851bb9791b4a0155b422e
bg1bgst333 <bg1bgst333@gmail.com>
2020-09-18 14:51:05 +0900

    add FragmentManager.findFragmentById

---
+++ android/FragmentManager/findFragmentById/src/FragmentManager/FragmentManager_/src/com/bgstation0/android/sample/fragmentmanager_/Fragment1.java
@@ -0,0 +1,31 @@
+package com.bgstation0.android.sample.fragmentmanager_;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+
+    // メンバフィールド
+    static final String TAG = "Fragment1";	// TAGを"Fragment1"で初期化.
+	
+    // ビュー生成時
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+			
+        // ログを残す.
+        Log.d(TAG, "Fragment1.onCreateView");	// "Fragment1.onCreateView"とログに残す.
+			
+        // 既定の処理.
+        super.onCreateView(inflater, container, savedInstanceState);
+		
+        // Fragment1のEditTextを取得.
+        View view = inflater.inflate(R.layout.fragment1_main, null);	// inflater.inflateでR.layout.fragment1_mainからビューを作成.
+        return view;	// viewを返す.
+        
+    }
+    
+}
---
+++ android/FragmentManager/findFragmentById/src/FragmentManager/FragmentManager_/src/com/bgstation0/android/sample/fragmentmanager_/Fragment2.java
@@ -0,0 +1,31 @@
+package com.bgstation0.android.sample.fragmentmanager_;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment {
+
+    // メンバフィールド
+    static final String TAG = "Fragment2";	// TAGを"Fragment2"で初期化.
+	
+    // ビュー生成時
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+			
+        // ログを残す.
+        Log.d(TAG, "Fragment2.onCreateView");	// "Fragment2.onCreateView"とログに残す.
+			
+        // 既定の処理.
+        super.onCreateView(inflater, container, savedInstanceState);
+		
+        // Fragment2のEditTextを取得.
+        View view = inflater.inflate(R.layout.fragment2_main, null);	// inflater.inflateでR.layout.fragment2_mainからビューを作成.
+        return view;	// viewを返す.
+        
+    }
+    
+}
