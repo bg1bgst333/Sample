1e96e1369717ab4ed3feb306cc070c86301969a9
bg1bgst333 <bg1bgst333@gmail.com>
2020-10-23 16:00:27 +0900

    add Fragment.setRetainInstance

---
+++ android/Fragment/setRetainInstance/src/Fragment/Fragment_/src/com/bgstation0/android/sample/fragment_/MainActivity.java
@@ -0,0 +1,55 @@
+package com.bgstation0.android.sample.fragment_;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	// メンバフィールド.
+	Fragment mCurrentFragment = null;	// mCurrentFragmentをnullで初期化.
+		
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // savedInstanceStateのチェック.
+        if (savedInstanceState == null){	// nullの時.
+        	
+        	// FragmentManager, fragmentTransactionの取得.
+        	FragmentManager fragmentManager = getFragmentManager();	// fragmentManagerの取得.
+        	FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();	// fragmentTransactionの取得.
+        	Fragment1 fragment1 = new Fragment1();	// fragment1を生成.
+        	fragmentTransaction.add(R.id.framelayout1, fragment1);	// fragment1を追加.
+        	fragmentTransaction.commit();	// コミット.
+        	mCurrentFragment = fragment1;	// mCurrentFragmentにfragment1をセット.
+        	
+        }
+        
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+    	super.onSaveInstanceState(outState);
+    	Log.d("MainActivity", "onSaveInstanceState:mCurrentFragment = " + mCurrentFragment.toString());
+    	getFragmentManager().putFragment(outState, "savedFragment", mCurrentFragment);	// 保存.
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+    	super.onRestoreInstanceState(savedInstanceState);
+    	mCurrentFragment = getFragmentManager().getFragment(savedInstanceState, "savedFragment");	// 復元.
+    	Log.d("MainActivity", "onRestoreInstanceState:mCurrentFragment = " + mCurrentFragment.toString());
+    }
+    
+    public void func(){
+    	Log.d("MainActivity", "func");
+    }
+    
+}
