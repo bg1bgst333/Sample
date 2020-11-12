10d6cfc3931fdbddca6c90ddcc0d1c890cb3c93a
bg1bgst333 <bg1bgst333@gmail.com>
2020-11-12 11:23:19 +0900

    delete ListView.setOnItemClickListener

--- android/ListView/setOnItemClickListener/src/ListView/ListView_/gen/com/bgstation0/android/sample/listview_/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.bgstation0.android.sample.listview_;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
