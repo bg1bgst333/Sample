f7139f01e237041086f1bcb21de153547d5b4983
bg1bgst333 <bg1bgst333@gmail.com>
2020-11-17 14:00:04 +0900

    add DialogFragment.onCancel

---
+++ android/DialogFragment/onCancel/src/DialogFragment/DialogFragment_/src/com/bgstation0/android/sample/dialogfragment_/MainActivity.java
@@ -0,0 +1,41 @@
+package com.bgstation0.android.sample.dialogfragment_;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+//メインアクティビティ.
+public class MainActivity extends Activity implements OnClickListener{
+
+	// メンバフィールド.
+	private final int DIALOG_ID_1 = 1;	// DIALOG_ID_1を1にする.
+	
+	// アクティビティ生成時.
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+		// 既定の処理.
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+   
+		// button1を取得し, OnClickListenerとして自身をセット.
+		Button button1 = (Button)findViewById(R.id.button1);	// findViewByIdでR.id.button1を取得.
+		button1.setOnClickListener(this);	// button1.setOnClickListenerでthis(自身)をセット.
+	
+	}
+  
+    // クリック時.
+ 	@Override
+ 	public void onClick(View v) {
+ 		// TODO Auto-generated method stub
+ 		// カスタムダイアログの表示.
+ 		CustomDialogFragment customDialogFragment = new CustomDialogFragment();	// CustomDialogFragmentオブジェクトcustomDialogFragmentの生成.
+ 		customDialogFragment.show(getFragmentManager(), "CustomDialogFragment");	// customDialogFragment.showで表示.
+    }
+ 	
+}
