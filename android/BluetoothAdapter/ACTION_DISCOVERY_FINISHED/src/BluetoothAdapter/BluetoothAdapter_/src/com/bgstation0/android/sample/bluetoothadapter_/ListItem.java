47497ea95b46fe02f1822a1712d00bbc06f9afb2
bg1bgst333 <bg1bgst333@gmail.com>
2020-12-16 15:08:19 +0900

    add BluetoothAdapter.ACTION_DISCOVERY_FINISHED

---
+++ android/BluetoothAdapter/ACTION_DISCOVERY_FINISHED/src/BluetoothAdapter/BluetoothAdapter_/src/com/bgstation0/android/sample/bluetoothadapter_/ListItem.java
@@ -0,0 +1,7 @@
+package com.bgstation0.android.sample.bluetoothadapter_;
+
+//リストアイテムクラスListItem
+public class ListItem {	// ListItemの定義
+	public String name;		// Bluetooth機器名.
+	public String address;	// Bluetoothアドレス.
+}
