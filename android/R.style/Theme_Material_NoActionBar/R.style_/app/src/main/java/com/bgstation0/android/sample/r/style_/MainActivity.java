d371f7d89fa6cec30e6f44c5559b88e8dcbab06a
B.G <bg1bgst333@gmail.com>
2021-02-28 20:58:06 +0900

    add R.style.Theme_Material_NoActionBar

---
+++ android/R.style/Theme_Material_NoActionBar/R.style_/app/src/main/java/com/bgstation0/android/sample/r/style_/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bgstation0.android.sample.r.style_;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends /*AppCompatActivity*/ Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
