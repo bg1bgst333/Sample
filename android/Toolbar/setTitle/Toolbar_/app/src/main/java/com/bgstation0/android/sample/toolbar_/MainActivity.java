a28fac2b2351de2ef660cf7e3bdc30e0755e2da5
B.G <bg1bgst333@gmail.com>
2021-03-17 18:54:58 +0900

    add Toolbar.setTitle

---
+++ android/Toolbar/setTitle/Toolbar_/app/src/main/java/com/bgstation0/android/sample/toolbar_/MainActivity.java
@@ -0,0 +1,21 @@
+package com.bgstation0.android.sample.toolbar_;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends /*AppCompatActivity*/ Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ツールバーのセット.
+        Toolbar toolbar1 = findViewById(R.id.toolbar1); // toolbar1を取得.
+        toolbar1.setLogo(R.mipmap.logo1);   // logo1をセット.
+        toolbar1.setTitle(R.string.title_string);   // title_stringをセット.
+        setActionBar(toolbar1); // setActionBarでtoolbar1をセット.
+    }
+}
