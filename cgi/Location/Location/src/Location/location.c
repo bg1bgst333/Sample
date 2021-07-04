b991c699588018ee19e10c9343cff63f9e8579d6
B.G <bg1bgst333@gmail.com>
2021-07-05 05:43:42 +0900

    add Location

---
+++ cgi/Location/Location/src/Location/location.c
@@ -0,0 +1,14 @@
+/* ヘッダファイルのインクルード */
+#include <stdio.h> /* 標準入出力 */
+
+/* main関数の定義 */
+int main(void){
+
+  /* CGIとして文字列の出力. */
+  printf("Location: http://bgstation0.com/classic1/\n"); /* "Location: http://bgstation0.com/classic1/\n"を出力. */
+  printf("\n"); /* printfで改行を出力. */
+
+  /* プログラムの終了 */
+  return 0; /* 0を返して正常終了. */
+
+}
