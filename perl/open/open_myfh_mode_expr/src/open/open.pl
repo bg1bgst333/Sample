557c368cf7ed94259cb7b993781ff6820ea361ac
B.G <bg1bgst333@gmail.com>
2021-08-23 14:38:50 +0900

    fixed open_myfh_mode_expr

--- perl/open/open_myfh_mode_expr/src/open/open.pl
+++ perl/open/open_myfh_mode_expr/src/open/open.pl
@@ -3,7 +3,7 @@
 use strict; # 厳密な文法チェック.
 
 # $fhでファイルをオープン.
-my $ret = open(my $fh, ">", "test.txt"); # $fhは"test.txt"を指す.(Perl 5.6からは, openにundefな変数が渡されると無名型グロブを返す.)
+my $ret = open(my $fh, ">", "test.txt"); # $fhは"test.txt"を指す.(Perl 5.6からは, openにundefな変数が渡されると無名型グロブへのリファレンスを返す.)
 # 書き込む.
 if ($ret){ # trueの時.
   print $fh "ABCDE\n"; # $fhに"ABCDE\n"を書き込む.
