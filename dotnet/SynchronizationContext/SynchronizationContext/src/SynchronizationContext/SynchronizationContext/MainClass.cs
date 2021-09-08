60ece007193ef9758e8ac3e92862a7d9e89dee1f
bg1bgst333 <bg1bgst333@gmail.com>
2021-09-08 15:40:09 +0900

    add SynchronizationContext

---
+++ dotnet/SynchronizationContext/SynchronizationContext/src/SynchronizationContext/SynchronizationContext/MainClass.cs
@@ -0,0 +1,18 @@
+﻿﻿// 名前空間の登録
+using System;   // 共通データ型と基本クラス(System名前空間)
+using System.Threading; // マルチスレッド(System.Threading名前空間)
+
+// MainClassの定義
+class MainClass
+{
+
+    // Mainメソッドの定義
+    static void Main()
+    {
+
+        // 現在の同期コンテキストを出力.
+        Console.WriteLine("SynchronizationContext.Current = " + SynchronizationContext.Current);    // SynchronizationContext.Currentを出力.(コンソールアプリの場合, nullになっている.)
+
+    }
+
+}
