b598f3ba0da0ff8b9eb0dd1959e2b908a074532d
bg1bgst333 <bg1bgst333@gmail.com>
2021-09-09 15:11:17 +0900

    add SynchronizationContext.Current

---
+++ dotnet/SynchronizationContext/Current/src/SynchronizationContext_/SynchronizationContext_/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading; // マルチスレッド(System.Threading名前空間)
+using System.Windows.Forms;
+
+namespace SynchronizationContext_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // メッセージボックスで現在の同期コンテキストを表示.
+            MessageBox.Show("SynchronizationContext.Current = " + SynchronizationContext.Current);  // MessageBox.ShowでSynchronizationContext.Currentを表示.
+            InitializeComponent();
+        }
+    }
+}
