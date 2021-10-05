f03b2e9e4a3393b771a46fd89b56dd054c21f6da
bg1bgst333 <bg1bgst333@gmail.com>
2021-10-05 15:45:33 +0900

    add CancellationTokenSource.Token

---
+++ dotnet/CancellationTokenSource/Token/src/CancellationTokenSource_/CancellationTokenSource_/MainClass.cs
@@ -0,0 +1,24 @@
+﻿// 名前空間の登録
+using System;   // 共通データ型と基本クラス(System名前空間)
+using System.Threading; // マルチスレッド(System.Threading名前空間)
+
+// MainClassの定義
+class MainClass
+{
+
+    // Mainメソッドの定義
+    static void Main()
+    {
+
+        // CancellationTokenSourceの生成.
+        CancellationTokenSource cts = new CancellationTokenSource();    // CancellationTokenSourceオブジェクトを生成し, ctsに格納.
+
+        // ctsの出力.
+        Console.WriteLine("cts = " + cts);  // ctsの内容を出力.
+
+        // Tokenの出力.
+        Console.WriteLine("cts.Token = " + cts.Token); // cts.Tokenの内容を出力.
+
+    }
+
+}
