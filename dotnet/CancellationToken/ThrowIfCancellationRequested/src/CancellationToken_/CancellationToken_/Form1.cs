2d7cbcb5451c074da862049cea08157b11898691
bg1bgst333 <bg1bgst333@gmail.com>
2021-11-01 17:21:21 +0900

    add CancellationToken.ThrowIfCancellationRequested

---
+++ dotnet/CancellationToken/ThrowIfCancellationRequested/src/CancellationToken_/CancellationToken_/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace CancellationToken_
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource cts;    // CancellationTokenSourceオブジェクトcts.
+        private CancellationToken token;        // CancellationTokenオブジェクトtoken.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // CancellationTokenSourceの生成.
+            cts = new CancellationTokenSource();    // CancellationTokenSourceオブジェクトを生成し, ctsに格納.
+
+            // CancellationTokenの取得.
+            token = cts.Token;  // cts.Tokenをtokenに格納.
+
+            // タスクにtokenを渡して実行.
+            var t = Task.Factory.StartNew(() => // Task.Factory.StartNew(action, cancellationToken)で実行.
+            {
+                for (int i = 0; i < 10; i++)    // 10回繰り返す.
+                {
+                    token.ThrowIfCancellationRequested();   // token.ThrowIfCancellationRequestedでcts.Cancelが実行されたら例外投げる.
+                    Debug.WriteLine("Step..."); // "Step..."とデバッグ出力.
+                    Thread.Sleep(1000); // 1秒休止.
+                }
+            }, token);  // tokenも渡す.
+            t.ContinueWith((x) => // Task.ContinueWithでtの継続タスクを指定.
+            {
+                Debug.WriteLine("Finish!"); // "Finish!"とデバッグ出力.
+            });
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // キャンセル.
+            if (cts != null)    // ctsがnullでなければ.
+            {
+                cts.Cancel();   // cts.Cancelでキャンセル.
+            }
+        }
+    }
+}
