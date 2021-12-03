00ed4a782fddb411b3a64bd4b67962d6256b4ce5
bg1bgst333 <bg1bgst333@gmail.com>
2021-12-03 17:07:39 +0900

    add FormStartPosition.Manual

---
+++ dotnet/FormStartPosition/Manual/src/FormStartPosition_/FormStartPosition_/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace FormStartPosition_
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 form1 = new Form1();
+            form1.StartPosition = FormStartPosition.Manual;
+            Application.Run(form1);
+        }
+    }
+}
