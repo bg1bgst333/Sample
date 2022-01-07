8d7604a64765fa8c04e421b7cccc54c71bb490c1
bg1bgst333 <bg1bgst333@gmail.com>
2022-01-07 17:49:11 +0900

    add AutoImplementedProperties

---
+++ cs/AutoImplementedProperties/AutoImplementedProperties/src/AutoImplementedProperties/AutoImplementedProperties/Class1.cs
@@ -0,0 +1,23 @@
+﻿// 整数値と文字列を持つクラス
+class Class1    // Class1クラスの定義
+{
+    // プロパティの定義
+    public int I    // int型プロパティI
+    {
+        get;
+        set;
+    }
+    public string Str   // string型プロパティStr
+    {
+        get;
+        set;
+    }
+    // メソッドの定義
+    // 初期化
+    public Class1() // コンストラクタClass1()の定義
+    {
+        // 初期化.
+        I = 0;  // Iを0にセット.
+        Str = "-";  // strに"-"をセット.
+    }
+}
---
+++ cs/AutoImplementedProperties/AutoImplementedProperties/src/AutoImplementedProperties/AutoImplementedProperties/MainClass.cs
@@ -0,0 +1,22 @@
+﻿// メインクラス
+class MainClass // MainClassクラスの定義
+{
+
+    // メインメソッド
+    static void Main()  // Mainメソッドの定義
+    {
+
+        // Class1オブジェクトの生成.
+        Class1 c1 = new Class1();   // Class1オブジェクトc1の生成.
+        c1.I = -5;  // -5をセット.
+        System.Console.WriteLine("c1.I = " + c1.I); // c1.Iを出力.
+        c1.I = 10;  // 10をセット.
+        System.Console.WriteLine("c1.I = " + c1.I); // c1.Iを出力.
+        c1.Str = "";    // ""をセット.
+        System.Console.WriteLine("c1.Str = " + c1.Str); // c1.Strを出力.
+        c1.Str = "ABC"; // "ABC"をセット.
+        System.Console.WriteLine("c1.Str = " + c1.Str); // c1.Strを出力.
+
+    }
+
+}
