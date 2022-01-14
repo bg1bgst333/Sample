3b895574021c1ca1e48f5f839da6c0c176053ea3
B.G <bg1bgst333@gmail.com>
2022-01-14 14:41:24 +0900

    add string_interpolation

---
+++ ruby/string_interpolation/string_interpolation/src/string_interpolation/string_interpolation.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+x = 10
+print("x = #{x}\n") # 整数型変数xの展開("x = 10"と出力.)
+
+str = "ABC"
+print("str = #{str}\n") # 文字列変数strの展開("str = ABC"と出力.)
+
+y = 20
+print("x + y = #{x + y}\n") # 式x + yの展開("x + y = 30"と出力.)
