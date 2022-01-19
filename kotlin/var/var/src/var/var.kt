50c8d533888239c9837fec56f5c908f900d3ade3
B.G <bg1bgst333@gmail.com>
2022-01-19 14:44:48 +0900

    add var

---
+++ kotlin/var/var/src/var/var.kt
@@ -0,0 +1,15 @@
+﻿fun main(args: Array<String>){
+  var a: Int
+  a = 10
+  println(a)
+  var b: Int = 20
+  println(b)
+  var c = 30
+  println(c)
+  a = 100
+  b = 200
+  c = 300
+  println(a)
+  println(b)
+  println(c)
+}
