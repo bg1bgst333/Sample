0313ed2ff2b03351666403de3c6c212df3ae4f76
bg1bgst333 <bg1bgst333@gmail.com>
2022-02-26 18:59:53 +0900

    add PageDirective#Inherits

---
+++ aspnet/PageDirective/Inherits/src/PageDirective/Inherits.aspx
@@ -0,0 +1,10 @@
+<%@ Page Language="C#" CodeFile="Inherits.cs" Inherits="Inherits2.Inherits" %>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title id="title_index">Inherits</title>
+  </head>
+  <body>
+    <% Response.Write(Method1()); %>
+  </body>
+</html>
---
+++ aspnet/PageDirective/Inherits/src/PageDirective/Inherits.cs
@@ -0,0 +1,20 @@
+namespace Inherits1
+{
+    public partial class Inherits : System.Web.UI.Page
+    {
+        public string Method1()
+        {
+            return "Inherits1.Inherits.Method1 Called!";
+        }
+    }
+}
+namespace Inherits2
+{
+    public partial class Inherits : System.Web.UI.Page
+    {
+        public string Method1()
+        {
+            return "Inherits2.Inherits.Method1 Called!";
+        }
+    }
+}
