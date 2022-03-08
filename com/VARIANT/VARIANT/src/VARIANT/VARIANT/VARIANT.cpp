12871811f7bb195b81aab08d555d1d3476f6ab6d
bg1bgst333 <bg1bgst333@gmail.com>
2022-03-09 07:09:45 +0900

    add VARIANT

---
+++ com/VARIANT/VARIANT/src/VARIANT/VARIANT/VARIANT.cpp
@@ -0,0 +1,15 @@
+// ヘッダファイルのインクルード
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR対応
+#include <stdio.h>		// C標準入出力
+
+// _tmain関数の定義
+int _tmain(int argc, TCHAR *argv[]){	// main関数のTCHAR版.
+
+	// VARIANT構造体の宣言
+	VARIANT vr;
+
+	// プログラムの終了.
+	return 0;	// 0を返して終了.
+
+}
