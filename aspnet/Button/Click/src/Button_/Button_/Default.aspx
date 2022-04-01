452ad061c75c65e19bc9e406ceacef55db84a770
bg1bgst333 <bg1bgst333@gmail.com>
2022-04-01 11:53:30 +0900

    add Button.Click

---
+++ aspnet/Button/Click/src/Button_/Button_/Default.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Button_._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+    <head runat="server">
+        <title>無題のページ</title>
+    </head>
+    <body>
+        <form id="form1" runat="server">
+            <asp:Button id="button1" Text="Button1" runat="server" />
+        </form>
+    </body>
+</html>
---
+++ aspnet/Button/Click/src/Button_/Button_/Default.aspx.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+namespace Button_
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            button1.Click += new EventHandler(button1_Click);
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            Title = "Clicked!";
+        }
+    }
+}
