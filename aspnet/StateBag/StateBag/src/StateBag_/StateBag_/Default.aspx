8996c15a80b980221355de798f21faa826afd769
bg1bgst333 <bg1bgst333@gmail.com>
2022-04-08 15:42:06 +0900

    add StateBag

---
+++ aspnet/StateBag/StateBag/src/StateBag_/StateBag_/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="StateBag_._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+    <head runat="server">
+        <title>無題のページ</title>
+    </head>
+    <script runat="server">
+    </script>
+    <body>
+        <form id="form1" runat="server">
+            <div>
+                <asp:Label id="label1" Text="Label1" runat="server" />
+            </div>
+            <div>
+                <asp:Button id="button1" Text="Button1" runat="server" />
+            </div>
+        </form>
+    </body>
+</html>
+
---
+++ aspnet/StateBag/StateBag/src/StateBag_/StateBag_/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("StateBag_")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StateBag_")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントには
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+[assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナ バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、下のように '*' を使ってリビジョンおよびビルド番号を
+// 既定値にすることができます:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
