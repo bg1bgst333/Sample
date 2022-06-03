fb4aa40608a49caa244e7d4d230dcb1386680dd8
bg1bgst333 <bg1bgst333@gmail.com>
2022-06-03 12:02:02 +0900

    add DateTime.Now

---
+++ dotnet/DateTime/Now/src/DateTime_/DateTime_/MainClass.cs
@@ -0,0 +1,18 @@
+// 名前空間の登録
+using System;   // 共通データ型と基本クラス(System名前空間)
+
+// MainClassの定義
+class MainClass
+{
+
+    // Mainメソッドの定義
+    static void Main()
+    {
+
+        // 現在日時の出力.
+        DateTime dateTime = DateTime.Now;   // DateTime.Nowで現在日時を取得し, dateTimeに格納.
+        Console.WriteLine("dateTime.ToString() = " + dateTime.ToString());  // dateTime.ToString()を出力.
+
+    }
+
+}
