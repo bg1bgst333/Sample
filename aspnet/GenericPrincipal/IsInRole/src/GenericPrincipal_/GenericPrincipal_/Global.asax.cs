0712ccc7ef51eda247f5de2eb9ea251c9b374293
bg1bgst333 <bg1bgst333@gmail.com>
2022-06-21 13:28:19 +0900

    add GenericPrincipal.IsInRole

---
+++ aspnet/GenericPrincipal/IsInRole/src/GenericPrincipal_/GenericPrincipal_/Global.asax.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace GenericPrincipal_
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            HttpCookie cookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (cookie == null)
+            {
+                return;
+            }
+            FormsAuthenticationTicket ticket = null;
+            try
+            {
+                ticket = FormsAuthentication.Decrypt(cookie.Value);
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+            if (ticket == null)
+            {
+                return;
+            }
+            string role = ticket.UserData;
+            FormsIdentity identity = new FormsIdentity(ticket);
+            GenericPrincipal principal = new GenericPrincipal(identity, new string[] { role });
+            Context.User = principal;
+        }
+    }
+}
---
+++ aspnet/GenericPrincipal/IsInRole/src/GenericPrincipal_/GenericPrincipal_/main.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="main.aspx.cs" Inherits="GenericPrincipal_.main" %>
+<%@ Import namespace="System.Security.Principal" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>無題のページ</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <%
+            FormsIdentity identity = (FormsIdentity)User.Identity;
+            Response.Write("identity.Name = " + identity.Name + "<br />");
+            GenericPrincipal principal = (GenericPrincipal)User;
+            if (principal.IsInRole("admin"))
+            {
+                Response.Write("principal.IsInRole(\"admin\")"); 
+            }
+            if (principal.IsInRole("users"))
+            {
+                Response.Write("principal.IsInRole(\"users\")");
+            }
+        %>
+    </div>
+    </form>
+</body>
+</html>
