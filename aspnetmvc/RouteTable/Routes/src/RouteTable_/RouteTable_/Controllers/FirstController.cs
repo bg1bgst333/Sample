c3dea1f2a2cdb4927b0aa418bdf1f189ebf5ef38
bg1bgst333 <bg1bgst333@gmail.com>
2022-06-24 11:14:55 +0900

    add RouteTable.Routes

---
+++ aspnetmvc/RouteTable/Routes/src/RouteTable_/RouteTable_/Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace RouteTable_.Controllers
+{
+    public class FirstController : Controller
+    {
+        //
+        // GET: /First/
+
+        public ActionResult Index()
+        {
+            ViewData["msg"] = "ABCDE";
+            return View();
+        }
+
+    }
+}
