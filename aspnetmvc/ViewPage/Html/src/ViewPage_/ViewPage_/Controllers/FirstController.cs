4c979ecb7ca32dd69a828218a802f549723a3f17
bg1bgst333 <bg1bgst333@gmail.com>
2022-06-27 12:53:01 +0900

    add ViewPage.Html

---
+++ aspnetmvc/ViewPage/Html/src/ViewPage_/ViewPage_/Controllers/FirstController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace ViewPage_.Controllers
+{
+    public class FirstController : Controller
+    {
+        //
+        // GET: /First/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
