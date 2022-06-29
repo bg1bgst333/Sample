09fc0fe454636736708d30fb2ca6552242e3f46d
bg1bgst333 <bg1bgst333@gmail.com>
2022-06-29 14:10:45 +0900

    add InputExtensions.TextBox

---
+++ aspnetmvc/InputExtensions/TextBox/src/InputExtensions_/InputExtensions_/Controllers/FirstController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace InputExtensions_.Controllers
+{
+    public class FirstController : Controller
+    {
+        //
+        // GET: /First/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
