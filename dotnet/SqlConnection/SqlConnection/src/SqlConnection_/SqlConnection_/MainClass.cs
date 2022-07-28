90f097f5cd3c4d65ac5bc51b311b87e878dd0a3e
bg1bgst333 <bg1bgst333@gmail.com>
2022-07-28 12:22:07 +0900

    add SqlConnection

---
+++ dotnet/SqlConnection/SqlConnection/src/SqlConnection_/SqlConnection_/MainClass.cs
@@ -0,0 +1,21 @@
+// 名前空間の登録
+using System;   // 共通データ型と基本クラス(System名前空間)
+using System.Data.SqlClient;    // SQLクライアント(System.Data.SqlClient名前空間)
+
+// メインクラス
+class MainClass // MainClassクラスの定義
+{
+
+    // メインメソッド
+    static void Main()  // Mainメソッドの定義
+    {
+
+        // SqlConnectionオブジェクトの作成.
+        SqlConnection sqlConn = new SqlConnection();    // SqlConnectionオブジェクトsqlConnを作成.
+
+        // sqlConnを出力.
+        Console.WriteLine("sqlConn = " + sqlConn);  // Console.WriteLineでsqlConnを出力.
+
+    }
+
+}
