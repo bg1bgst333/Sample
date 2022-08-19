20a1c8a23485baf0e9b3d7c60d044fa83605ac7c
bg1bgst333 <bg1bgst333@gmail.com>
2022-08-19 14:16:48 +0900

    add CWnd::GetDlgItem

---
+++ mfc/CWnd/GetDlgItem/src/CWnd/CWnd/CWndDlg.cpp
@@ -0,0 +1,98 @@
+// CWndDlg.cpp : 実装ファイル
+//
+
+#include "stdafx.h"
+#include "CWnd.h"
+#include "CWndDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CWndDlg ダイアログ
+
+
+
+
+CWndDlg::CWndDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CWndDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CWndDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CWndDlg, CDialog)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	//}}AFX_MSG_MAP
+	ON_BN_CLICKED(IDC_BUTTON1, &CWndDlg::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// CWndDlg メッセージ ハンドラ
+
+BOOL CWndDlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// このダイアログのアイコンを設定します。アプリケーションのメイン ウィンドウがダイアログでない場合、
+	//  Framework は、この設定を自動的に行います。
+	SetIcon(m_hIcon, TRUE);			// 大きいアイコンの設定
+	SetIcon(m_hIcon, FALSE);		// 小さいアイコンの設定
+
+	// TODO: 初期化をここに追加します。
+
+	return TRUE;  // フォーカスをコントロールに設定した場合を除き、TRUE を返します。
+}
+
+// ダイアログに最小化ボタンを追加する場合、アイコンを描画するための
+//  下のコードが必要です。ドキュメント/ビュー モデルを使う MFC アプリケーションの場合、
+//  これは、Framework によって自動的に設定されます。
+
+void CWndDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 描画のデバイス コンテキスト
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// クライアントの四角形領域内の中央
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// アイコンの描画
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialog::OnPaint();
+	}
+}
+
+// ユーザーが最小化したウィンドウをドラッグしているときに表示するカーソルを取得するために、
+//  システムがこの関数を呼び出します。
+HCURSOR CWndDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+void CWndDlg::OnBnClickedButton1()
+{
+	// TODO: ここにコントロール通知ハンドラ コードを追加します。
+	CWnd *pButton1 = GetDlgItem(IDC_BUTTON1);	// Cwnd::GetDlgItemでボタンIDC_BUTTON1のオブジェクトポインタpButton1を取得.
+	HWND hButton1 = ::GetDlgItem(m_hWnd, IDC_BUTTON1);	// Win32APIの::GetDlgItemでボタンIDC_BUTTON1のウィンドウハンドルhButton1を取得.
+	if (pButton1->m_hWnd == hButton1){	// 同じなら.
+		AfxMessageBox(_T("pButton1->m_hWnd == hButton1"));	// "pButton1->m_hWnd == hButton1"と表示.
+	}
+}
