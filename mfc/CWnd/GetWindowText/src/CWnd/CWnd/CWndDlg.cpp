d609e9ae9429f4ec7ee5d056d6a82aa89f23af2d
bg1bgst333 <bg1bgst333@gmail.com>
2022-08-24 12:17:56 +0900

    add CWnd::GetWindowText

---
+++ mfc/CWnd/GetWindowText/src/CWnd/CWnd/CWndDlg.cpp
@@ -0,0 +1,100 @@
+// CWndDlg.cpp : 実装ファイル
+//
+
+#include "stdafx.h"
+#include "CWnd.h"
+#include "CWndDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CWndDlg ダイアログ
+
+
+
+
+CWndDlg::CWndDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CWndDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CWndDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CWndDlg, CDialog)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	//}}AFX_MSG_MAP
+	ON_BN_CLICKED(IDC_BUTTON1, &CWndDlg::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// CWndDlg メッセージ ハンドラ
+
+BOOL CWndDlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// このダイアログのアイコンを設定します。アプリケーションのメイン ウィンドウがダイアログでない場合、
+	//  Framework は、この設定を自動的に行います。
+	SetIcon(m_hIcon, TRUE);			// 大きいアイコンの設定
+	SetIcon(m_hIcon, FALSE);		// 小さいアイコンの設定
+
+	// TODO: 初期化をここに追加します。
+
+	return TRUE;  // フォーカスをコントロールに設定した場合を除き、TRUE を返します。
+}
+
+// ダイアログに最小化ボタンを追加する場合、アイコンを描画するための
+//  下のコードが必要です。ドキュメント/ビュー モデルを使う MFC アプリケーションの場合、
+//  これは、Framework によって自動的に設定されます。
+
+void CWndDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 描画のデバイス コンテキスト
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// クライアントの四角形領域内の中央
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// アイコンの描画
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialog::OnPaint();
+	}
+}
+
+// ユーザーが最小化したウィンドウをドラッグしているときに表示するカーソルを取得するために、
+//  システムがこの関数を呼び出します。
+HCURSOR CWndDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+void CWndDlg::OnBnClickedButton1()
+{
+	// TODO: ここにコントロール通知ハンドラ コードを追加します。
+	CWnd *pEdit1 = GetDlgItem(IDC_EDIT1);	// CWnd::GetDlgItemでエディットコントロールIDC_EDIT1のオブジェクトポインタpEdit1を取得.
+	int iLen = pEdit1->GetWindowTextLength();	// CWnd::GetWindowTextLengthでテキストの長さを取得.
+	TCHAR *ptszBuf = new TCHAR[iLen + 1];	// TCHARバッファptszBuf(長さiLen + 1)を確保.
+	ZeroMemory(ptszBuf, sizeof(TCHAR) * (iLen + 1));	// ptszBufの初期化.
+	pEdit1->GetWindowText(ptszBuf, iLen + 1);	// CWnd::GetWindowTextでpEdit1のテキストを取得.
+	AfxMessageBox(ptszBuf);	// ptszBufを表示.
+	delete[] ptszBuf;	// delete[]でptszBufを解放.
+}
