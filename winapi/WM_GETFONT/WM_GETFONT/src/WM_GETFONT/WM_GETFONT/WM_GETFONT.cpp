4e5610436891e2d4ffc7816d3a685af66b65de5b
bg1bgst333 <bg1bgst333@gmail.com>
2022-12-07 09:11:11 +0900

    add WM_GETFONT

---
+++ winapi/WM_GETFONT/WM_GETFONT/src/WM_GETFONT/WM_GETFONT/WM_GETFONT.cpp
@@ -0,0 +1,150 @@
+// ヘッダファイルのインクルード
+// 既定のヘッダファイル
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+#include <stdio.h>		// C標準入出力
+#include <commctrl.h>	// コモンコントロール
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ウィンドウメッセージに対して独自の処理をするように定義したコールバック関数WindowProc.
+
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd){
+
+	// 変数・配列の宣言・初期化
+	HWND hWnd;					// CreateWindowで作成したウィンドウのウィンドウハンドルを格納するHWND型変数hWnd.
+	MSG msg;					// ウィンドウメッセージ情報を格納するMSG構造体型変数msg.
+	WNDCLASS wc;				// ウィンドウクラス情報をもつWNDCLASS構造体型変数wc.
+
+	// ウィンドウクラスの設定
+	wc.lpszClassName = _T("WM_GETFONT");						// ウィンドウクラス名は"WM_GETFONT".
+	wc.style = CS_HREDRAW | CS_VREDRAW;						// スタイルはCS_HREDRAW | CS_VREDRAW.
+	wc.lpfnWndProc = WindowProc;							// ウィンドウプロシージャは独自の処理を定義したWindowProc.
+	wc.hInstance = hInstance;								// インスタンスハンドルは_tWinMainの引数.
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);				// アイコンはアプリケーション既定のもの.
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);				// カーソルは矢印.
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	// 背景は白ブラシ.
+	wc.lpszMenuName = NULL;									// なし.
+	wc.cbClsExtra = 0;										// 0でいい.
+	wc.cbWndExtra = 0;										// 0でいい.
+
+	// ウィンドウクラスの登録
+	if (!RegisterClass(&wc)){	// RegisterClassでウィンドウクラスを登録し, 0が返ったらエラー.
+
+		// エラー処理
+		MessageBox(NULL, _T("RegisterClass Failure!"), _T("WM_GETFONT"), MB_OK | MB_ICONHAND);	// MessageBoxで"RegisterClass Failure!"とエラーメッセージを表示.
+		return -1;	// 異常終了(1)
+
+	}
+
+	// ウィンドウの作成
+	hWnd = CreateWindow(_T("WM_GETFONT"), _T("WM_GETFONT"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);	// CreateWindowで, 上で登録した"WM_GETFONT"ウィンドウクラスのウィンドウを作成.
+	if (hWnd == NULL){	// ウィンドウの作成に失敗したとき.
+
+		// エラー処理
+		MessageBox(NULL, _T("CreateWindow Failure!"), _T("WM_GETFONT"), MB_OK | MB_ICONHAND);	// MessageBoxで"CreateWindow Failure!"とエラーメッセージを表示.
+		return -2;	// 異常終了(2)
+
+	}
+
+	// ウィンドウハンドルの表示.
+	TCHAR tszHWnd[64] = {0};	// ウィンドウハンドル文字列tszHWnd(長さ64)を{0}で初期化.
+	_stprintf(tszHWnd, _T("hWnd = 0x%08x"), hWnd);	// hWndを文字列tszHWndに変換.
+	MessageBox(NULL, tszHWnd, _T("WM_GETFONT"), MB_OK);	// tszHWndを表示.
+
+	// ウィンドウの表示
+	ShowWindow(hWnd, SW_SHOW);	// ShowWindowでSW_SHOWを指定してウィンドウの表示.
+
+	// メッセージループ
+	while (GetMessage(&msg, NULL, 0, 0) > 0){	// GetMessageでメッセージを取得, 戻り値が0より大きい間はループし続ける.
+
+		// ウィンドウメッセージ処理
+		TranslateMessage(&msg);	// TranslateMessageで仮想キーメッセージを文字メッセージへ変換.
+		DispatchMessage(&msg);	// DispatchMessageで受け取ったメッセージをウィンドウプロシージャ(この場合は独自に定義したWindowProc)に送出.
+
+	}
+
+	// プログラムの終了
+	return (int)msg.wParam;	// 終了コード(msg.wParam)を戻り値として返す.
+
+}
+
+// WindowProc関数の定義
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){	// ウィンドウメッセージに対して独自の処理をするように定義したウィンドウプロシージャ.
+
+	// ウィンドウメッセージに対する処理.
+	switch (uMsg){	// switch-casa文でuMsgの値ごとに処理を振り分ける.
+
+		// ウィンドウの作成が開始された時.
+		case WM_CREATE:		// ウィンドウの作成が開始された時.(uMsgがWM_CREATEの時.)
+
+			// WM_CREATEブロック
+			{
+
+				// 変数の宣言
+				HWND hEdit;				// エディットコントロールのウィンドウハンドルhEdit.
+				LPCREATESTRUCT lpCS;	// CreateStruct構造体のポインタlpCS.
+
+				// hwndの表示.
+				TCHAR tszHWND[64] = {0};	// ウインドウハンドル文字列tszHWND(長さ64)を{0}で初期化.
+				_stprintf(tszHWND, _T("hwnd = 0x%08x"), hwnd);	// hwndを文字列tszHWNDに変換.
+				MessageBox(NULL, tszHWND, _T("WM_GETFONT"), MB_OK);	// tszHWNDを表示.
+
+				// アプリケーションインスタンスハンドルの取得.
+				lpCS = (LPCREATESTRUCT)lParam;	// lParamをLPCREATESTRUCTにキャストして, lpCSに格納.
+
+				// エディットコントロールの作成.
+				hEdit = CreateWindow(WC_EDIT, _T("Edit1"), WS_CHILD | WS_VISIBLE | WS_HSCROLL | WS_VSCROLL | ES_WANTRETURN | ES_MULTILINE | ES_AUTOHSCROLL | ES_AUTOVSCROLL, 50, 50, 100, 100, hwnd, (HMENU)(WM_APP + 1), lpCS->hInstance, NULL);	// CreateWindowのWC_EDITでエディットコントロール"Edit1"を作成.
+				if (hEdit == NULL){	// hEditがNULLなら.
+
+					// エラー処理.
+					return -1;	// -1を返す.
+
+				}
+
+				// エディットコントロールハンドルの表示.
+				TCHAR tszHEdit[64] = {0};	// エディットコントロールハンドル文字列tszHEdit(長さ64)を{0}で初期化.
+				_stprintf(tszHEdit, _T("hEdit = 0x%08x"), hEdit);	// hEditを文字列tszHEditに変換.
+				MessageBox(NULL, tszHEdit, _T("WM_GETFONT"), MB_OK);	// tszHEditを表示.
+
+				// 親ウィンドウハンドルの表示.
+				HWND hParent;	// hEditの親ウィンドウhParent.
+				hParent = GetParent(hEdit);	// GetParentでhEditの親ウィンドウhParent取得.
+				TCHAR tszHParent[64] = {0};	// 親ウィンドウハンドル文字列tszHParent(長さ64)を{0}で初期化.
+				_stprintf(tszHParent, _T("hParent = 0x%08x"), hParent);	// hParentを文字列tszHParentに変換.
+				MessageBox(NULL, tszHParent, _T("WM_GETFONT"), MB_OK);	// tszHParentを表示.
+
+				// ウィンドウ作成成功
+				return 0;	// return文で0を返して, ウィンドウ作成成功とする.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// ウィンドウが破棄された時.
+		case WM_DESTROY:	// ウィンドウが破棄された時.(uMsgがWM_DESTROYの時.)
+
+			// WM_DESTROYブロック
+			{
+
+				// 終了メッセージの送信.
+				PostQuitMessage(0);	// PostQuitMessageで終了コードを0としてWM_QUITメッセージを送信.(するとメッセージループのGetMessageの戻り値が0になるので, メッセージループから抜ける.)
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 上記以外の時.
+		default:	// 上記以外の値の時の既定処理.
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+	}
+
+	// あとは既定の処理に任せる.
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);	// 戻り値も含めDefWindowProcに既定の処理を任せる.
+
+}
