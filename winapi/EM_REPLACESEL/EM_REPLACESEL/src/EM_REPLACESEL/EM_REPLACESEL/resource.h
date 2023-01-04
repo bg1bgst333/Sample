88b6370ae6080dd71a31638adb5d238054a9dc47
bg1bgst333 <bg1bgst333@gmail.com>
2023-01-04 16:16:26 +0900

    add EM_REPLACESEL

---
+++ winapi/EM_REPLACESEL/EM_REPLACESEL/src/EM_REPLACESEL/EM_REPLACESEL/resource.h
@@ -0,0 +1,10 @@
+#define IDR_MAINMENU	101
+#define ID_ITEM_1_1	10101
+#define ID_ITEM_1_2	10102
+#define ID_ITEM_2_1	10201
+#define ID_ITEM_2_2	10202
+#define ID_ITEM_3_1	10301
+#define ID_ITEM_4_1	10401
+#define ID_ITEM_5_1	10501
+#define ID_ITEM_5_2	10502
+#define ID_ITEM_5_3	10503
