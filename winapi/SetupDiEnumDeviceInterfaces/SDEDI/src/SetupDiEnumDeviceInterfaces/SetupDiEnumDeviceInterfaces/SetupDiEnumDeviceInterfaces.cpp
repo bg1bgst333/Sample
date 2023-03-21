767cdccf5bb48edcc663221c673bd38e75c28c6b
bg1bgst333 <bg1bgst333@gmail.com>
2023-03-21 18:06:44 +0900

    add SetupDiEnumDeviceInterfaces

---
+++ winapi/SetupDiEnumDeviceInterfaces/SDEDI/src/SetupDiEnumDeviceInterfaces/SetupDiEnumDeviceInterfaces/SetupDiEnumDeviceInterfaces.cpp
@@ -0,0 +1,45 @@
+// ヘッダファイルのインクルード
+#include <tchar.h>		// TCHAR型
+#include <stdio.h>		// 標準入出力
+#include <windows.h>	// 標準WindowsAPI
+#include <setupapi.h>	// SetUpAPI
+
+// _tmain関数の定義
+int _tmain(int argc, TCHAR *argv[]){	// main関数のTCHAR版.
+
+	// 変数の宣言
+	HDEVINFO hDevInfo;	// デバイス情報ハンドルhDevInfo
+
+	// ボリュームデバイスのデバイスインターフェースクラスのデバイス情報を取得.
+	hDevInfo = SetupDiGetClassDevs(&GUID_DEVINTERFACE_VOLUME, NULL, NULL, DIGCF_PRESENT | DIGCF_INTERFACEDEVICE);	// SetupDiGetClassDevsでGUID_DEVINTERFACE_VOLUMEのhDevInfo取得.
+	if (hDevInfo != INVALID_HANDLE_VALUE){	// INVALID_HANDLE_VALUEでなければ.
+		
+		// 出力.
+		_tprintf(_T("hDevInfo = 0x%08x\n"), (unsigned long)hDevInfo);	// hDevInfoを出力.
+
+		// 列挙ループ.
+		int i = 0;	// インデックスiを0で初期化.
+		BOOL bLoop = TRUE;	// ループするかのbLoopをTRUEで初期化.
+		while (bLoop){	// bLoopがTRUEの間は続ける.
+
+			// インターフェースデバイスデータの取得.
+			SP_INTERFACE_DEVICE_DATA spidd = {0};	// SP_INTERFACE_DEVICE_DATAのspiddを{0}で初期化.
+			spidd.cbSize = sizeof(SP_INTERFACE_DEVICE_DATA);	// spidd.cbSizeにsizeofで測ったSP_INTERFACE_DEVICE_DATAのサイズをセット.
+			BOOL bRet = SetupDiEnumDeviceInterfaces(hDevInfo, NULL, &GUID_DEVINTERFACE_VOLUME, i, &spidd);	// SetupDiEnumDeviceInterfacesで列挙.
+			if (!bRet){	// falseなら
+				bLoop = FALSE;	// bLoopをFALSEに.
+			}
+			else{
+				_tprintf(_T("Found!\n"));	// "Found!"と出力.
+				i++;	// iをインクリメント.
+			}
+		}
+
+		// 破棄.
+		SetupDiDestroyDeviceInfoList(hDevInfo);	// SetupDiDestroyDeviceInfoListでhDevInfoを破棄.
+	}
+
+	// プログラムの終了.
+	return 0;	// 0を返して正常終了.
+
+}
