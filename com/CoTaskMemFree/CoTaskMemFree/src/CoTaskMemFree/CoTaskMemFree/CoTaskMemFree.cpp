f3b5bfc95a540454a1e185f4a7ee8806cecb6566
bg1bgst333 <bg1bgst333@gmail.com>
2023-04-06 17:42:17 +0900

    add CoTaskMemFree

---
+++ com/CoTaskMemFree/CoTaskMemFree/src/CoTaskMemFree/CoTaskMemFree/CoTaskMemFree.cpp
@@ -0,0 +1,24 @@
+// ヘッダファイルのインクルード
+#include <tchar.h>		// TCHAR型
+#include <stdio.h>		// 標準入出力
+#include <windows.h>	// 標準WindowsAPI
+
+// _tmain関数の定義
+int _tmain(int argc, TCHAR *argv[]){	// main関数のTCHAR版.
+
+	// ポインタの宣言.
+	WCHAR *pwszGuidStr1;	// GUID文字列を指すWCHARポインタpwszGuidStr1.
+
+	// ディスクデバイスインターフェースクラスGUIDを文字列に変換.
+	StringFromCLSID(GUID_DEVINTERFACE_DISK, &pwszGuidStr1);	// StringFromCLSIDでGUID_DEVINTERFACE_DISKをGUID文字列に変換.
+
+	// GUID文字列の出力.
+	_tprintf(_T("%s\n"), pwszGuidStr1);	// pwszGuidStr1を出力.
+
+	// pwszGuidStr1の解放.
+	CoTaskMemFree(pwszGuidStr1);	// CoTaskMemFreeで解放.
+
+	// プログラムの終了.
+	return 0;	// 0を返して正常終了.
+
+}
