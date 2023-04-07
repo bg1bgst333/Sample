a21976a41ded4b150e60f5d8db5506007dde0d24
bg1bgst333 <bg1bgst333@gmail.com>
2023-04-08 03:57:40 +0900

    add IsEqualGUID

---
+++ com/IsEqualGUID/IsEqualGUID/src/IsEqualGUID/IsEqualGUID/IsEqualGUID.cpp
@@ -0,0 +1,26 @@
+// ヘッダファイルのインクルード
+#include <tchar.h>		// TCHAR型
+#include <stdio.h>		// 標準入出力
+#include <windows.h>	// 標準WindowsAPI
+
+// _tmain関数の定義
+int _tmain(int argc, TCHAR *argv[]){	// main関数のTCHAR版.
+
+	// GUID文字列をGUIDに変換.
+	GUID guid1 = {0};	// GUID構造体guid1を{0}で初期化.
+	WCHAR wszBuf[39] = L"{53F5630D-B6BF-11D0-94F2-00A0C91EFB8B}";	// WCHAR配列wszBuf(長さ39)を"{53F5630D-B6BF-11D0-94F2-00A0C91EFB8B}"で初期化.
+	CLSIDFromString(wszBuf, &guid1);	// CLSIDFromStringで文字列からGUIDに変換.
+
+	// GUIDとGUIDが同じか判定.
+	int iRet = IsEqualGUID(guid1, GUID_DEVINTERFACE_VOLUME);	// IsEqualGUIDでGUID_DEVINTERFACE_VOLUMEと同じか判定.
+	if (iRet){	// TRUE
+		_tprintf(_T("guid1 is GUID_DEVINTERFACE_VOLUME.\n"));	// "guid1 is GUID_DEVINTERFACE_VOLUME."と出力.
+	}
+	else{
+		_tprintf(_T("guid1 is not GUID_DEVINTERFACE_VOLUME.\n"));	// "guid1 is not GUID_DEVINTERFACE_VOLUME."と出力.
+	}
+
+	// プログラムの終了.
+	return 0;	// 0を返して正常終了.
+
+}
