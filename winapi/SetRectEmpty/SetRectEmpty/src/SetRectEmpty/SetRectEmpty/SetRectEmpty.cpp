67c30f220cb1d3af1eb8ab027147839475d107c8
bg1bgst333 <bg1bgst333@gmail.com>
2023-06-18 05:18:05 +0900

    add SetRectEmpty

---
+++ winapi/SetRectEmpty/SetRectEmpty/src/SetRectEmpty/SetRectEmpty/SetRectEmpty.cpp
@@ -0,0 +1,185 @@
+// ヘッダファイルのインクルード
+// 既定のヘッダファイル
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+#include <stdio.h>		// C標準入出力
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ウィンドウメッセージに対して独自の処理をするように定義したコールバック関数WindowProc.
+
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd){
+
+	// 変数・配列の宣言・初期化
+	HWND hWnd;					// CreateWindowで作成したウィンドウのウィンドウハンドルを格納するHWND型変数hWnd.
+	MSG msg;					// ウィンドウメッセージ情報を格納するMSG構造体型変数msg.
+	WNDCLASS wc;				// ウィンドウクラス情報をもつWNDCLASS構造体型変数wc.
+
+	// ウィンドウクラスの設定
+	wc.lpszClassName = _T("SetRectEmpty");					// ウィンドウクラス名は"SetRectEmpty".
+	wc.style = CS_HREDRAW | CS_VREDRAW;						// スタイルはCS_HREDRAW | CS_VREDRAW.
+	wc.lpfnWndProc = WindowProc;							// ウィンドウプロシージャは独自の処理を定義したWindowProc.
+	wc.hInstance = hInstance;								// インスタンスハンドルは_tWinMainの引数.
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);				// アイコンはアプリケーション既定のもの.
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);				// カーソルは矢印.
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	// 背景は白ブラシ.
+	wc.lpszMenuName = NULL;									// なし
+	wc.cbClsExtra = 0;										// 0でいい.
+	wc.cbWndExtra = 0;										// 0でいい.
+
+	// ウィンドウクラスの登録
+	if (!RegisterClass(&wc)){	// RegisterClassでウィンドウクラスを登録し, 0が返ったらエラー.
+
+		// エラー処理
+		MessageBox(NULL, _T("RegisterClass Failure!"), _T("SetRectEmpty"), MB_OK | MB_ICONHAND);	// MessageBoxで"RegisterClass Failure!"とエラーメッセージを表示.
+		return -1;	// 異常終了(1)
+
+	}
+
+	// ウィンドウの作成
+	hWnd = CreateWindow(_T("SetRectEmpty"), _T("SetRectEmpty"), WS_OVERLAPPEDWINDOW, 100, 100, 640, 480, NULL, NULL, hInstance, NULL);	// CreateWindowで, 上で登録した"SetRectEmpty"ウィンドウクラスのウィンドウを作成.
+	if (hWnd == NULL){	// ウィンドウの作成に失敗したとき.
+
+		// エラー処理
+		MessageBox(NULL, _T("CreateWindow Failure!"), _T("SetRectEmpty"), MB_OK | MB_ICONHAND);	// MessageBoxで"CreateWindow Failure!"とエラーメッセージを表示.
+		return -2;	// 異常終了(2)
+
+	}
+				
+	// ウィンドウの表示
+	ShowWindow(hWnd, SW_SHOW);	// ShowWindowでSW_SHOWを指定してウィンドウの表示.
+
+	// メッセージループ
+	while (GetMessage(&msg, NULL, 0, 0) > 0){	// GetMessageでメッセージを取得, 戻り値が0より大きい間はループし続ける.
+
+		// ウィンドウメッセージ処理
+		TranslateMessage(&msg);	// TranslateMessageで仮想キーメッセージを文字メッセージへ変換.
+		DispatchMessage(&msg);	// DispatchMessageで受け取ったメッセージをウィンドウプロシージャ(この場合は独自に定義したWindowProc)に送出.
+
+	}
+
+	// プログラムの終了
+	return (int)msg.wParam;	// 終了コード(msg.wParam)を戻り値として返す.
+
+}
+
+// WindowProc関数の定義
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){	// ウィンドウメッセージに対して独自の処理をするように定義したウィンドウプロシージャ.
+
+	// ウィンドウメッセージに対する処理.
+	switch (uMsg){	// switch-casa文でuMsgの値ごとに処理を振り分ける.
+
+		// ウィンドウの作成が開始された時.
+		case WM_CREATE:		// ウィンドウの作成が開始された時.(uMsgがWM_CREATEの時.)
+ 
+			// WM_CREATEブロック
+			{
+
+				// 変数の宣言
+				LPCREATESTRUCT lpCS;	// CreateStruct構造体のポインタlpCS.
+
+				// アプリケーションインスタンスハンドルの取得.
+				lpCS = (LPCREATESTRUCT)lParam;	// lParamをLPCREATESTRUCTにキャストして, lpCSに格納.
+
+				// ウィンドウ作成成功
+				return 0;	// return文で0を返して, ウィンドウ作成成功とする.
+ 
+			}
+ 
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+ 
+		// ウィンドウが破棄された時.
+		case WM_DESTROY:	// ウィンドウが破棄された時.(uMsgがWM_DESTROYの時.)
+ 
+			// WM_DESTROYブロック
+			{
+ 
+				// 終了メッセージの送信.
+				PostQuitMessage(0);	// PostQuitMessageで終了コードを0としてWM_QUITメッセージを送信.(するとメッセージループのGetMessageの戻り値が0になるので, メッセージループから抜ける.)
+ 
+			}
+ 
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// ウィンドウの描画を要求された時.
+		case WM_PAINT:
+
+			// WM_PAINTブロック
+			{
+
+				// 描画開始.
+				PAINTSTRUCT ps = {0};	// PAINTSTRUCT構造体psを{0}で初期化.
+				HDC hDC = BeginPaint(hwnd, &ps);	// BeginPaintで描画開始し, hDC取得.
+				
+				// ウィンドウのRECTを取得.
+				RECT rc = {0};	// rcを{0}で初期化.
+				GetWindowRect(hwnd, &rc);	// GetWindowRectでウィンドウのRECT取得.
+				
+				// rcの描画.
+				TCHAR tszRect[256] = {0};	// rcを文字列にしたものtszRect(長さ256)を{0}で初期化.
+				_stprintf(tszRect, _T("rc.left = %d, rc.top = %d, rc.right = %d, rc.bottom = %d"), rc.left, rc.top, rc.right, rc.bottom);	// rcを文字列tszRectに変換.
+				TextOut(hDC, 50, 50, tszRect, (int)_tcslen(tszRect));	// TextOutでtszRectを描画.
+
+				// rcをデバッグ出力.
+				OutputDebugString(tszRect);	// OutputDebugStringでtszRectをデバッグ出力.
+				OutputDebugString(_T("\n"));	// OutputDebugStringで改行文字をデバッグ出力.
+
+				// 幅と高さの計算.
+				int width = rc.right - rc.left;		// 幅はright - left.
+				int height = rc.bottom - rc.top;	// 高さはbottom - top.
+				
+				// widthとheightの描画.
+				TCHAR tszWidthHeight[256] = {0};	// widthとheightを文字列にしたものtszWidthHeight(長さ256)を{0}で初期化.
+				_stprintf(tszWidthHeight, _T("width = %d, height = %d"), width, height);	// widthとheightを文字列tszWidthHeightに変換.
+				TextOut(hDC, 50, 150, tszWidthHeight, (int)_tcslen(tszWidthHeight));	// TextOutでtszWidthHeightを描画.
+
+				// rcを空にしてデバッグ出力.
+				SetRectEmpty(&rc);	// SetRectEmptyでrcを空にする.
+				_stprintf(tszRect, _T("rc.left = %d, rc.top = %d, rc.right = %d, rc.bottom = %d"), rc.left, rc.top, rc.right, rc.bottom);	// rcを文字列tszRectに変換.
+				OutputDebugString(tszRect);	// OutputDebugStringでtszRectをデバッグ出力.
+				OutputDebugString(_T("\n"));	// OutputDebugStringで改行文字をデバッグ出力.
+
+				// 描画終了.
+				EndPaint(hwnd, &ps);	// EndPaintで描画終了.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりした時.
+		case WM_COMMAND:	// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりした時.(uMsgがWM_COMMANDの時.)
+
+			// WM_COMMANDブロック
+			{
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// マウスの左ボタンが押された時.
+		case WM_LBUTTONDOWN:	// マウスの左ボタンが押された時.(uMsgがWM_LBUTTONDOWNの時.)
+
+			// WM_LBUTTONDOWNブロック
+			{
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// 上記以外の時.
+		default:	// 上記以外の値の時の既定処理.
+ 
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+ 
+	}
+ 
+	// あとは既定の処理に任せる.
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);	// 戻り値も含めDefWindowProcに既定の処理を任せる.
+
+}
