dd47fe1c64a522ee0e83d08f10ee94eb657aa815
B.G <bg1bgst333@gmail.com>
2023-07-17 08:30:32 +0900

    add len

---
+++ python/len/len/src/len/len.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# 要素数5のリスト.
+lst = [10, 20, 30, 40, 50] # 要素が10, 20, 30, 40, 50のリスト.
+print lst # lstを出力.
+
+# リストの長さ.
+lst_len = len(lst) # lenでlstの長さを取得し, lst_lenに格納.
+print lst_len # lst_lenを出力.
+
+# 要素数3のタプル.
+tpl = (100, 200, 300) # 要素が100, 200, 300のタプル.
+print tpl # tplを出力.
+
+# タプルの長さ.
+tpl_len = len(tpl) # lenでtplの長さを取得し, tpl_lenに格納.
+print tpl_len # tpl_lenを出力.
