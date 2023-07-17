3ec64efbf8f83197db4cc8b9c8af7f08b25327d2
B.G <bg1bgst333@gmail.com>
2023-07-17 09:03:38 +0900

    add tuple#len_1

---
+++ python/tuple/len_1/src/tuple/tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# 要素数が1のタプル.
+tpl1 = (100, ) # これだと要素(100)だけの要素数1のタプル.
+print tpl1 # tpl1を出力.
+print len(tpl1) # lenでtpl1の要素数を出力.
+
+# これだとタプルではなく数値.
+num1 = (100) # これだとタプルではなく数値の100.
+print num1 # num1を出力.
+print len(num1) # lenでnum1の要素数を出力.
