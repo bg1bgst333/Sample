90c163138d77ab08a73d39ad0f5cee9b97890741
B.G <bg1bgst333@gmail.com>
2023-07-19 12:22:22 +0900

    add def#return

---
+++ python/def/return/src/def/def.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# 加算関数add(x, y)の定義.
+def add(x, y):
+  result = x + y # xとyを加算し, resultに格納.
+  return result # returnでresultを返す.
+
+# 10 + 20.
+res = add(10, 20) # 関数addで10と20を加算, 結果をresに格納.
+print res # resを出力.
