5cfc7966b3bf98be7013f395ba4f30df0e78b4fb
B.G <bg1bgst333@gmail.com>
2023-07-19 11:16:29 +0900

    add Array.length

---
+++ ruby/Array/length/src/Array/Array.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+# 要素が[10, 20, 30]の配列.
+ary1 = [10, 20, 30] # 要素[10, 20, 30]の配列ary1.
+
+# 要素数の取得.
+len1 = ary1.length # ary1.lengthで要素数を取得し, len1に格納.
+
+# 要素数の出力.
+puts len1 # putsでlen1を出力.
