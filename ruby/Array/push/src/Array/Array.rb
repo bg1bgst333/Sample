e3950cdb7bb98a173b31fc1f921e68c3d36da692
B.G <bg1bgst333@gmail.com>
2023-07-19 13:39:35 +0900

    add Array#push

---
+++ ruby/Array/push/src/Array/Array.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+# 要素が[10, 20, 30]の配列.
+ary1 = [10, 20, 30] # 要素[10, 20, 30]の配列ary1.
+
+# ary1を出力.
+puts ary1 # putsでary1を出力.
+
+# 区切り線.
+puts '-----'
+
+# 要素40を追加.
+ary1.push 40 # ary1.pushで要素40を追加.
+
+# ary1を出力.
+puts ary1 # putsでary1を出力.
