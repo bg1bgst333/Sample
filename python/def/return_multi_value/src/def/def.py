05c2efdba930e8b1258836d1a9c8635d175a110e
B.G <bg1bgst333@gmail.com>
2023-07-20 14:29:40 +0900

    add def#return_multi_value

---
+++ python/def/return_multi_value/src/def/def.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# 関数ret_multi_valueの定義.
+def ret_multi_value():
+  return 1, 2 # 1と2を返す.
+
+# ret_multi_value()を呼び出す.
+x, y = ret_multi_value() # ret_multi_valueでx, yを返す.
+print x, y # xとyを出力.
