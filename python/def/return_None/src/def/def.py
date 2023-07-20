902cd9a0dccd3440a85789372aed4801e164e3bd
B.G <bg1bgst333@gmail.com>
2023-07-20 12:22:32 +0900

    add def#return_None

---
+++ python/def/return_None/src/def/def.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# 関数ret_noneの定義.
+def ret_none():
+  print "ret_none()" # "ret_none()"を出力.
+  # 値を返さずに終わる.
+
+# ret_none()を呼び出す.
+ret = ret_none(); # 戻り値はretに格納.
+print ret # retを出力.
