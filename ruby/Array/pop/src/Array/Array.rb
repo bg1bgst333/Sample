7dea431cdd086a9a81b0d4dde4d1e5b9f9df4165
B.G <bg1bgst333@gmail.com>
2023-07-20 12:01:07 +0900

    add Array.pop

---
+++ ruby/Array/pop/src/Array/Array.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+# 要素が[10, 20, 30]の配列.
+ary1 = [10, 20, 30] # 要素[10, 20, 30]の配列ary1.
+
+# ary1を出力.
+puts ary1 # putsでary1を出力.
+
+# 区切り線.
+puts '-----'
+
+# 末尾の要素を取り出し.
+e = ary1.pop # ary1.popで末尾の要素を取り出し, eに格納.
+
+# eを出力.
+puts e # putsでeを出力.
+
+# 区切り線.
+puts '-----'
+
+# ary1を出力.
+puts ary1 # putsでary1を出力.
