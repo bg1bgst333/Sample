08140deb1897237c522d2b3cc837d923480f60a2
B.G <bg1bgst333@gmail.com>
2023-07-20 14:12:06 +0900

    add Array.unshift

---
+++ ruby/Array/unshift/src/Array/Array.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+# 要素が[10, 20, 30]の配列.
+ary1 = [10, 20, 30] # 要素[10, 20, 30]の配列ary1.
+
+# ary1を出力.
+puts ary1 # putsでary1を出力.
+
+# 区切り線.
+puts '-----'
+
+# 要素0を先頭に挿入.
+ary1.unshift 0 # ary1.unshiftで要素0を先頭に挿入.
+
+# ary1を出力.
+puts ary1 # putsでary1を出力.
