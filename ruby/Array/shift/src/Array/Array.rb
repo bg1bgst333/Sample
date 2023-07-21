30c6d03a2eddac613e220babb368116b6b57e609
B.G <bg1bgst333@gmail.com>
2023-07-21 11:04:46 +0900

    add Array.shift

---
+++ ruby/Array/shift/src/Array/Array.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+# 要素が[10, 20, 30]の配列.
+ary1 = [10, 20, 30] # 要素[10, 20, 30]の配列ary1.
+
+# ary1を出力.
+puts ary1 # putsでary1を出力.
+
+# 区切り線.
+puts '-----'
+
+# 先頭の要素を取り出し.
+e = ary1.shift # ary1.shiftで先頭要素を取り出し, eに格納.
+
+# eを出力.
+puts e # putsでeを出力.
+
+# 区切り線.
+puts '-----'
+
+# ary1を出力.
+puts ary1 # putsでary1を出力.
