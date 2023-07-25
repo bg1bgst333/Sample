a8da167251d2fe57cb72bfae023e12a82e180fb1
B.G <bg1bgst333@gmail.com>
2023-07-26 08:37:22 +0900

    add Hash#length

---
+++ ruby/Hash/length/src/Hash/Hash.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+# キーが"a", "b", "c"となるハッシュ.
+hash1 = {"a" => "aaa", "b" => "bbb", "c" => "ccc"} # 値は"aaa", "bbb", "ccc".
+
+# 要素数の取得.
+hash1_len = hash1.length # hash1.lengthで要素数を取得し, hash1_lenに格納.
+
+# 要素数の出力.
+puts hash1_len # putsでhash1_lenを出力.
