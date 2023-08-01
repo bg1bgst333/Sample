27f38ce062a3a05b20598ad25fd818082b7643f2
B.G <bg1bgst333@gmail.com>
2023-08-01 13:13:16 +0900

    add Hash#keys

---
+++ ruby/Hash/keys/src/Hash/Hash.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+# キーが"a", "b", "c"となるハッシュ.
+hash1 = {"a" => "aaa", "b" => "bbb", "c" => "ccc"} # 値は"aaa", "bbb", "ccc".
+
+# キー配列の取得.
+hash1_keys = hash1.keys # hash1.keysでキー配列を取得し, hash1_keysに格納.
+
+# キー配列の出力.
+puts hash1_keys # putsでhash1_keysを出力.
