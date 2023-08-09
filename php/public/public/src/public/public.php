663682a23dfac29078c19af0287a75c4c5d35e03
B.G <bg1bgst333@gmail.com>
2023-08-09 10:35:33 +0900

    add public

---
+++ php/public/public/src/public/public.php
@@ -0,0 +1,150 @@
+<html>
+  <head>
+    <title>public</title>
+  </head>
+  <body>
+    <?php
+      class RootClass{
+        function func1(){
+          echo "[RootClass]";
+          echo "<br />";
+          echo $this->var1;
+          echo "<br />";
+          echo $this->private1; // 呼べるが表示されない.
+          echo "<br />";
+          //echo $this->privatefunc(); // 呼べるが以降実行されない.
+          //echo "<br />";
+          echo $this->protected1;
+          echo "<br />";
+          echo $this->protectedfunc();
+          echo "<br />";
+          echo $this->public1;
+          echo "<br />";
+          echo $this->publicfunc();
+          echo "<br />";
+        }
+      }
+      class BaseClass extends RootClass{
+        var $var1 = "var1";
+        private $private1 = "private1";
+        protected $protected1 = "protected1";
+        public $public1 = "public1";
+        function func2(){
+          echo "[BaseClass]";
+          echo "<br />";
+          echo $this->var1;
+          echo "<br />";
+          echo $this->private1;
+          echo "<br />";
+          echo $this->privatefunc();
+          echo "<br />";
+          echo $this->protected1;
+          echo "<br />";
+          echo $this->protectedfunc();
+          echo "<br />";
+          echo $this->public1;
+          echo "<br />";
+          echo $this->publicfunc();
+          echo "<br />";
+        }
+        private function privatefunc(){
+          echo "[BaseClass#privatefunc]";
+          echo "<br />";
+        }
+        protected function protectedfunc(){
+          echo "[BaseClass#protectedfunc]";
+          echo "<br />";
+        }
+        public function publicfunc(){
+          echo "[BaseClass#publicfunc]";
+          echo "<br />";
+        }
+      }
+      class DerivedClass extends BaseClass{
+        function func3(){
+          echo "[DerivedClass]";
+          echo "<br />";
+          echo $this->var1;
+          echo "<br />";
+          echo $this->private1; // 呼べるが表示されない.
+          echo "<br />";
+          //echo $this->privatefunc(); // 呼べるが以降実行されない.
+          //echo "<br />";
+          echo $this->protected1;
+          echo "<br />";
+          echo $this->protectedfunc();
+          echo "<br />";
+          echo $this->public1;
+          echo "<br />";
+          echo $this->publicfunc();
+          echo "<br />";
+        }
+      }
+      $d = new DerivedClass();
+      $d->func1();
+      $d->func2();
+      $d->func3();
+      echo '[$d->var1]';
+      echo "<br />";
+      echo $d->var1;
+      echo "<br />";
+      echo '[$d->private1]';
+      echo "<br />";
+      echo $d->private1; // 呼べるが表示されない.
+      echo "<br />";
+      $r = new RootClass();
+      echo '[$r->privatefunc()]';
+      echo "<br />";
+      //echo $r->privatefunc(); // 呼べるが以降実行されない.
+      echo "<br />";
+      $b = new BaseClass();
+      echo '[$b->privatefunc()]';
+      echo "<br />";
+      //echo $b->privatefunc(); // 呼べるが以降実行されない.
+      echo "<br />";
+      echo '[$d->privatefunc()]';
+      echo "<br />";
+      //echo $d->privatefunc(); // 呼べるが以降実行されない.
+      echo "<br />";
+      echo '[$d->protected1]';
+      echo "<br />";
+      //echo $d->protected1; // 呼べるが以降表示されない.
+      echo "<br />";
+      echo '[$r->protectedfunc()]';
+      echo "<br />";
+      //echo $r->protectedfunc(); // 呼べるが以降実行されない.
+      echo "<br />";
+      echo '[$b->protectedfunc()]';
+      echo "<br />";
+      //echo $b->protectedfunc(); // 呼べるが以降実行されない.
+      echo "<br />";
+      echo '[$d->protectedfunc()]';
+      echo "<br />";
+      //echo $d->protectedfunc(); // 呼べるが以降実行されない.
+      echo "<br />";
+      echo '[$d->public1]';
+      echo "<br />";
+      echo $d->public1;
+      echo "<br />";
+      echo '[$r->public1]';
+      echo "<br />";
+      echo $r->public1;
+      echo "<br />";
+      echo '[$b->public1]';
+      echo "<br />";
+      echo $b->public1;
+      echo "<br />";
+      echo '[$d->publicfunc()]';
+      echo "<br />";
+      echo $d->publicfunc();
+      echo "<br />";
+      echo '[$r->publicfunc()]';
+      echo "<br />";
+      //echo $r->publicfunc(); // 呼べるが以降実行されない.
+      echo "<br />";
+      echo '[$b->publicfunc()]';
+      echo "<br />";
+      echo $b->publicfunc();
+    ?>
+  </body>
+</html>
