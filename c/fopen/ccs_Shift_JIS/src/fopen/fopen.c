eebc19bf6714d321334269f27336367b59a64434
B.G <bg1bgst333@gmail.com>
2023-08-29 13:15:24 +0900

    add fopen#ccs#Shift_JIS

---
+++ c/fopen/ccs_Shift_JIS/src/fopen/fopen.c
@@ -0,0 +1,27 @@
+/* ヘッダファイルのインクルード */
+#include <stdio.h> /* 標準入出力 */
+#include <wchar.h> /* ワイド文字 */
+
+/* main関数の定義 */
+int main(void){
+
+  /* ファイルを開く. */
+  FILE *fp = fopen("test.txt", "w,ccs=Shift_JIS"); /* fopenで"test.txt"を開く.(ccsにShift_JISを指定.) */
+  if (fp != NULL){ /* 成功. */
+
+    /* ワイド文字で"あいうえお"を出力. */
+    fwprintf(fp, L"%ls", L"あいうえお"); /* fwprintfでL"あいうえお"を出力. */
+
+    /* 閉じる. */
+    fclose(fp); /* fcloseでfpを閉じる. */
+
+    /* プログラムの終了 */
+    return 0;
+
+  }
+
+  /* 異常終了 */
+  wprintf(L"error"); /* L"error"と出力. */
+  return -1; /* -1を返す. */
+
+}
