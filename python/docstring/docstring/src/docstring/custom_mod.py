ea88bcae54f815655e54f24601c8e79bc896f4dd
B.G <bg1bgst333@gmail.com>
2023-11-11 09:36:16 +0900

    add docstring

---
+++ python/docstring/docstring/src/docstring/custom_mod.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# coding: utf-8
+"""custom_mod"""
+
+# custom_func
+def custom_func(a, b):
+
+  # 引数同士の加算 
+  return a + b
---
+++ python/docstring/docstring/src/docstring/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# インポート
+import custom_mod
+
+# custom_funcを呼ぶ.
+num = custom_mod.custom_func(10, 20)
+
+# numの出力.
+print "num = %d" % num
