4b921c6085fc009ce6e5d2aab965826cb6a34cee
B.G <bg1bgst333@gmail.com>
2023-11-25 14:47:16 +0900

    add basic_string::at

---
+++ cpp/basic_string/at/src/basic_string/basic_string.cpp
@@ -0,0 +1,17 @@
+// ヘッダのインクルード
+#include <iostream> // 標準入出力
+#include <string> // C++文字列
+
+// main関数
+int main(){
+
+  // オブジェクトの宣言と初期化.
+  std::basic_string<char> str = "ABCDE"; // std::basic_string<char>オブジェクトstrを"ABCDE"で初期化.
+
+  // 3番目の文字を出力.
+  std::cout << str.at(3) << std::endl; // std::basic_string<char>::atで3番目の要素の参照を取得し, 出力.
+
+  // プログラムの終了
+  return 0; // 0を返して正常終了.
+
+}
