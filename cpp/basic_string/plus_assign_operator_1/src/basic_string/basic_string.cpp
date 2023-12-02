412f27f830a2ef95655ad7192c0b8090f35da0e6
B.G <bg1bgst333@gmail.com>
2023-12-02 10:08:28 +0900

    add basic_string#plus_assign_operator_1

---
+++ cpp/basic_string/plus_assign_operator_1/src/basic_string/basic_string.cpp
@@ -0,0 +1,21 @@
+// ヘッダのインクルード
+#include <iostream> // 標準入出力
+#include <string> // C++文字列
+
+// main関数
+int main(){
+
+  // オブジェクトの宣言と初期化.
+  std::basic_string<char> str1 = "ABC"; // std::basic_string<char>オブジェクトstr1を"ABC"で初期化.
+  char c = 'D'; // char型変数cを'D'で初期化.
+
+  // 指定のchar型変数の内容を連結する.
+  str1 += c; // str1にcを連結.
+
+  // str1を出力.
+  std::cout << str1 << std::endl; // str1を出力.
+
+  // プログラムの終了
+  return 0; // 0を返して正常終了.
+
+}
