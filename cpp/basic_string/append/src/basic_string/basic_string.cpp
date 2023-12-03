5438b5b18e64b67ad3763952e4ed560f3a341b60
B.G <bg1bgst333@gmail.com>
2023-12-03 17:18:13 +0900

    add basic_string::append

---
+++ cpp/basic_string/append/src/basic_string/basic_string.cpp
@@ -0,0 +1,21 @@
+// ヘッダのインクルード
+#include <iostream> // 標準入出力
+#include <string> // C++文字列
+
+// main関数
+int main(){
+
+  // オブジェクトの宣言と初期化.
+  std::basic_string<char> str1 = "ABC"; // std::basic_string<char>オブジェクトstr1を"ABC"で初期化.
+  std::basic_string<char> str2 = "DEF"; // std::basic_string<char>オブジェクトstr2を"DEF"で初期化.
+
+  // 指定のstd::basic_string<char>オブジェクトの内容を連結する.
+  str1.append(str2); // str1.appendでstr2を連結.
+
+  // str1を出力.
+  std::cout << str1 << std::endl; // str1を出力.
+
+  // プログラムの終了
+  return 0; // 0を返して正常終了.
+
+}
