e2482638429d912ba13623b4284b1eaa8d022fb3
B.G <bg1bgst333@gmail.com>
2023-12-06 16:04:42 +0900

    add basic_string::append_2

---
+++ cpp/basic_string/append_2/src/basic_string/basic_string.cpp
@@ -0,0 +1,20 @@
+// ヘッダのインクルード
+#include <iostream> // 標準入出力
+#include <string> // C++文字列
+
+// main関数
+int main(){
+
+  // オブジェクトの宣言と初期化.
+  std::basic_string<char> str1 = "ABC"; // std::basic_string<char>オブジェクトstr1を"ABC"で初期化.
+
+  // 指定のcharポインタ文字列の先頭からn文字を連結する.
+  str1.append("DEF", 2); // str1.appendで"DEF"のうち, 先頭から2文字を連結する.
+
+  // str1を出力.
+  std::cout << str1 << std::endl; // str1を出力.
+
+  // プログラムの終了
+  return 0; // 0を返して正常終了.
+
+}
