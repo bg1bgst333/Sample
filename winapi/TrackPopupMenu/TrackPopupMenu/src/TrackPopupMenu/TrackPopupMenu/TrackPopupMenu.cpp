cc128f37a78b7c70316684df4ce83aea995c6b48
bg1bgst333 <bg1bgst333@gmail.com>
2024-01-18 13:50:00 +0900

    add TrackPopupMenu

---
+++ winapi/TrackPopupMenu/TrackPopupMenu/src/TrackPopupMenu/TrackPopupMenu/TrackPopupMenu.cpp
@@ -0,0 +1,235 @@
+// ヘッダファイルのインクルード
+// 既定のヘッダファイル
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+
+// 独自のヘッダファイル
+#include "resource.h"	// リソースID
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ウィンドウメッセージに対して独自の処理をするように定義したコールバック関数WindowProc.
+
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd){
+
+	// 変数・配列の宣言・初期化
+	HWND hWnd;				// CreateWindowで作成したウィンドウのウィンドウハンドルを格納するHWND型変数hWnd.
+	MSG msg;				// ウィンドウメッセージ情報を格納するMSG構造体型変数msg.
+	WNDCLASS wc;			// ウィンドウクラス情報をもつWNDCLASS構造体型変数wc.
+	HDC hDC = NULL;			// このウィンドウのデバイスコンテキストハンドルhDCをNULLに初期化.
+
+	// ウィンドウクラスの設定
+	wc.lpszClassName = _T("TrackPopupMenu");				// ウィンドウクラス名は"TrackPopupMenu".
+	wc.style = CS_HREDRAW | CS_VREDRAW;						// スタイルはCS_HREDRAW | CS_VREDRAW.
+	wc.lpfnWndProc = WindowProc;							// ウィンドウプロシージャは独自の処理を定義したWindowProc.
+	wc.hInstance = hInstance;								// インスタンスハンドルは_tWinMainの引数.
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);				// アイコンはアプリケーション既定のもの.
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);				// カーソルは矢印.
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	// 背景は白ブラシ.
+	wc.lpszMenuName = NULL;									// NULLでいい.
+	wc.cbClsExtra = 0;										// 0でいい.
+	wc.cbWndExtra = 0;										// 0でいい.
+
+	// ウィンドウクラスの登録
+	if (!RegisterClass(&wc)){	// RegisterClassでウィンドウクラスを登録し, 0が返ったらエラー.
+
+		// エラー処理
+		MessageBox(NULL, _T("RegisterClass failed!"), _T("TrackPopupMenu"), MB_OK | MB_ICONHAND);	// MessageBoxで"RegisterClass failed!"とエラーメッセージを表示.
+		return -1;	// 異常終了(1)
+
+	}
+
+	// ウィンドウの作成
+	hWnd = CreateWindow(_T("TrackPopupMenu"), _T("TrackPopupMenu"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);	// CreateWindowで, 上で登録した"TrackPopupMenu"ウィンドウクラスのウィンドウを作成.(9番目にNULLを指定する.)
+	if (hWnd == NULL){	// ウィンドウの作成に失敗したとき.
+
+		// エラー処理
+		MessageBox(NULL, _T("CreateWindow failed!"), _T("TrackPopupMenu"), MB_OK | MB_ICONHAND);	// MessageBoxで"CreateWindow failed!"とエラーメッセージを表示.
+		return -2;	// 異常終了(2)
+
+	}
+
+	// ウィンドウの表示
+	ShowWindow(hWnd, SW_SHOW);	// ShowWindowでSW_SHOWを指定してウィンドウの表示.
+
+	// デバイスコンテキストの取得.
+	hDC = GetDC(hWnd);	// GetDCでデバイスコンテキストハンドルhDCを取得.
+
+	// PeekMessageによるメインループ.
+	while (TRUE){	// 常に真(TRUE)なので無限ループ.
+
+		// ウィンドウメッセージが来ているかを確認する.
+		if (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE)){	// PeekMessageでウィンドウメッセージが来ているかを確認し, 真なら来ている.(PM_NOREMOVEなのでメッセージキューからこのメッセージを削除しない.次のGetMessageがそのメッセージを処理する.)
+
+			// 来ていたらそのメッセージを取得.
+			if (GetMessage(&msg, NULL, 0, 0) > 0){	// GetMessageでPeekMessageで確認したメッセージを取得.
+
+				// ウィンドウメッセージの送出
+				TranslateMessage(&msg);	// TranslateMessageで仮想キーメッセージを文字メッセージへ変換.
+				DispatchMessage(&msg);	// DispatchMessageで受け取ったメッセージをウィンドウプロシージャ(この場合は独自に定義したWindowProc)に送出.
+
+			}
+			else{	// 正常終了(0), またはエラーによる異常終了(-1).
+
+				// メインループを抜ける.
+				break;	// breakでメインループを抜ける.
+
+			}
+
+		}
+		else{	// 偽ならウィンドウメッセージが来ていないとき.
+
+			// 何もしない.
+
+		}
+
+	}
+
+	// デバイスコンテキストの解放.
+	if (hDC != NULL){	// hDCが解放されていなければ.
+
+		// デバイスコンテキストを解放する.
+		ReleaseDC(hWnd, hDC);	// ReleaseDCでhDCを解放.
+		hDC = NULL;	// NULLをセット.
+
+	}
+
+	// プログラムの終了
+	return (int)msg.wParam;	// 終了コード(msg.wParam)を戻り値として返す.
+
+}
+
+// WindowProc関数の定義
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){	// ウィンドウメッセージに対して独自の処理をするように定義したウィンドウプロシージャ.
+
+	// ウィンドウプロシージャ全体で使われるスタティック変数の宣言.
+	static HINSTANCE hInstance;	// アプリケーションインスタンスハンドルhInstance.
+	static HMENU hContextMenu;	// HMENU型コンテキストメニューハンドルhContextMenu.
+	static HMENU hMenuR;	// HMENU型右クリックメニューhMenuR.
+
+	// ウィンドウメッセージに対する処理.
+	switch (uMsg){	// switch-casa文でuMsgの値ごとに処理を振り分ける.
+
+		// ウィンドウの作成が開始された時.
+		case WM_CREATE:		// ウィンドウの作成が開始された時.(uMsgがWM_CREATEの時.)
+
+			// WM_CREATEブロック
+			{
+
+				// 変数の宣言と初期化.
+				LPCREATESTRUCT lpCreateStruct;	// CREATESTRUCT構造体ポインタlpCreateStruct.
+				
+				// hInstanceの取得
+				lpCreateStruct = (LPCREATESTRUCT)lParam;	// lParamをLPCREATESTRUCTにキャストし, lpCreateStructに格納.
+				hInstance = lpCreateStruct->hInstance;	// lpCreateStruct->hInstanceをhInstanceに.
+				
+				// 右クリックメニューハンドルの取得.
+				hContextMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDR_POPUPMENU));	// LoadMenuでIDR_POPUPMENUをロード.
+				hMenuR = GetSubMenu(hContextMenu, 0);	// GetSubMenuで0番目を取得.
+				
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// ウィンドウが破棄された時.
+		case WM_DESTROY:	// ウィンドウが破棄された時.(uMsgがWM_DESTROYの時.)
+
+			// WM_DESTROYブロック
+			{
+
+				// 終了メッセージの送信.
+				PostQuitMessage(0);	// PostQuitMessageで終了コードを0としてWM_QUITメッセージを送信.(するとメッセージループのGetMessageの戻り値が0になるので, メッセージループから抜ける.)
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりした時.
+		case WM_COMMAND:	// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりした時.(uMsgがWM_COMMANDの時.)
+
+			// WM_COMMANDブロック
+			{
+
+				// どのメニュー項目が選択されたかを判定する.
+				switch (LOWORD(wParam)){	// LOWORD(wParam)で選択されたメニュー項目のIDが取得できるので, その値で判定する.
+
+					// 取得したIDごとに処理を分岐.
+					// Item1-1が選択された時.
+					case ID_ITEM_1_1:
+
+						// ID_ITEM_1_1ブロック
+						{
+
+						}
+
+						// 既定の処理へ向かう.
+						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+					// Item1-2が選択された時.
+					case ID_ITEM_1_2:
+
+						// ID_ITEM_1_2ブロック
+						{
+
+						}
+
+						// 既定の処理へ向かう.
+						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+					// Item1-3が選択された時.
+					case ID_ITEM_1_3:
+
+						// ID_ITEM_1_3ブロック
+						{
+
+						}
+
+						// 既定の処理へ向かう.
+						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+					// 上記以外の時.
+					default:
+						
+						// 既定の処理へ向かう.
+						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+				}
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// マウスの右ボタンが離された時.
+		case WM_RBUTTONUP:	// マウスの右ボタンが離された時.(uMsgがWM_RBUTTONUPの時.)
+
+			// WM_RBUTTONUPブロック
+			{
+
+				// コンテキストメニューの表示
+				POINT pt = {0};	// POINT構造体変数ptを{0}で初期化.
+				pt.x = LOWORD(lParam);	// x座標.
+				pt.y = HIWORD(lParam);	// y座標.
+				ClientToScreen(hwnd, &pt);	// ClientToScreenでクライアント座標からスクリーン座標に.
+				TrackPopupMenu(hMenuR, TPM_LEFTALIGN | TPM_TOPALIGN, pt.x, pt.y, 0, hwnd, NULL);	// TrackPopupMenuでhMenuRを表示.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+
+		// 上記以外の時.
+		default:	// 上記以外の値の時の既定処理.
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+	}
+
+	// あとは既定の処理に任せる.
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);	// 戻り値も含めDefWindowProcに既定の処理を任せる.
+
+}
