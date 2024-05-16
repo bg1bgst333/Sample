d087a41b2cb19657a976d99c171dbf78b188da1c
bg1bgst333 <bg1bgst333@gmail.com>
2024-05-16 20:52:12 +0900

    add CMenu::GetSafeHmenu

---
+++ mfc/CMenu/GetSafeHmenu/src/CMenu/CMenu/CFrameWnd_.cpp
@@ -0,0 +1,39 @@
+// ヘッダファイルのインクルード
+#include "CFrameWnd_.h"	// CFrameWnd_クラス
+#include "resource.h"	// リソースID
+
+// CFrameWnd_のメッセージマップ
+BEGIN_MESSAGE_MAP(CFrameWnd_, CFrameWnd)
+	ON_UPDATE_COMMAND_UI(ID_ITEM_1_1, OnUpdateItem1_1)	// メニュー項目などのUI更新マクロ.(Item1_1)
+	ON_COMMAND(ID_ITEM_1_1, OnItem1_1)	// メニュー項目などのコマンドマクロ.(Item1_1)
+END_MESSAGE_MAP()
+
+// CFrameWnd_のメンバの定義
+// コンストラクタCFrameWnd_()
+CFrameWnd_::CFrameWnd_(){	// 初期化処理
+
+}
+
+// ハンドラOnUpdateItem1_1()
+void CFrameWnd_::OnUpdateItem1_1(CCmdUI* pCmdUI){	// メニュー項目Item1_1のUI更新ハンドラ
+
+	// メニュー項目Item1_1のUI更新
+	if (pCmdUI){	// pCmdUIがNULLでないなら.
+
+		// メニュー項目Item1_1の有効化
+		pCmdUI->Enable();	// EnableでItem1_1を有効にする.(デフォルト引数はTRUE.)
+
+	}
+
+}
+
+// ハンドラOnItem1_1()
+void CFrameWnd_::OnItem1_1(){	// メニュー項目Item1_1のコマンドハンドラ
+
+	// メニューハンドルの取得.
+	CMenu *pMenu = GetMenu();	// CWnd::GetMenuでCMenuオブジェクトポインタpMenu取得.
+	HMENU hMenu = pMenu->GetSafeHmenu();	// CMenu::GetSafeHmenuでメニューハンドルhMenu取得.
+	::AppendMenu(hMenu, MF_ENABLED | MF_STRING, 102, _T("Item2"));	// Win32APIのAppendMenuで"Item2"を追加.
+	::DrawMenuBar(m_hWnd);	// Win32APIのDrawMenuBarでメニューを再描画.
+
+}
