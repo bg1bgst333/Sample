2954a4f4c6d5d6a0d45851660db9784ddb0f52fb
bg1bgst333 <bg1bgst333@gmail.com>
2024-05-24 11:25:20 +0900

    add CMenu::Attach

---
+++ mfc/CMenu/Attach/src/CMenu/CMenu/CFrameWnd_.cpp
@@ -0,0 +1,44 @@
+// ヘッダファイルのインクルード
+#include "CFrameWnd_.h"	// CFrameWnd_クラス
+#include "resource.h"	// リソースID
+
+// CFrameWnd_のメッセージマップ
+BEGIN_MESSAGE_MAP(CFrameWnd_, CFrameWnd)
+	ON_UPDATE_COMMAND_UI(ID_ITEM_1_1, OnUpdateItem1_1)	// メニュー項目などのUI更新マクロ.(Item1_1)
+	ON_COMMAND(ID_ITEM_1_1, OnItem1_1)	// メニュー項目などのコマンドマクロ.(Item1_1)
+END_MESSAGE_MAP()
+
+// CFrameWnd_のメンバの定義
+// コンストラクタCFrameWnd_()
+CFrameWnd_::CFrameWnd_(){	// 初期化処理
+
+}
+
+// ハンドラOnUpdateItem1_1()
+void CFrameWnd_::OnUpdateItem1_1(CCmdUI* pCmdUI){	// メニュー項目Item1_1のUI更新ハンドラ
+
+	// メニュー項目Item1_1のUI更新
+	if (pCmdUI){	// pCmdUIがNULLでないなら.
+
+		// メニュー項目Item1_1の有効化
+		pCmdUI->Enable();	// EnableでItem1_1を有効にする.(デフォルト引数はTRUE.)
+
+	}
+
+}
+
+// ハンドラOnItem1_1()
+void CFrameWnd_::OnItem1_1(){	// メニュー項目Item1_1のコマンドハンドラ
+
+	// メニューの差し替え.
+	CMenu cMenu;
+	cMenu.Attach(m_hMenuDefault);
+	SetMenu(NULL);
+	cMenu.DestroyMenu();	// ここの中でDetachしてDestroyMenuしてm_hMenuはNULLになり, デストラクタで破棄しないので安全に終われるので, ここでのDetach不要.
+	CMenu cNewMenu;
+	cNewMenu.CreateMenu();
+	cNewMenu.AppendMenu(MF_ENABLED | MF_STRING, 102, _T("Item2"));
+	SetMenu(&cNewMenu);
+	m_hMenuDefault = cNewMenu.m_hMenu;
+
+}
