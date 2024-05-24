78537210578a2eb4bb9444b1374e67a4885f3259
bg1bgst333 <bg1bgst333@gmail.com>
2024-05-24 14:26:14 +0900

    add CMenu::Detach

---
+++ mfc/CMenu/Detach/src/CMenu/CMenu/CFrameWnd_.cpp
@@ -0,0 +1,40 @@
+// ヘッダファイルのインクルード
+#include "CFrameWnd_.h"	// CFrameWnd_クラス
+#include "resource.h"	// リソースID
+
+// CFrameWnd_のメッセージマップ
+BEGIN_MESSAGE_MAP(CFrameWnd_, CFrameWnd)
+	ON_UPDATE_COMMAND_UI(ID_ITEM_1_1, OnUpdateItem1_1)	// メニュー項目などのUI更新マクロ.(Item1_1)
+	ON_COMMAND(ID_ITEM_1_1, OnItem1_1)	// メニュー項目などのコマンドマクロ.(Item1_1)
+END_MESSAGE_MAP()
+
+// CFrameWnd_のメンバの定義
+// コンストラクタCFrameWnd_()
+CFrameWnd_::CFrameWnd_(){	// 初期化処理
+
+}
+
+// ハンドラOnUpdateItem1_1()
+void CFrameWnd_::OnUpdateItem1_1(CCmdUI* pCmdUI){	// メニュー項目Item1_1のUI更新ハンドラ
+
+	// メニュー項目Item1_1のUI更新
+	if (pCmdUI){	// pCmdUIがNULLでないなら.
+
+		// メニュー項目Item1_1の有効化
+		pCmdUI->Enable();	// EnableでItem1_1を有効にする.(デフォルト引数はTRUE.)
+
+	}
+
+}
+
+// ハンドラOnItem1_1()
+void CFrameWnd_::OnItem1_1(){	// メニュー項目Item1_1のコマンドハンドラ
+
+	// メニュー項目の追加.
+	CMenu cMenu;
+	cMenu.Attach(m_hMenuDefault);
+	cMenu.AppendMenu(MF_ENABLED | MF_STRING, 102, _T("Item2"));
+	cMenu.Detach();	// これがないとm_hMenuDefaultがcMenuのデストラクタで破棄されちゃう.
+	::DrawMenuBar(m_hWnd);
+
+}
