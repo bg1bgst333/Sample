fa810fa83a1c1983a632312f1c6a02e40b638d50
bg1bgst333 <bg1bgst333@gmail.com>
2024-05-27 13:45:08 +0900

    add CMenu::InsertMenu

---
+++ mfc/CMenu/InsertMenu/src/CMenu/CMenu/CFrameWnd_.cpp
@@ -0,0 +1,84 @@
+// ヘッダファイルのインクルード
+#include "CFrameWnd_.h"	// CFrameWnd_クラス
+#include "resource.h"	// リソースID
+
+// CFrameWnd_のメッセージマップ
+BEGIN_MESSAGE_MAP(CFrameWnd_, CFrameWnd)
+	ON_UPDATE_COMMAND_UI(ID_ITEM_1_1, OnUpdateItem1_1)	// メニュー項目などのUI更新マクロ.(Item1_1)
+	ON_COMMAND(ID_ITEM_1_1, OnItem1_1)	// メニュー項目などのコマンドマクロ.(Item1_1)
+	ON_UPDATE_COMMAND_UI(ID_ITEM_1_2, OnUpdateItem1_2)	// メニュー項目などのUI更新マクロ.(Item1_2)
+	ON_COMMAND(ID_ITEM_1_2, OnItem1_2)	// メニュー項目などのコマンドマクロ.(Item1_2)
+	ON_UPDATE_COMMAND_UI(ID_ITEM_1_3, OnUpdateItem1_3)	// メニュー項目などのUI更新マクロ.(Item1_3)
+	ON_COMMAND(ID_ITEM_1_3, OnItem1_3)	// メニュー項目などのコマンドマクロ.(Item1_3)
+END_MESSAGE_MAP()
+
+// CFrameWnd_のメンバの定義
+// コンストラクタCFrameWnd_()
+CFrameWnd_::CFrameWnd_(){	// 初期化処理
+
+}
+
+// ハンドラOnUpdateItem1_1()
+void CFrameWnd_::OnUpdateItem1_1(CCmdUI* pCmdUI){	// メニュー項目Item1_1のUI更新ハンドラ
+
+	// メニュー項目Item1_1のUI更新
+	if (pCmdUI){	// pCmdUIがNULLでないなら.
+
+		// メニュー項目Item1_1の有効化
+		pCmdUI->Enable();	// EnableでItem1_1を有効にする.(デフォルト引数はTRUE.)
+
+	}
+
+}
+
+// ハンドラOnItem1_1()
+void CFrameWnd_::OnItem1_1(){	// メニュー項目Item1_1のコマンドハンドラ
+
+	// メニュー項目の挿入.
+	CMenu *pMenu = GetMenu();	// メニューの取得.
+	CMenu *pSubMenu = pMenu->GetSubMenu(0);	// 0番目のサブメニュー取得.
+	pSubMenu->InsertMenu(1, MF_BYPOSITION, ID_ITEM_1_2, _T("Item1-2(&B)"));	// pSubMenu->InsertMenuで1番目にItem1-2を追加.
+
+}
+
+// ハンドラOnUpdateItem1_2()
+void CFrameWnd_::OnUpdateItem1_2(CCmdUI* pCmdUI){	// メニュー項目Item1_2のUI更新ハンドラ
+
+	// メニュー項目Item1_2のUI更新
+	if (pCmdUI){	// pCmdUIがNULLでないなら.
+
+		// メニュー項目Item1_2の有効化
+		pCmdUI->Enable();	// EnableでItem1_2を有効にする.(デフォルト引数はTRUE.)
+
+	}
+
+}
+
+// ハンドラOnItem1_2()
+void CFrameWnd_::OnItem1_2(){	// メニュー項目Item1_2のコマンドハンドラ
+
+	// "Item1-2"と表示.
+	AfxMessageBox(_T("Item1-2"));
+
+}
+
+// ハンドラOnUpdateItem1_3()
+void CFrameWnd_::OnUpdateItem1_3(CCmdUI* pCmdUI){	// メニュー項目Item1_3のUI更新ハンドラ
+
+	// メニュー項目Item1_3のUI更新
+	if (pCmdUI){	// pCmdUIがNULLでないなら.
+
+		// メニュー項目Item1_3の有効化
+		pCmdUI->Enable();	// EnableでItem1_3を有効にする.(デフォルト引数はTRUE.)
+
+	}
+
+}
+
+// ハンドラOnItem1_3()
+void CFrameWnd_::OnItem1_3(){	// メニュー項目Item1_3のコマンドハンドラ
+
+	// "Item1-3"と表示.
+	AfxMessageBox(_T("Item1-3"));
+
+}
---
+++ mfc/CMenu/InsertMenu/src/CMenu/CMenu/resource.h
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+#define IDM_MAINMENU	100
+#define ID_ITEM_1_1	10101
+#define ID_ITEM_1_2	10102
+#define ID_ITEM_1_3	10103
