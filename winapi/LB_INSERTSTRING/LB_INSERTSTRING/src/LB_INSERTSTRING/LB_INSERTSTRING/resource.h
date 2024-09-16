c3126b106b2d2c2eaf2ad3492a0e5c290c84b738
bg1bgst333 <bg1bgst333@gmail.com>
2024-09-16 09:18:10 +0900

    add LB_INSERTSTRING

---
+++ winapi/LB_INSERTSTRING/LB_INSERTSTRING/src/LB_INSERTSTRING/LB_INSERTSTRING/resource.h
@@ -0,0 +1,36 @@
+#include <windows.h>
+
+#define IDR_MAINMENU	101
+#define ID_ITEM_1_1	10101
+#define ID_ITEM_1_2	10102
+#define ID_ITEM_1_3	10103
+#define ID_ITEM_2_1	10201
+#define ID_ITEM_2_2	10202
+#define ID_ITEM_2_3	10203
+#define ID_ITEM_3_1	10301
+#define ID_ITEM_3_2	10302
+#define ID_ITEM_3_3	10303
+
+#define IDD_DIALOG	201
+
+#define IDI_ICON1	301
+
+#define IDS_WINDOW_NAME	401
+
+#define ID_BUTTON1	501
+#define ID_BUTTON2	502
+#define ID_BUTTON3	503
+
+#define ID_STATIC1	511
+
+#define ID_EDIT1	521
+
+#define ID_CHECK1	531
+
+#define ID_RADIO1	541
+#define ID_RADIO2	542
+#define ID_RADIO3	543
+
+#define ID_GROUP1	551
+
+#define ID_LIST1	561
