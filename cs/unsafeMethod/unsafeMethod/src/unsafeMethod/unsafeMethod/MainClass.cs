62b035fe2032a35a3ba501bdbbf53bc97773dda6
bg1bgst333 <bg1bgst333@gmail.com>
2024-09-18 07:50:23 +0900

    add unsafeMethod

---
+++ cs/unsafeMethod/unsafeMethod/src/unsafeMethod/unsafeMethod/MainClass.cs
@@ -0,0 +1,32 @@
+// メインクラス
+class MainClass // MainClassクラスの定義
+{
+
+    // メインメソッド
+    static void Main()  // Mainメソッドの定義
+    {
+
+        // UnsafeMethodを呼ぶ.
+        UnsafeMethod();
+
+    }
+
+    // アンセーフメソッド
+    unsafe void UnsafeMethod()
+    {
+
+        // int型nをi23で初期化.
+        int n = 123;
+
+        // int型ポインタpをnのアドレスで初期化.
+        int* p = &n;
+
+        // pの参照先に456を代入.
+        *p = 456;
+
+        // nを出力.
+        System.Console.WriteLine("n = " + n);
+
+    }
+
+}
