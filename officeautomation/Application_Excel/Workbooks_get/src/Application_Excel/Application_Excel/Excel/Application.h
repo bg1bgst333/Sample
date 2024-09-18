fcd416296733940c337fa9cd5c5c7ed9c7a60552
bg1bgst333 <bg1bgst333@gmail.com>
2024-09-18 15:19:13 +0900

    add Application#Excel::Workbooks#get

---
+++ officeautomation/Application_Excel/Workbooks_get/src/Application_Excel/Application_Excel/Excel/Application.h
@@ -0,0 +1,38 @@
+// 二重インクルード防止
+#ifndef __EXCEL__APPLICATION_H__
+#define __EXCEL__APPLICATION_H__
+
+// 名前空間Excelの定義
+namespace Excel {
+
+	// アプリケーションクラスCApplicationの定義
+	class CApplication {
+
+		// privateメンバ
+		private:
+
+			// privateメンバ変数
+			IDispatch *m_pDispatch;	// IDispatchポインタm_pDispatch.
+
+			// privateメンバ関数
+			// コンストラクタ
+			CApplication();	// コンストラクタCApplication(newで生成不可.)
+
+
+		// publicメンバ
+		public:
+
+			// publicメンバ関数
+			// staticメンバ関数
+			static CApplication * CreateInstance();	// staticな生成関数CreateInstance.
+			// デストラクタ
+			virtual ~CApplication();	// デストラクタ~CApplication
+			// メンバ関数
+			BOOL put_Visible(BOOL bVisible);	// 表示・非表示プロパティの設定.
+			IDispatch * get_Workbooks();	// Workbooksオブジェクトプロパティの取得.
+
+	};
+
+}
+
+#endif
