ca58169ca30a940d3ffc429f26a6d713b597f716
bg1bgst333 <bg1bgst333@gmail.com>
2024-09-28 09:28:22 +0900

    add Range.Item#get

---
+++ officeautomation/Range/Item_get/src/Range/Range/Excel/Range.h
@@ -0,0 +1,36 @@
+// 二重インクルード防止
+#ifndef __EXCEL__RANGE_H__
+#define __EXCEL__RANGE_H__
+
+// 名前空間Excelの定義
+namespace Excel {
+
+	// レンジクラスCRangeの定義
+	class CRange {
+
+		// privateメンバ
+		private:
+
+			// privateメンバ変数
+			IDispatch *m_pDispatch;	// IDispatchポインタm_pDispatch.
+
+			// privateメンバ関数
+			// コンストラクタ
+			CRange();	// コンストラクタCRange(newで生成不可.)
+
+		// publicメンバ
+		public:
+
+			// publicメンバ関数
+			// コンストラクタ
+			CRange(IDispatch *pDispatch);	// 引数付きコンストラクタ.(IDispatchポインタpDispatchを受け取る.)					
+			// デストラクタ
+			virtual ~CRange();	// デストラクタ~CRange
+			// メンバ関数
+			CRange * get_Item(int i, int j);	// Itemプロパティ(Rangeオブジェクト)の取得.
+
+	};
+
+}
+
+#endif
