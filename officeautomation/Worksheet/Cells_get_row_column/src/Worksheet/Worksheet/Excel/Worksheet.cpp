c2f29bc9317cedc144218ad98cc5bf3260dcb2af
bg1bgst333 <bg1bgst333@gmail.com>
2024-10-08 07:46:17 +0900

    add Worksheet.Cells#get#row_column

---
+++ officeautomation/Worksheet/Cells_get_row_column/src/Worksheet/Worksheet/Excel/Worksheet.cpp
@@ -0,0 +1,136 @@
+// ヘッダのインクルード
+// 既定のヘッダ
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR対応
+// 独自のヘッダ
+#include "Worksheet.h"	// Excel::CWorksheet
+
+// コンストラクタCWorksheet
+Excel::CWorksheet::CWorksheet() {
+
+}
+
+// 引数付きコンストラクタ.(IDispatchポインタpDispatchを受け取る.)
+Excel::CWorksheet::CWorksheet(IDispatch *pDispatch) {
+
+	// m_pDispatchにセット.
+	m_pDispatch = pDispatch;
+
+}
+
+// デストラクタ~CWorksheet
+Excel::CWorksheet::~CWorksheet() {
+
+	// m_pDispatchの解放.
+	if (m_pDispatch != NULL) {	// m_pDispatchがNULLでなければ.
+		m_pDispatch->Release();	// IDispatch::Releaseで解放.
+		m_pDispatch = NULL;	// NULLをセット.
+	}
+
+}
+
+// Cellsプロパティ(Rangeオブジェクト)の取得.
+Excel::CRange * Excel::CWorksheet::get_Cells() {
+
+	// メソッドやプロパティのDISPIDの取得.
+	LPOLESTR lpwszName = L"Cells";	// lpwszNameを"Cells"で初期化.
+	DISPID dispid;	// DISPID型dispid.
+	HRESULT hr = m_pDispatch->GetIDsOfNames(IID_NULL, &lpwszName, 1, LOCALE_USER_DEFAULT, &dispid);	// IDispatch::GetIDsOfNamesでdispid取得.
+	if (FAILED(hr)) {	// 失敗.
+		return NULL;
+	}
+
+	// メソッドやプロパティにアクセス.
+	VARIANT var;	// VARIANT構造体var.
+	DISPPARAMS dispparams;	// DISPPARAMS構造体dispparams.
+	DISPID dispidname = DISPID_PROPERTYPUT;	// dispidnameをDISPID_PROPERTYPUTに初期化.
+	VariantInit(&var);	// varをVariantInitで初期化.
+	dispparams.cArgs = 0;	// 引数の数は0.
+	dispparams.rgvarg = NULL;	// NULLを指定.
+	dispparams.rgdispidNamedArgs = NULL;	// NULLを指定.
+	dispparams.cNamedArgs = 0;	// cNamedArgsも0.
+	hr = m_pDispatch->Invoke(dispid, IID_NULL, LOCALE_SYSTEM_DEFAULT, DISPATCH_PROPERTYGET, &dispparams, &var, NULL, NULL);	// IDispatch::Invokeでアクセス.
+	if (FAILED(hr)) {	// 失敗.
+		return NULL;
+	}
+
+	// 成功.
+	CRange *pRange = new CRange(var.pdispVal);	// CRangeのコンストラクタにvar.pdispValをセット.
+	return pRange;
+
+}
+
+// Cellsプロパティ(Rangeオブジェクト)の取得.(row, columnを渡す.)
+Excel::CRange * Excel::CWorksheet::get_Cells(int row, int column) {
+
+	// メソッドやプロパティのDISPIDの取得.
+	LPOLESTR lpwszName = L"Cells";	// lpwszNameを"Cells"で初期化.
+	DISPID dispid;	// DISPID型dispid.
+	HRESULT hr = m_pDispatch->GetIDsOfNames(IID_NULL, &lpwszName, 1, LOCALE_USER_DEFAULT, &dispid);	// IDispatch::GetIDsOfNamesでdispid取得.
+	if (FAILED(hr)) {	// 失敗.
+		return NULL;
+	}
+
+	// メソッドやプロパティにアクセス.
+	VARIANT var[2];	// VARIANT構造体配列var.
+	VARIANT varResult;	// 結果を格納するVARIANT構造体varResult.
+	DISPPARAMS dispparams;	// DISPPARAMS構造体dispparams.
+	DISPID dispidname = DISPID_PROPERTYPUT;	// dispidnameをDISPID_PROPERTYPUTに初期化.
+	// 複数の引数の場合, 逆順なので注意.(行(i)がvar[1], 列(j)がvar[0]になる.)
+	VariantInit(&var[1]);	// var[1]を初期化.
+	var[1].vt = VT_I4;	// 4バイトint.
+	var[1].lVal = row;	// rowを代入.
+	VariantInit(&var[0]);	// var[0]を初期化.
+	var[0].vt = VT_I4;	// 4バイトint.
+	var[0].lVal = column;	// columnを代入.
+	VariantInit(&varResult);	// VariantInitでvarResult初期化.
+	dispparams.cArgs = 2;	// 引数の数は2.
+	dispparams.rgvarg = var;	// varを指定.
+	dispparams.rgdispidNamedArgs = NULL;	// NULLを指定.
+	dispparams.cNamedArgs = 0;	// cNamedArgsも0.
+	hr = m_pDispatch->Invoke(dispid, IID_NULL, LOCALE_SYSTEM_DEFAULT, DISPATCH_PROPERTYGET, &dispparams, &varResult, NULL, NULL);	// IDispatch::Invokeでアクセス.
+	if (FAILED(hr)) {	// 失敗.
+		return NULL;
+	}
+
+	// 成功.
+	CRange *pRange = new CRange(varResult.pdispVal);	// CRangeのコンストラクタにvarResult.pdispValをセット.
+	return pRange;
+
+}
+
+// Rangeプロパティの取得.
+Excel::CRange * Excel::CWorksheet::get_Range(LPCTSTR lpctszCell) {
+
+	// メソッドやプロパティのDISPIDの取得.
+	LPOLESTR lpwszName = L"Range";	// lpwszNameを"Range"で初期化.
+	DISPID dispid;	// DISPID型dispid.
+	HRESULT hr = m_pDispatch->GetIDsOfNames(IID_NULL, &lpwszName, 1, LOCALE_USER_DEFAULT, &dispid);	// IDispatch::GetIDsOfNamesでdispid取得.
+	if (FAILED(hr)) {	// 失敗.
+		return NULL;
+	}
+
+	// メソッドやプロパティにアクセス.
+	VARIANT var;	// VARIANT構造体var.
+	VARIANT varResult;	// 結果を格納するVARIANT構造体varResult.
+	DISPPARAMS dispparams;	// DISPPARAMS構造体dispparams.
+	DISPID dispidname = DISPID_PROPERTYPUT;	// dispidnameをDISPID_PROPERTYPUTに初期化.
+	var.vt = VT_BSTR;	// 文字列
+	var.bstrVal = SysAllocString(lpctszCell);	// SysAllocStringでlpctszCellのBSTR生成.
+	VariantInit(&varResult);	// VariantInitでvarResult初期化.
+	dispparams.cArgs = 1;	// 引数の数は1.
+	dispparams.rgvarg = &var;	// &varを指定.
+	dispparams.rgdispidNamedArgs = NULL;	// NULLを指定.
+	dispparams.cNamedArgs = 0;	// cNamedArgsも0.
+	hr = m_pDispatch->Invoke(dispid, IID_NULL, LOCALE_SYSTEM_DEFAULT, DISPATCH_PROPERTYGET, &dispparams, &varResult, NULL, NULL);	// IDispatch::Invokeでアクセス.
+	SysFreeString(var.bstrVal);	// SysFreeStringでvar.bstrValを破棄.
+	if (FAILED(hr)) {	// 失敗.
+		return NULL;
+	}
+
+	// 成功.
+	CRange *pRange = new CRange(varResult.pdispVal);	// CRangeのコンストラクタにvarResult.pdispValをセット.
+	return pRange;
+
+}
+
---
+++ officeautomation/Worksheet/Cells_get_row_column/src/Worksheet/Worksheet/Worksheet_.cpp
@@ -0,0 +1,104 @@
+// ヘッダファイルのインクルード
+// 既定のヘッダ
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR対応
+#include <locale.h>		// ロケール
+
+// 独自のヘッダ
+#include "Excel/Application.h"	// Excel::CApplication
+
+// _tmain関数の定義
+int _tmain(int argc, TCHAR *argv[]) {	// main関数のTCHAR版.
+
+	// COMの初期化.
+	CoInitialize(NULL);	// CoInitializeでCOMを初期化.
+
+	// Excel::CApplicationの生成
+	Excel::CApplication *pApplication = Excel::CApplication::CreateInstance();	// Excel::CApplication::CreateInstanceで作成.
+
+	// 取得したpApplicationを出力.
+	_tprintf(_T("pApplication = 0x%08x\n"), (unsigned long)pApplication);	// pApplicationを出力.
+
+	// アプリケーションの表示.
+	pApplication->put_Visible(TRUE);	// Excel::CApplication::put_VisibleにTRUE渡して表示.
+
+	// Workbooksの取得.
+	Excel::CWorkbooks * pWorkbooks = NULL;	// CWorkbooksポインタpWorkbooksをNULLで初期化.
+	pWorkbooks = pApplication->get_Workbooks();	// Excel::CApplication::get_WorkbooksでpWorkbooksを取得.
+
+	// 取得したpWorkbooksを出力.
+	_tprintf(_T("CWorkbooks * pWorkbooks = 0x%08x\n"), (unsigned long)pWorkbooks);	// pWorkbooksを出力.
+
+	// xlsxファイルのパス生成.
+	TCHAR tszCurrentPath[MAX_PATH] = { 0 };	// このプロセスの現在のディレクトリパスを指すTCHAR配列tszCurrentPath(長さMAX_PATH)を{0}で初期化.
+	GetCurrentDirectory(MAX_PATH, tszCurrentPath);	// GetCurrentDirectoryでtszCurrentPathを取得.
+	_tcscat(tszCurrentPath, _T("\\"));	// "\"を連結.
+	_tcscat(tszCurrentPath, _T("test.xlsx"));	// "test.xlsx"を連結.
+
+	// Workbookを開く.
+	Excel::CWorkbook * pWorkbook = NULL;	// CWorkbookポインタpWorkbookをNULLで初期化.
+	pWorkbook = pWorkbooks->Open(tszCurrentPath);	// Excel::CWorkbooks::OpenでtszCurrentPathを開き, それを取得.
+
+	// 追加したpWorkbookを出力.
+	_tprintf(_T("CWorkbook * pWorkbook = 0x%08x\n"), (unsigned long)pWorkbook);	// pWorkbookを出力.
+
+	// ActiveSheetの取得.
+	Excel::CWorksheet * pActiveSheet = NULL;	// CWorksheetポインタpActiveSheetをNULLで初期化.
+	pActiveSheet = pWorkbook->get_ActiveSheet();	// Excel::CWorkbook::get_ActiveSheetでpActiveSheetを取得.
+
+	// 取得したpActiveSheetを出力.
+	_tprintf(_T("CWorksheet * pActiveSheet = 0x%08x\n"), (unsigned long)pActiveSheet);	// pActiveSheetを出力.
+
+	// Rangeの取得.
+	Excel::CRange * pRange12 = NULL;	// CRangeポインタpRange12をNULLで初期化.
+	pRange12 = pActiveSheet->get_Cells(1, 2);	// Excel::CWorksheet::get_Cells(1, 2)でpRange12を取得.
+
+	// 取得したpRange12を出力.
+	_tprintf(_T("CRange * pRange12 = 0x%08x\n"), (unsigned long)pRange12);	// pRange12を出力.
+
+	// 1行目の2列目の値を取得.
+	VARIANT var12;	// VARIANT構造体var12.
+	VariantInit(&var12);	// var12をVariantInitで初期化.
+	pRange12->get_Value(&var12);	// Excel::CRange::get_Valueで値を取得.
+	if (var12.vt == VT_R8) {	// 8バイト実数.
+		_tprintf(_T("var12.dblVal = %le\n"), var12.dblVal);	// var12.dblValを出力.
+	}
+	else if (var12.vt == VT_BSTR) {	// BSTR文字列
+		char *old = setlocale(LC_ALL, "japanese");	// setlocaleでLC_ALL, "japanese"をセット.
+		_tprintf(_T("var12.bstrVal = %s\n"), var12.bstrVal);	// var12.bstrValを出力.
+		setlocale(LC_ALL, old);	// setlocaleでLC_ALL, oldをセット.
+	}
+	VariantClear(&var12);	// var12をVariantClearでクリア.
+
+	// pRange12の破棄.
+	if (pRange12 != NULL) {
+		delete pRange12;
+	}
+
+	// pActiveSheetの破棄.
+	if (pActiveSheet != NULL) {
+		delete pActiveSheet;
+	}
+
+	// pWorkbookの破棄.
+	if (pWorkbook != NULL) {
+		delete pWorkbook;
+	}
+
+	// pWorkbooksの破棄.
+	if (pWorkbooks != NULL) {
+		delete pWorkbooks;
+	}
+
+	// pApplicationの破棄.
+	if (pApplication != NULL) {
+		delete pApplication;
+	}
+
+	// COMの終了処理.
+	CoUninitialize();	// CoUninitializeで終了処理.
+
+	// プログラムの終了.
+	return 0;	// 0を返して終了.
+
+}
