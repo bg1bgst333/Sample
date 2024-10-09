dd8f992b1e7dc53e146d9c27ca61d01caa58a637
bg1bgst333 <bg1bgst333@gmail.com>
2024-10-09 09:17:59 +0900

    add Range.Item#get#strRow

---
+++ officeautomation/Range/Item_get_strRow/src/Range/Range/Excel/Worksheet.h
@@ -0,0 +1,42 @@
+// 二重インクルード防止
+#ifndef __EXCEL__WORKSHEET_H__
+#define __EXCEL__WORKSHEET_H__
+
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "Range.h"		// Excel::CRange
+
+// 名前空間Excelの定義
+namespace Excel {
+
+	// ワークシートクラスCWorksheetの定義
+	class CWorksheet {
+
+		// privateメンバ
+		private:
+
+			// privateメンバ変数
+			IDispatch *m_pDispatch;	// IDispatchポインタm_pDispatch.
+
+			// privateメンバ関数
+			// コンストラクタ
+			CWorksheet();	// コンストラクタCWorksheet(newで生成不可.)
+
+		// publicメンバ
+		public:
+
+			// publicメンバ関数
+			// コンストラクタ
+			CWorksheet(IDispatch *pDispatch);	// 引数付きコンストラクタ.(IDispatchポインタpDispatchを受け取る.)					
+			// デストラクタ
+			virtual ~CWorksheet();	// デストラクタ~CWorksheet
+			// メンバ関数
+			CRange * get_Cells();	// Cellsプロパティ(Rangeオブジェクト)の取得.
+			CRange * get_Cells(int row, int column);	// Cellsプロパティ(Rangeオブジェクト)の取得.(row, columnを渡す.)
+			CRange * get_Range(LPCTSTR lpctszCell);	// Rangeプロパティの取得.
+
+	};
+
+}
+
+#endif
