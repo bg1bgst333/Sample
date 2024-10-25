a10cfa6590e7c23acfedae5d65703f2d8528dc4b
bg1bgst333 <bg1bgst333@gmail.com>
2024-10-26 07:32:24 +0900

    add IntPtr

---
+++ dotnet/IntPtr/IntPtr/src/IntPtr/IntPtr/MainClass.cs
@@ -0,0 +1,23 @@
+// 名前空間の登録
+using System;   // 共通データ型と基本クラス(System名前空間)
+using System.Runtime.InteropServices;   // COM相互運用とプラットフォーム呼び出し(System.Runtime.InteropServices名前空間)
+
+// メインクラス
+class MainClass // MainClassクラスの定義
+{
+
+    // DLLのインポート
+    [DllImport("user32.dll", CharSet = CharSet.Auto)] // DllImportで"user32.dll"のインポート.(CharSet.Autoにする.)
+    public static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint Type);   // WindowsAPIのMessageBoxの宣言.
+
+    // メインメソッド
+    static void Main()  // Mainメソッドの定義
+    {
+
+        // メッセージボックスの表示.
+        IntPtr handle = new IntPtr(0);  // IntPtr型handleを生成し, コンストラクタに0をセット.(0なのでnullのウィンドウハンドルを表す.)
+        MessageBox(handle, "あいうえお", "DllImportAttribute_", 0);    // MessageBoxで"あいうえお"を表示.
+
+    }
+
+}
