d46b9bd14c86dd3e051976236684f0b46deb356a
bg1bgst333 <bg1bgst333@gmail.com>
2024-11-02 12:04:32 +0900

    add Int32.MaxValue

---
+++ dotnet/Int32/MaxValue/src/Int32_/Int32_/MainClass.cs
@@ -0,0 +1,17 @@
+// 名前空間の登録
+using System;       // 共通データ型と基本クラス(System名前空間)
+
+// メインクラス
+class MainClass // MainClassクラスの定義
+{
+
+    // メインメソッド
+    static void Main()  // Mainメソッドの定義
+    {
+
+        // Int32の上限値を出力.
+        System.Console.WriteLine("Int32.MaxValue = " + Int32.MaxValue); // Int32.MaxValueを出力.
+
+    }
+
+}
