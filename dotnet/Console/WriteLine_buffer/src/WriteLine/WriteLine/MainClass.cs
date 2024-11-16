d5e99e5e7c3d4607627193d661cf9beca3aec8f9
bg1bgst333 <bg1bgst333@gmail.com>
2024-11-16 15:35:55 +0900

    add Console.WriteLine#buffer

---
+++ dotnet/Console/WriteLine_buffer/src/WriteLine/WriteLine/MainClass.cs
@@ -0,0 +1,18 @@
+// 名前空間の登録
+using System;   // 共通データ型と基本クラス(System名前空間)
+
+// MainClassの定義
+class MainClass
+{
+    // Mainメソッドの定義
+    static void Main()
+    {
+
+        // 配列の処理化.
+        char[] charArray = new char[]{'A', 'B', 'C', 'D', 'E'}; // charArrayを{'A', 'B', 'C', 'D', 'E'}で初期化.
+
+        // charArrayの出力.
+        Console.WriteLine(charArray);   // Console.WriteLineでcharArrayを出力.
+
+    }
+}
