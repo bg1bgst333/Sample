f1054c34cc90c01865be8d75202ae6684ab14970
bg1bgst333 <bg1bgst333@gmail.com>
2025-02-23 18:41:15 +0900

    add ServletResponse.getWriter

---
+++ servlet/ServletResponse/getWriter/src/ServletResponse/ServletResponse/src/main/java/com/bgstation0/servlet/sample/servletresponse/ServletMain.java
@@ -0,0 +1,42 @@
+package com.bgstation0.servlet.sample.servletresponse;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletMain
+ */
+@WebServlet("/ServletMain")
+public class ServletMain extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletMain() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("<html><head><title>main</title></head><body>ABCDE</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
