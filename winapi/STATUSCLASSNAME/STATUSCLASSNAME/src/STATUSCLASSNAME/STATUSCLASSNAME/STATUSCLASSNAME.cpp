6e7e851d3e23c753549b3e34b54ce97836f57b3d
bg1bgst333 <bg1bgst333@gmail.com>
2025-05-07 16:21:42 +0900

    add STATUSCLASSNAME

---
+++ winapi/STATUSCLASSNAME/STATUSCLASSNAME/src/STATUSCLASSNAME/STATUSCLASSNAME/STATUSCLASSNAME.cpp
@@ -0,0 +1,131 @@
+// ヘッダファイルのインクルード
+// 既定のヘッダファイル
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+#include <commctrl.h>	// コモンコントロール
+
+// リソースID.
+#define ID_STATUS 101
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ウィンドウプロシージャ
+
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd){
+
+	// 変数・配列の宣言・初期化
+	HWND hWnd;					// ウィンドウハンドルhWnd.
+	MSG msg;					// メッセージmsg.
+	WNDCLASS wc;				// ウィンドウクラスwc.
+
+	// ウィンドウクラスの設定
+	wc.lpszClassName = _T("STATUSCLASSNAME");					// ウィンドウクラス名"STATUSCLASSNAME".
+	wc.style = CS_HREDRAW | CS_VREDRAW;							// スタイルCS_HREDRAW | CS_VREDRAW.
+	wc.lpfnWndProc = WindowProc;								// ウィンドウプロシージャWindowProc.
+	wc.hInstance = hInstance;									// インスタンスhInstance.
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);					// アイコンはアプリケーション既定のもの.
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);					// カーソルIDC_ARROW.
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);		// 背景WHITE_BRUSH.
+	wc.lpszMenuName = NULL;										// メニューはNULL.
+	wc.cbClsExtra = 0;											// 0.
+	wc.cbWndExtra = 0;											// 0.
+
+	// ウィンドウクラスの登録
+	if (!RegisterClass(&wc)){	// RegisterClassでウィンドウクラスを登録.
+
+		// エラー処理
+		return -1;	// 異常終了(-1)
+
+	}
+
+	// ウィンドウの作成
+	hWnd = CreateWindow(_T("STATUSCLASSNAME"), _T("STATUSCLASSNAME"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);	// CreateWindowでウィンドウ作成.
+	if (hWnd == NULL){
+
+		// エラー処理
+		return -2;	// 異常終了(-2)
+
+	}
+
+	// ウィンドウの表示
+	ShowWindow(hWnd, SW_SHOW);
+
+	// メッセージループ
+	while (GetMessage(&msg, NULL, 0, 0) > 0){	// GetMessageでメッセージ取得.
+
+		// ウィンドウメッセージの送出
+		TranslateMessage(&msg);	// TranslateMessageで仮想キーメッセージを文字メッセージへ変換.
+		DispatchMessage(&msg);	// DispatchMessageで受け取ったメッセージをウィンドウプロシージャに送出.
+
+	}
+
+	// プログラムの終了
+	return (int)msg.wParam;	// msg.wParamを返す.
+
+}
+
+// WindowProc関数の定義
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+
+	// ウィンドウメッセージに対する処理.
+	switch (uMsg){	// uMsgごとに処理振り分け.
+
+		// ウィンドウの作成が開始された時.
+		case WM_CREATE:
+
+			// WM_CREATEブロック
+			{
+
+				// 変数の宣言
+				LPCREATESTRUCT lpCS;	// CREATESTRUCT構造体ポインタlpCS.
+				HWND hStatusBar;	// ステータスバーハンドルhStatusBar.
+
+				// lpCSの取得.
+				lpCS = (LPCREATESTRUCT)lParam;	// lParamをlpCSに変換.
+
+				// コモンコントロールの初期化.
+				InitCommonControls();	// InitCommonControlsでコモンコントロールの初期化.
+
+				// ステータスバーの作成.
+				hStatusBar = CreateWindowEx(0, STATUSCLASSNAME, _T("あいうえお"), WS_CHILD | WS_VISIBLE | CCS_BOTTOM, 0, 0, 0, 0, hwnd, (HMENU)ID_STATUS, lpCS->hInstance, NULL);	// CreateWindowExでステータスバー作成.
+				if (hStatusBar == NULL){	// hStatusBarがNULL.
+						
+					// ウィンドウ作成失敗
+					return -1;	// -1を返す.
+
+				}
+
+				// ウィンドウ作成成功
+				return 0;	// 0を返すとウィンドウ作成成功となる.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;
+
+		// ウィンドウが破棄された時.
+		case WM_DESTROY:
+
+			// WM_DESTROYブロック
+			{
+
+				// 終了メッセージの送信.
+				PostQuitMessage(0);	// PostQuitMessageで終了コードを0とするWM_QUITを送信.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;
+
+		// 上記以外の時.
+		default:
+
+			// 既定の処理へ向かう.
+			break;
+
+	}
+
+	// 既定の処理に任せる.
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);	// DefWindowProcを呼び, 戻り値をそのまま返す.
+
+}
