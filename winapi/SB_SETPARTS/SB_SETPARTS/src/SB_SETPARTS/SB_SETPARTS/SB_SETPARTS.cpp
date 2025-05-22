916a7e4032b894a64f5959650091169f5d85952e
bg1bgst333 <bg1bgst333@gmail.com>
2025-05-22 12:21:40 +0900

    add SB_SETPARTS

---
+++ winapi/SB_SETPARTS/SB_SETPARTS/src/SB_SETPARTS/SB_SETPARTS/SB_SETPARTS.cpp
@@ -0,0 +1,178 @@
+// ヘッダファイルのインクルード
+// 既定のヘッダファイル
+#include <windows.h>	// 標準WindowsAPI
+#include <tchar.h>		// TCHAR型
+#include <commctrl.h>	// コモンコントロール
+// 独自のヘッダファイル
+#include "resource.h"	// リソースID
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ウィンドウプロシージャ
+
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd){
+
+	// 変数・配列の宣言・初期化
+	HWND hWnd;					// ウィンドウハンドルhWnd.
+	MSG msg;					// メッセージmsg.
+	WNDCLASS wc;				// ウィンドウクラスwc.
+
+	// ウィンドウクラスの設定
+	wc.lpszClassName = _T("SB_SETPARTS");						// ウィンドウクラス名"SB_SETPARTS".
+	wc.style = CS_HREDRAW | CS_VREDRAW;							// スタイルCS_HREDRAW | CS_VREDRAW.
+	wc.lpfnWndProc = WindowProc;								// ウィンドウプロシージャWindowProc.
+	wc.hInstance = hInstance;									// インスタンスhInstance.
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);					// アイコンはアプリケーション既定のもの.
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);					// カーソルIDC_ARROW.
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);		// 背景WHITE_BRUSH.
+	wc.lpszMenuName = MAKEINTRESOURCE(IDR_MAINMENU);			// メニューにはIDR_MAINMENUをMAKEINTRESOURCEマクロで指定.
+	wc.cbClsExtra = 0;											// 0.
+	wc.cbWndExtra = 0;											// 0.
+
+	// ウィンドウクラスの登録
+	if (!RegisterClass(&wc)){	// RegisterClassでウィンドウクラスを登録.
+
+		// エラー処理
+		return -1;	// 異常終了(-1)
+
+	}
+
+	// ウィンドウの作成
+	hWnd = CreateWindow(_T("SB_SETPARTS"), _T("SB_SETPARTS"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);	// CreateWindowでウィンドウ作成.
+	if (hWnd == NULL){
+
+		// エラー処理
+		return -2;	// 異常終了(-2)
+
+	}
+
+	// ウィンドウの表示
+	ShowWindow(hWnd, SW_SHOW);
+
+	// メッセージループ
+	while (GetMessage(&msg, NULL, 0, 0) > 0){	// GetMessageでメッセージ取得.
+
+		// ウィンドウメッセージの送出
+		TranslateMessage(&msg);	// TranslateMessageで仮想キーメッセージを文字メッセージへ変換.
+		DispatchMessage(&msg);	// DispatchMessageで受け取ったメッセージをウィンドウプロシージャに送出.
+
+	}
+
+	// プログラムの終了
+	return (int)msg.wParam;	// msg.wParamを返す.
+
+}
+
+// WindowProc関数の定義
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+
+	// static変数の宣言.
+	static HWND hStatusBar;	// ステータスバーハンドルhStatusBar.
+
+	// ウィンドウメッセージに対する処理.
+	switch (uMsg){	// uMsgごとに処理振り分け.
+
+		// ウィンドウの作成が開始された時.
+		case WM_CREATE:
+
+			// WM_CREATEブロック
+			{
+
+				// 変数・配列の宣言
+				LPCREATESTRUCT lpCS;	// CREATESTRUCT構造体ポインタlpCS.
+				int size[] = {100, 250, 450};	// 区画サイズ(100, 150, 200)に分ける場合, こう({100, 250, 450})書く.
+
+				// lpCSの取得.
+				lpCS = (LPCREATESTRUCT)lParam;	// lParamをlpCSに変換.
+
+				// コモンコントロールの初期化.
+				InitCommonControls();	// InitCommonControlsでコモンコントロールの初期化.
+
+				// ステータスバーの作成.
+				hStatusBar = CreateWindowEx(0, STATUSCLASSNAME, _T("あいうえお"), WS_CHILD | WS_VISIBLE | CCS_BOTTOM, 0, 0, 0, 0, hwnd, (HMENU)ID_STATUS, lpCS->hInstance, NULL);	// CreateWindowExでステータスバー作成.
+				if (hStatusBar == NULL){	// hStatusBarがNULL.
+						
+					// ウィンドウ作成失敗
+					return -1;	// -1を返す.
+
+				}
+
+				// 3つの区画に分ける.
+				SendMessage(hStatusBar, SB_SETPARTS, (WPARAM)3, (LPARAM)(LPINT)size);	// SB_SETPARTSで区画分け.
+
+				// ウィンドウ作成成功
+				return 0;	// 0を返すとウィンドウ作成成功となる.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;
+
+		// ウィンドウが破棄された時.
+		case WM_DESTROY:
+
+			// WM_DESTROYブロック
+			{
+
+				// 終了メッセージの送信.
+				PostQuitMessage(0);	// PostQuitMessageで終了コードを0とするWM_QUITを送信.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;
+
+		// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりした時.
+		case WM_COMMAND:	// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりした時.(uMsgがWM_COMMANDの時.)
+
+			// WM_COMMANDブロック
+			{
+
+				// どのメニュー項目が選択されたかを判定する.
+				switch (LOWORD(wParam)){	// LOWORD(wParam)で選択されたメニュー項目のIDが取得できるので, その値で判定する.
+
+					// 取得したIDごとに処理を分岐.
+					// Set Textが選択された時.
+					case ID_ITEM_SETTEXT:
+
+						// ID_ITEM_SETTEXTブロック
+						{
+
+							// ステータスバーにテキストをセット.
+							SendMessage(hStatusBar, SB_SETTEXT, (WPARAM)0 | 0, (LPARAM)_T("かきくけこ"));	// SB_SETTEXTでhStatusBarの0番目に"かきくけこ"をセット.
+							SendMessage(hStatusBar, SB_SETTEXT, (WPARAM)1 | 0, (LPARAM)_T("さしすせそ"));	// SB_SETTEXTでhStatusBarに1番目に"さしすせそ"をセット.
+							SendMessage(hStatusBar, SB_SETTEXT, (WPARAM)2 | 0, (LPARAM)_T("たちつてと"));	// SB_SETTEXTでhStatusBarに2番目に"たちつてと"をセット.
+
+						}
+
+						// 既定の処理へ向かう.
+						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+					// 上記以外の時.
+					default:
+
+						// 既定の処理へ向かう.
+						break;
+
+				}
+
+				// 既定の処理へ向かう.
+				break;
+
+			}
+
+			// 既定の処理へ向かう.
+			break;
+
+		// 上記以外の時.
+		default:
+
+			// 既定の処理へ向かう.
+			break;
+
+	}
+
+	// 既定の処理に任せる.
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);	// DefWindowProcを呼び, 戻り値をそのまま返す.
+
+}
