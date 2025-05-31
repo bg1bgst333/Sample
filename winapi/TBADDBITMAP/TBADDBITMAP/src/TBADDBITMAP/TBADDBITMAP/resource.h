31e6d724a7e8bd25afba9733b0d3b54657343e6c
bg1bgst333 <bg1bgst333@gmail.com>
2025-05-31 16:38:19 +0900

    add TBADDBITMAP

---
+++ winapi/TBADDBITMAP/TBADDBITMAP/src/TBADDBITMAP/TBADDBITMAP/resource.h
@@ -0,0 +1,12 @@
+#define IDR_MAINMENU			101
+#define ID_ITEM_SETTEXT			10101
+#define ID_ITEM_ADDBUTTONS		10102
+#define ID_FILE_NEW				10201
+#define ID_STATUS			201
+#define ID_TOOLBAR1			301
+#define IDM_ZERO			30101
+#define IDM_ONE				30102
+#define IDM_TWO				30103
+#define IDM_THREE			30104
+#define IDB_BITMAP1			401
+#define IDB_BITMAP2			402
