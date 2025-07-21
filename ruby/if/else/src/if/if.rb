3bb7d0596e682c5b96cbe181af5c21e2acff0324
bg1bgst333 <bg1bgst333@gmail.com>
2025-07-21 11:55:22 +0900

    add if#else

---
+++ ruby/if/else/src/if/if.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+# 年齢ageが20以上なら"adult", そうでないなら"young"と表示.
+age = 25
+if age >= 20
+  puts "adult"
+else
+  puts "young"
+end
+age = 10
+if age >= 20
+  puts "adult"
+else
+  puts "young"
+end
+puts "end"
