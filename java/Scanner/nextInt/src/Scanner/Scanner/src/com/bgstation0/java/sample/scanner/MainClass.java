a7c9ea4243efbbd6e87ecb84fbd079bdf8c060b7
bg1bgst333 <bg1bgst333@gmail.com>
2025-08-02 11:56:33 +0900

    add Scanner.nextInt

---
+++ java/Scanner/nextInt/src/Scanner/Scanner/src/com/bgstation0/java/sample/scanner/MainClass.java
@@ -0,0 +1,26 @@
+// パッケージ名定義
+package com.bgstation0.java.sample.scanner;	// com.bgstation0.java.sample.scannerパッケージ.
+
+//パッケージ・クラスのインポート
+import java.util.Scanner;       // Scannerクラス(java.utilパッケージ)
+
+//メインクラス
+public class MainClass {        // MainClassクラスの定義
+
+    // Javaのエントリポイント
+    public static void main(String[] args){ // mainメソッドの定義
+
+        // Scannerオブジェクトscannerの生成.
+        Scanner scanner = new Scanner(System.in);       // new Scanner(System.in)で標準入力を読み取るscannerを生成.
+        System.out.print("number: ");
+        try {
+            int number = scanner.nextInt();
+            System.out.println("number = " + number);
+        }
+        catch (Exception e) {
+            System.out.println("これは数値ではありません");
+        }
+
+    }
+
+}
