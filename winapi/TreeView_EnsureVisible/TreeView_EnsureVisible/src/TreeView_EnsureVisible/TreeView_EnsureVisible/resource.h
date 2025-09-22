c8e4ee6946b301b6a8f207e0cc298896eeea86f5
bg1bgst333 <bg1bgst333@gmail.com>
2025-09-22 11:50:45 +0900

    add TreeView_EnsureVisible

---
+++ winapi/TreeView_EnsureVisible/TreeView_EnsureVisible/src/TreeView_EnsureVisible/TreeView_EnsureVisible/resource.h
@@ -0,0 +1,10 @@
+#include <windows.h>
+#include <commctrl.h>
+
+#define IDR_MAINMENU		101
+#define ID_DIALOG		10101
+#define IDD_DIALOG1		201
+#define IDC_TREE1		301
+#define ID_BUTTON1		401
+#define ID_BUTTON2		402
+#define ID_EDIT_NAME		411
