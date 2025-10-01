e54f8e152236048c1ce04b9c5fc6210c6c520eb9
bg1bgst333 <bg1bgst333@gmail.com>
2025-10-01 10:57:46 +0900

    add TreeView_SortChildren

---
+++ winapi/TreeView_SortChildren/TreeView_SortChildren/src/TreeView_SortChildren/TreeView_SortChildren/resource.h
@@ -0,0 +1,14 @@
+#include <windows.h>
+#include <commctrl.h>
+
+#define IDR_MAINMENU		101
+#define IDR_CONTEXTMENU		102
+#define ID_DIALOG		10101
+#define ID_DELETE_ITEM		10201
+#define ID_SORT_ITEMS		10202
+#define IDD_DIALOG1		201
+#define IDC_TREE1		301
+#define ID_BUTTON1		401
+#define ID_BUTTON2		402
+#define ID_BUTTON3		403
+#define ID_EDIT_NAME		411
