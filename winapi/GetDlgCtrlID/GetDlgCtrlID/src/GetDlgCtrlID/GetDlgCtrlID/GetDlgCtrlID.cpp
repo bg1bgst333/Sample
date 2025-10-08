23310a90ad664e477e82cf9b2d3f61ad50d01521
bg1bgst333 <bg1bgst333@gmail.com>
2025-10-08 11:29:48 +0900

    add GetDlgCtrlID

---
+++ winapi/GetDlgCtrlID/GetDlgCtrlID/src/GetDlgCtrlID/GetDlgCtrlID/GetDlgCtrlID.cpp
@@ -0,0 +1,291 @@
+// ヘッダファイルのインクルード
+// 既定のヘッダファイル
+#include <windows.h>	// 標準WindowsAPI
+#include <windowsx.h>	// 互換マクロ
+#include <tchar.h>		// TCHAR型
+
+// 独自のヘッダファイル
+#include "resource.h"	// リソースID
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ウィンドウメッセージに対して独自の処理をするように定義したコールバック関数WindowProc.
+INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);	// ダイアログの処理をするコールバック関数DialogProc.
+
+// _tWinMain関数の定義
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd){
+
+	// 変数の宣言
+	HWND hWnd;					// CreateWindowで作成したウィンドウのウィンドウハンドルを格納するHWND型変数hWnd.
+	MSG msg;					// ウィンドウメッセージ情報を格納するMSG構造体型変数msg.
+	WNDCLASS wc;				// ウィンドウクラス情報をもつWNDCLASS構造体型変数wc.
+
+	// ウィンドウクラスの設定
+	wc.lpszClassName = _T("GetDlgCtrlID");						// ウィンドウクラス名は"GetDlgCtrlID".
+	wc.style = CS_HREDRAW | CS_VREDRAW;							// スタイルはCS_HREDRAW | CS_VREDRAW.
+	wc.lpfnWndProc = WindowProc;								// ウィンドウプロシージャは独自の処理を定義したWindowProc.
+	wc.hInstance = hInstance;									// インスタンスハンドルは_tWinMainの引数.
+	wc.hIcon = LoadIcon(hInstance, IDI_APPLICATION);			// アイコンはアプリケーション既定のもの.
+	wc.hCursor = LoadCursor(hInstance, IDC_ARROW);				// カーソルは矢印.
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);		// 背景は白ブラシ.
+	wc.lpszMenuName = MAKEINTRESOURCE(IDR_MAINMENU);			// メニューにはIDR_MAINMENUをMAKEINTRESOURCEマクロで指定.
+	wc.cbClsExtra = 0;											// 0でいい.
+	wc.cbWndExtra = 0;											// 0でいい.
+
+	// ウィンドウクラスの登録
+	if (!RegisterClass(&wc)){	// RegisterClassでウィンドウクラスを登録し, 0が返ったらエラー.
+
+		// エラー処理
+		MessageBox(NULL, _T("RegisterClass failed!"), _T("GetDlgCtrlID"), MB_OK | MB_ICONHAND);	// MessageBoxで"RegisterClass failed!"とエラーメッセージを表示.
+		return -1;	// 異常終了(1)
+
+	}
+
+	// ウィンドウの作成
+	hWnd = CreateWindow(_T("GetDlgCtrlID"), _T("GetDlgCtrlID"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);	// CreateWindowで, 上で登録した"GetDlgCtrlID"ウィンドウクラスのウィンドウを作成.
+	if (hWnd == NULL){	// ウィンドウの作成に失敗したとき.
+
+		// エラー処理
+		MessageBox(NULL, _T("CreateWindow failed!"), _T("GetDlgCtrlID"), MB_OK | MB_ICONHAND);	// MessageBoxで"CreateWindow failed!"とエラーメッセージを表示.
+		return -2;	// 異常終了(2)
+
+	}
+
+	// ウィンドウの表示
+	ShowWindow(hWnd, SW_SHOW);	// ShowWindowでSW_SHOWを指定してウィンドウの表示.
+
+	// メッセージループ
+	while (GetMessage(&msg, NULL, 0, 0) > 0){	// GetMessageでメッセージを取得, 戻り値が0より大きい間はループし続ける.
+
+		// ウィンドウメッセージの送出
+		DispatchMessage(&msg);	// DispatchMessageで受け取ったメッセージをウィンドウプロシージャ(この場合は独自に定義したWindowProc)に送出.
+		TranslateMessage(&msg);	// TranslateMessageで仮想キーメッセージを文字メッセージへ変換.
+
+	}
+
+	// プログラムの終了
+	return (int)msg.wParam;	// 終了コード(msg.wParam)を戻り値として返す.
+
+}
+
+// WindowProc関数の定義
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){	// ウィンドウメッセージに対して独自の処理をするように定義したウィンドウプロシージャ.
+
+	// ウィンドウメッセージに対する処理.
+	switch (uMsg){	// switch-casa文でuMsgの値ごとに処理を振り分ける.
+
+		// ウィンドウの作成が開始された時.
+		case WM_CREATE:		// ウィンドウの作成が開始された時.(uMsgがWM_CREATEの時.)
+		
+			// WM_CREATEブロック
+			{
+
+				// ウィンドウ作成成功
+				return 0;	// return文で0を返して, ウィンドウ作成成功とする.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// ウィンドウが破棄された時.
+		case WM_DESTROY:	// ウィンドウが破棄された時.(uMsgがWM_DESTROYの時.)
+
+			// WM_DESTROYブロック
+			{
+
+				// メッセージループを抜ける.
+				PostQuitMessage(0);	// PostQuitMessageで抜ける.
+
+			}
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+		// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりした時.
+		case WM_COMMAND:	// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりした時.(uMsgがWM_COMMANDの時.)
+
+			// WM_COMMANDブロック
+			{
+
+				// どのメニュー項目が選択されたかを判定する.
+				switch (LOWORD(wParam)){	// LOWORD(wParam)で選択されたメニュー項目のIDが取得できるので, その値で判定する.
+
+					// 取得したIDごとに処理を分岐.
+					// Dialogが選択された時.
+					case ID_DIALOG:
+
+						// ID_DIALOGブロック
+						{
+
+							// 変数の宣言
+							HINSTANCE hInstance;	// アプリケーションインスタンスハンドル
+
+							// hInstanceを取得
+							hInstance = (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE);	// GetWindowLongでアプリケーションインスタンスハンドルを取得し, hInstanceに格納.
+
+							// ダイアログボックスの表示
+							DialogBox(hInstance, MAKEINTRESOURCEW(IDD_DIALOG1), hwnd, DialogProc);	// DialogBoxでダイアログボックスを表示.(ダイアログの処理はDialogProcに書いてある.)
+
+							// 0を返す.
+							return 0;	// 処理したので戻り値として0を返す.
+
+						}
+
+						// 既定の処理へ向かう.
+						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+						// 上記以外の時.
+					default:
+
+						// 既定の処理へ向かう.
+						break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+						
+				}
+
+			}
+
+		// それ以外の時.
+		default:
+
+			// 既定の処理へ向かう.
+			break;	// breakで抜けて, 既定の処理(DefWindowProc)へ向かう.
+
+	}
+
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+
+}
+
+// DialogProc関数の定義
+INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam){	// ダイアログの処理をするコールバック関数.
+
+	// 変数の初期化.
+	static HBRUSH hbrBackground = NULL;	// hbrBackgroundをNULLで初期化.
+	static HBRUSH hbrEditBackground = NULL;	// hbrEditBackgroundをNULLで初期化.
+
+	// ダイアログのメッセージ処理
+	switch (uMsg){	// uMsgの内容で判断.
+
+		// ダイアログの初期化時.
+		case WM_INITDIALOG:	// ダイアログの初期化時.(uMsgがWM_INITDIALOGの時.)
+
+			// WM_INITDIALOGブロック
+			{
+
+				// TRUEを返す.
+				return TRUE;	// 処理できたのでTRUE.
+
+			}
+			
+			// 抜ける.
+			break;	// breakで抜ける.
+
+		// ダイアログが閉じられた時.
+		case WM_CLOSE:	// ダイアログが閉じられた時.(uMsgがWM_CLOSEの時.)
+
+			// WM_CLOSEブロック
+			{
+
+				// ブラシの破棄.
+				if (hbrEditBackground != NULL){
+					DeleteObject(hbrEditBackground);
+					hbrEditBackground = NULL;
+				}
+				if (hbrBackground != NULL){
+					DeleteObject(hbrBackground);
+					hbrBackground = NULL;
+				}
+
+				// ダイアログを終了する.
+				EndDialog(hwndDlg, IDOK);	// EndDialogでダイアログを終了する.
+
+				// TRUEを返す.
+				return TRUE;	// 処理できたのでTRUE.
+
+			}
+
+			// 抜ける.
+			break;	// breakで抜ける.
+
+		// コモンコントロールから通知が来た時.
+		case WM_NOTIFY:
+
+			// WM_NOTIFYブロック
+			{
+
+			}
+
+			// 既定の処理へ向かう.
+			break;
+
+		// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりして, コマンドが発生した時.
+		case WM_COMMAND:	// メニュー項目が選択されたり, ボタンなどのコントロールが押されたりして, コマンドが発生した時.(uMsgがWM_COMMANDの時.)
+
+			// WM_COMMANDブロック
+			{
+
+			}
+
+			// 抜ける.
+			break;	// breakで抜ける.
+
+		// ダイアログのエディットコントロールの背景の描画を要求された時.
+		case WM_CTLCOLOREDIT:
+
+			// WM_CTLCOLOREDITブロック
+			{
+
+				// ブラシハンドル作成.
+				if (hbrEditBackground == NULL){
+					hbrEditBackground = CreateSolidBrush(RGB(255, 0, 255));	// 紫
+				}
+
+				// 変換
+				HDC hDC = (HDC)wParam;
+				HWND hwndCtrl = (HWND)lParam;
+				int nID = GetDlgCtrlID(hwndCtrl);	// GetDlgCtrlIDで指定のコントロールのウィンドウハンドルに紐付くリソースIDを取得.
+				
+				// 判定
+				if (nID == IDC_EDIT1){	// IDC_EDIT1
+					SetBkColor(hDC, RGB(255, 0, 255));	// 紫
+					SetTextColor(hDC, RGB(255, 255, 255));	// 白
+					return (INT_PTR)hbrEditBackground;	// 紫のブラシを返す.
+				}
+				else{
+					return FALSE;	// それ以外はFALSEを返す.
+				}
+
+			}
+
+			// 抜ける.
+			break;	// breakで抜ける.
+
+		// ダイアログ本体の背景の描画を要求された時.
+		case WM_CTLCOLORDLG:
+
+			// WM_CTLCOLORDLGブロック
+			{
+
+				// デバイスコンテキストハンドルへの変換.
+				HDC hDC = (HDC)wParam;
+
+				// ブラシハンドル作成.
+				if (hbrBackground == NULL){
+					hbrBackground = CreateSolidBrush(RGB(0, 255, 0));
+				}
+				return (INT_PTR)hbrBackground;
+
+			}
+
+		// 上記以外の時.
+		default:
+
+			// 抜ける.
+			break;	// breakで抜ける.
+
+	}
+
+	// ここに来るときは処理できていない.
+	return FALSE;	// 処理できていないのでFALSE.
+
+}
