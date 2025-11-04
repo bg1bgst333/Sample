d6ff141a2907f2405a5227fc64196687add790db
bg1bgst333 <bg1bgst333@gmail.com>
2025-11-04 10:39:03 +0900

    add CWnd::CreateEx

---
+++ mfc/CWnd/CreateEx/src/CWnd/CWnd/CWndDlg.h
@@ -0,0 +1,36 @@
+// CWndDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CWndDlg ダイアログ
+class CWndDlg : public CDialog
+{
+// コンストラクション
+public:
+	CWndDlg(CWnd* pParent = NULL);	// 標準コンストラクタ
+
+// ダイアログ データ
+	enum { IDD = IDD_CWND_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnDestroy();
+
+// 追加
+public:
+	CWnd *m_pButton1;	// CWndオブジェクトポインタm_pButton1
+};
