c4dd28819085d7658a09c4acda1270ae871475d3
bg1bgst333 <bg1bgst333@gmail.com>
2025-11-22 10:22:30 +0900

    add CComboBox::AddString

---
+++ mfc/CComboBox/AddString/src/CComboBox/CComboBox/CComboBoxDlg.h
@@ -0,0 +1,33 @@
+// CComboBoxDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CComboBoxDlg ダイアログ
+class CComboBoxDlg : public CDialog
+{
+// コンストラクション
+public:
+	CComboBoxDlg(CWnd* pParent = NULL);	// 標準コンストラクタ
+
+// ダイアログ データ
+	enum { IDD = IDD_CCOMBOBOX_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+};
