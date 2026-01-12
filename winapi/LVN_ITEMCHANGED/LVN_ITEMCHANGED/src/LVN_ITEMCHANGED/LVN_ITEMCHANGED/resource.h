9493dce61d1ec63afc466605414d3823efc2c21f
bg1bgst333 <bg1bgst333@gmail.com>
2026-01-12 14:27:15 +0900

    add LVN_ITEMCHANGED

---
+++ winapi/LVN_ITEMCHANGED/LVN_ITEMCHANGED/src/LVN_ITEMCHANGED/LVN_ITEMCHANGED/resource.h
@@ -0,0 +1,15 @@
+#include <windows.h>
+#include <commctrl.h>
+
+#define IDR_MAINMENU		101
+#define ID_DIALOG		10101
+#define IDD_DIALOG1		201
+#define IDC_LIST1		301
+#define ID_BUTTON1		311
+#define ID_BUTTON2		312
+#define ID_BUTTON3		313
+#define ID_BUTTON4		314
+#define ID_EDIT_NAME		321
+#define ID_EDIT_ADDRESS		331
+#define ID_EDIT_PHONENUMBER	341
+#define ID_EDIT_AGE		351
