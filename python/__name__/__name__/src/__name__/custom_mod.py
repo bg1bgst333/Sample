a93f90684ab7fac1f32c6700e90e14439c18c301
bg1bgst333 <bg1bgst333@gmail.com>
2026-04-07 09:53:10 +0900

    add __name__

---
+++ python/__name__/__name__/src/__name__/custom_mod.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# __name__の中身がモジュール名なので, それをprintで出力.
+print(__name__)
---
+++ python/__name__/__name__/src/__name__/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# custom_modのimport
+import custom_mod
+
+# __name__の中身がモジュール名なので, それをprintで出力.
+print(__name__)
