684176710a95e89bab4efc653a855c27cd56610b
bg1bgst333 <bg1bgst333@gmail.com>
2026-04-24 14:55:00 +0900

    add net.Server.address

---
+++ nodejs/net.Server/address/src/net.Server/server.js
@@ -0,0 +1,19 @@
+// httpモジュール参照の取得
+const http = require('http'); // requireで'http'モジュールの参照をconstのfsに格納.
+
+// 簡易Webサーバの作成
+var server = http.createServer((req, res) => { // http.createServerで簡易Webサーバを作成し, serverに格納.(その結果に対する処理はラムダ式で書く.)
+
+  // HTTPレスポンスヘッダの書き込み.
+  res.writeHead(200, {'Content-Type': 'text/plain'}); // HTTPステータスコードは200, Content-Typeはtext/plainであることをres.writeHeadで書き込む.
+
+  // HTTPレスポンスボディの最後に'ABCDE'を書き込む.
+  res.end('ABCDE\n'); // res.endで'ABCDE'と改行を書き込んで終わる.
+
+});
+
+// リッスン.
+server.listen(3000, 'localhost', () => {
+  console.log('Local Server Listen');
+  console.log(server.address()); // server.addressでアドレス情報を取得し, 出力.
+}); // ポート番号3000, ホスト名localhostでリッスン, 'Local Server Listen'と出力.
