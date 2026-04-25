851fedde35b162ce0d01d17141658a8a730ce706
bg1bgst333 <bg1bgst333@gmail.com>
2026-04-25 11:20:07 +0900

    add String.charAt

---
+++ java/String/charAt/src/String/MainClass.java
@@ -0,0 +1,18 @@
+// メインクラス
+public class MainClass{
+
+	// Javaのエントリポイント
+	public static void main(String[] arg){ // mainメソッドの定義
+
+		// 文字列strの定義.
+		String str = "ABCDE";
+
+		// 2番目の文字を取得.
+		char c = str.charAt(2);
+
+		// 出力.
+		System.out.println(c);
+
+	}
+
+}
