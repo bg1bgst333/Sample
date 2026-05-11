c3e0a9ba5f34dfc000a1ecad2151383c0c760eea
bg1bgst333 <bg1bgst333@gmail.com>
2026-05-11 22:44:47 +0900

    add String.toLowerCase

---
+++ java/String/toLowerCase/src/String/MainClass.java
@@ -0,0 +1,18 @@
+// メインクラス
+public class MainClass{
+
+	// Javaのエントリポイント
+	public static void main(String[] arg){ // mainメソッドの定義
+
+		// 文字列str1の定義.
+		String str1 = "AbcDE!fgHI";
+
+		// 全て小文字に変換.
+		String str2 = str1.toLowerCase();
+
+		// 出力.
+		System.out.println(str2);
+
+	}
+
+}
