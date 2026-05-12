df13c99db3123ef98310411c47801429beefa3e7
bg1bgst333 <bg1bgst333@gmail.com>
2026-05-12 13:46:51 +0900

    add String.valueOf

---
+++ java/String/valueOf/src/String/MainClass.java
@@ -0,0 +1,18 @@
+// メインクラス
+public class MainClass{
+
+	// Javaのエントリポイント
+	public static void main(String[] arg){ // mainメソッドの定義
+
+		// int型変数iの初期化.
+		int val = 1234;
+
+		// int型をStringに変換.
+		String str = String.valueOf(val);
+
+		// 出力.
+		System.out.println(str);
+
+	}
+
+}
