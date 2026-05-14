10b7aa713d02b649246e8fbf6e190d9a6be57707
bg1bgst333 <bg1bgst333@gmail.com>
2026-05-14 09:20:12 +0900

    add String.concat

---
+++ java/String/concat/src/String/MainClass.java
@@ -0,0 +1,19 @@
+// メインクラス
+public class MainClass{
+
+	// Javaのエントリポイント
+	public static void main(String[] arg){ // mainメソッドの定義
+
+		// 2つの文字列を定義.
+		String str1 = "ABC";
+		String str2 = "DEF";
+
+		// 2つの文字列を連結.
+		String str3 = str1.concat(str2);
+
+		// 出力.
+		System.out.println(str3);
+
+	}
+
+}
