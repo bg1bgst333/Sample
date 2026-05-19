83a86a70c519bbe1bb49b4ee6713803e6a9f5312
bg1bgst333 <bg1bgst333@gmail.com>
2026-05-19 13:19:52 +0900

    add SOCK_STREAM

---
+++ unixsyscall/SOCK_STREAM/SOCK_STREAM/src/SOCK_STREAM/SOCK_STREAM.c
@@ -0,0 +1,53 @@
+/* ヘッダファイルのインクルード */
+#include <stdio.h> /* 標準入出力 */
+#include <unistd.h> /* UNIX標準 */
+#include <sys/types.h> /* 派生型 */
+#include <sys/socket.h> /* ソケット */
+ 
+/* main関数の定義 */
+int main(void){
+
+  /* 変数の宣言 */
+  int soc;  /* ソケットファイルディスクリプタsoc */
+  int type; /* ソケット種別 */
+  socklen_t len; /* getsockoptのサイズ */
+  int ret; /* getsockoptの戻り値 */
+ 
+  /* TCPソケットの作成 */
+  soc = socket(AF_INET, SOCK_STREAM, 0); /* SOCK_STREAM指定でTCPソケットを作成し, socに格納. */
+  if (soc == -1){ /* socが-1の時はエラー. */
+
+    /* エラー処理 */
+    printf("socket error!\n"); /* "socket error!"と出力. */
+    return -1; /* -1を返す. */
+
+  }
+ 
+  /* socの値を出力. */
+  printf("soc = %d\n", soc); /* printfでsocの値を出力. */
+ 
+  /* SO_TYPEでソケット種別を取得. */
+  len = sizeof(type);
+  ret = getsockopt(soc, SOL_SOCKET, SO_TYPE, &type, &len); /* getsockoptでSO_TYPEを取得し, retに格納. */
+  if (ret == -1){ /* retが-1の時はエラー. */
+
+    /* エラー処理 */
+    printf("getsockopt error!\n"); /* "getsockopt error!"と出力. */
+    close(soc);
+    return -1; /* -1を返す. */
+
+  }
+ 
+  /* typeの値を出力. */
+  printf("type = %d\n", type); /* printfでtypeの値を出力. */
+  if (type == SOCK_STREAM){ /* typeがSOCK_STREAMの時. */
+    printf("SOCK_STREAM\n"); /* "SOCK_STREAM"と出力. */
+  }
+ 
+  /* TCPソケットファイルディスクリプタを閉じる. */
+  close(soc); /* closeでsocを閉じる. */
+ 
+  /* プログラムの終了 */
+  return 0;
+
+}
